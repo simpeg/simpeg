150cbc7df3b7c94e4173629b5130776079289aaa
Rowan Cockett <rowan@visiblegeology.com>
2013-07-26 12:11:45 -0700

    Test for edge inner products working.

--- SimPEG/tests/test_massMatrices.py
+++ SimPEG/tests/test_massMatrices.py
@@ -2,18 +2,21 @@ import numpy as np
 import unittest
 import sys
 sys.path.append('../')
-from TensorMesh import TensorMesh
 from OrderTest import OrderTest
-from scipy.sparse.linalg import dsolve
-from getEdgeInnerProducts import getEdgeInnerProducts
+from getEdgeInnerProducts import *
 
 
-class TestNodalGrad(OrderTest):
-    name = "Nodal Gradient"
-    
+class TestEdgeInnerProduct(OrderTest):
+    """Integrate a function over a unit cube domain."""
+
+    name = "Edge Inner Product"
+
     meshSizes = [4, 8, 16, 32]
 
     def getError(self):
+
+        call = lambda fun, xyz: fun(xyz[:, 0], xyz[:, 1], xyz[:, 2])
+
         ex = lambda x, y, z: x**2+y*z
         ey = lambda x, y, z: (z**2)*x+y*z
         ez = lambda x, y, z: y**2+x*z
@@ -24,25 +27,25 @@ class TestNodalGrad(OrderTest):
         sigma4 = lambda x, y, z: 0.1*x*y*z
         sigma5 = lambda x, y, z: 0.2*x*y
         sigma6 = lambda x, y, z: 0.1*z
-        
-        Ex = ex(self.M.gridEx[:, 0],self.M.gridEx[:, 1],self.M.gridEx[:, 2])
-        Ey = ey(self.M.gridEy[:, 0],self.M.gridEy[:, 1],self.M.gridEy[:, 2])
-        Ez = ez(self.M.gridEz[:, 0],self.M.gridEz[:, 1],self.M.gridEz[:, 2])
-        
-        E = np.r_[Ex,Ey,Ez]
+
+        Ex = call(ex, self.M.gridEx)
+        Ey = call(ey, self.M.gridEy)
+        Ez = call(ez, self.M.gridEz)
+
+        E = np.matrix(mkvc(np.r_[Ex, Ey, Ez], 2))
         Gc = self.M.gridCC
-        sigma = np.c_[sigma1(Gc[:,0],Gc[:,1],Gc[:,2]),
-                      sigma2(Gc[:,0],Gc[:,1],Gc[:,2]),
-                      sigma3(Gc[:,0],Gc[:,1],Gc[:,2]),
-                      sigma4(Gc[:,0],Gc[:,1],Gc[:,2]),
-                      sigma5(Gc[:,0],Gc[:,1],Gc[:,2]),
-                      sigma6(Gc[:,0],Gc[:,1],Gc[:,2])]
+        sigma = np.c_[call(sigma1, Gc), call(sigma2, Gc), call(sigma3, Gc),
+                      call(sigma4, Gc), call(sigma5, Gc), call(sigma6, Gc)]
 
-        A = getEdgeInnerProducts(self.M, sigma)
-
-        err = np.abs(E.T*A*E - 69881./21600)
-
+        A = getEdgeInnerProduct(self.M, sigma)
+        numeric = E.T*A*E
+        analytic = 69881./21600  # Found using matlab symbolic toolbox.
+        err = np.abs(numeric - analytic)
         return err
 
     def test_order(self):
         self.orderTest()
+
+
+if __name__ == '__main__':
+    unittest.main()
