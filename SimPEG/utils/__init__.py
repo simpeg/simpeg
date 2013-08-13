3e21ba86a7246e336c343b6b40d1607591a2a8b5
Lars Ruthotto <lruthotto@eos.ubc.ca>
2013-08-12 17:04:15 -0700

    fixed typo in util's __init__.py

--- SimPEG/utils/__init__.py
+++ SimPEG/utils/__init__.py
@@ -1,3 +1,3 @@
 from utils import getSubArray, mkvc, ndgrid, ind2sub, sub2ind
 from sputils import spzeros, kron3, speye, sdiag
-from lomUtils import volTetra, faceInfo, inv2X2BlockDiagonal, inv3X3BlockDiagonal, indexCube, exampleLomGird
+from lomutils import volTetra, faceInfo, inv2X2BlockDiagonal, inv3X3BlockDiagonal, indexCube, exampleLomGird
