d7d4303e34cf309671c1869f1966984548ec51d5
Rowan Cockett <rowan@visiblegeology.com>
2013-09-30 10:27:03 -0700

    update to conf.py for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ sys.path.append('../')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'matplotlib.sphinxext.plot_directive']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc'] #, 'matplotlib.sphinxext.plot_directive'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
