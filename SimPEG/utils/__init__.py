1427fe5cec9abd8f0a7a71f341cee0b699c5634b
Rowan Cockett <rowan@visiblegeology.com>
2013-11-07 12:27:59 -0800

    Able to add movies to the python notebook. using utils.animate(fig,animate,init)

--- SimPEG/utils/__init__.py
+++ SimPEG/utils/__init__.py
@@ -9,3 +9,4 @@ from matutils import getSubArray, mkvc, ndgrid, ind2sub, sub2ind
 from sputils import spzeros, kron3, speye, sdiag
 from lomutils import volTetra, faceInfo, inv2X2BlockDiagonal, inv3X3BlockDiagonal, indexCube, exampleLomGird
 from interputils import interpmat
+from ipythonUtils import easyAnimate as animate
