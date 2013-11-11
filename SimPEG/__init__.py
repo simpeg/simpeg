2cbbb3639c5adada3b6cf707ea706c597f0e0a94
Gudni Karl Rosenkjaer <gkrosen@gmail.com>
2013-11-11 09:47:35 -0800

    Fixed SimPEG.__init__.py to include visulization (some merging issuse).

--- SimPEG/__init__.py
+++ SimPEG/__init__.py
@@ -2,10 +2,6 @@ import utils
 from utils import Solver
 import mesh
 import inverse
-<<<<<<< HEAD
-from Solver import Solver
 import visulize
-=======
 import forward
 import regularization
->>>>>>> refs/remotes/origin/master
