6c029e5e2c5d0bd379b5e086c630119579215483
rowanc1 <rowanc1@gmail.com>
2013-11-22 01:26:22 +0000

    Added matplotlib into the startup. SimPEG needs it.

--- SimPEG/GCEtools/startup.sh
+++ SimPEG/GCEtools/startup.sh
@@ -2,7 +2,7 @@
 sudo aptitude -y update
 sudo aptitude -y upgrade
 sudo aptitude -y install gcc gfortran git libopenmpi-dev python-pip python-dev
-sudo aptitude -y install ipython python-scipy python-numpy python-nose python-pip
+sudo aptitude -y install ipython python-scipy python-numpy python-nose python-pip python-matplotlib
 sudo aptitude -y install libmumps-ptscotch-4.10.0 libmumps-ptscotch-dev
 sudo aptitude -y install libblas-dev liblapack-dev
 
