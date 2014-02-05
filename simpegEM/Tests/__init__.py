45c25365d905ea593871499eca0dd2c9241d720f
rowanc1 <rowan@visiblegeology.com>
2014-02-05 13:13:06 -0800

    Error in testing init file.

--- simpegEM/Tests/__init__.py
+++ simpegEM/Tests/__init__.py
@@ -1,10 +1,8 @@
-from TestUtils import checkDerivative, Rosenbrock, OrderTest, getQuadratic
-
+import os
+import glob
+import unittest
 
 if __name__ == '__main__':
-    import os
-    import glob
-    import unittest
     test_file_strings = glob.glob('test_*.py')
     module_strings = [str[0:len(str)-3] for str in test_file_strings]
     suites = [unittest.defaultTestLoader.loadTestsFromName(str) for str
