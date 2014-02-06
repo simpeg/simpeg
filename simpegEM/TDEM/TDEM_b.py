92788ad5f1352dc86c8ed6b7c785949a31005d50
Dave Marchant <dwfmarchant@gmail.com>
2014-02-05 23:11:53 -0800

    Added example of field generation.

--- simpegEM/TDEM/TDEM_b.py
+++ simpegEM/TDEM/TDEM_b.py
@@ -59,3 +59,30 @@ class ProblemTDEM_b(ProblemBaseTDEM):
     def getRHS(self, tInd, F):
         dt = self.getDt(tInd)
         return (1/dt)*self.MfMui*F.get_b(tInd-1)        
+
+if __name__ == '__main__':
+    from SimPEG import *
+    import simpegEM as EM
+
+    cs = 5.
+    ncx = 20
+    ncy = 6
+    npad = 15
+    hx = Utils.meshTensors(((0,cs), (ncx,cs), (npad,cs)))
+    hy = Utils.meshTensors(((npad,cs), (ncy,cs), (npad,cs)))
+    mesh = Mesh.Cyl1DMesh([hx,hy], -hy.sum()/2)
+    model = Model.Vertical1DModel(mesh)
+
+    txLoc = 0.
+    txType = 'VMD_MVP'
+    rxLoc = np.r_[150., 0.]
+    rxType = 'bz'
+    timeCh = np.logspace(-4,-2,20)
+    dat = EM.TDEM.DataTDEM1D(txLoc=txLoc, txType=txType, rxLoc=rxLoc, rxType=rxType, timeCh=timeCh)
+
+    prb = EM.TDEM.ProblemTDEM_b(mesh, model)
+    prb.setTimes([1e-5, 5e-5, 2.5e-4], [50, 50, 50])
+    prb.sigma = np.ones(mesh.nCz)
+    prb.pair(dat)
+
+    F = prb.field(prb.sigma)
