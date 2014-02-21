cc33d148b67d3bdd74c24859d8a4e4efc409738d
seogi <sgkang09@gmail.com>
2014-02-20 17:30:31 -0800

    initial set up

--- simpegPF/Tests/test_forward_PFproblem.py
+++ simpegPF/Tests/test_forward_PFproblem.py
@@ -1,15 +1,31 @@
 import unittest
 from SimPEG import *
-import simpegPF
+import simpegPF as PF
 
 
-class PFProblemTests(unittest.TestCase):
+class MagProblemTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        M = Mesh.TensorMesh([10,10])
+        mod = Model.LogModel(M)
+        prob = PF.Mag.MagProblem(M, mod, None)
 
-    def test_awesome(self):
-        self.assertTrue(True)
+        self.prob = prob
+        self.M = M    
+
+    def test_forward(self):
+    
+        passed = True
+        self.assertTrue(passed) 
+
+
+    def test_DirchletBC(self):
+        q = lambda x: np.sin(x)
+
+        M = self.M
+        order = 2
+        self.assertTrue(order > 1)
+
 
 
 if __name__ == '__main__':
