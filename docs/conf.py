752696a5d56d584688fd9684eb61b8a0ac7048ac
rowanc1 <rowan@visiblegeology.com>
2014-02-27 15:20:35 -0800

    added a few things!

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 
 sys.path.append('../')
+sys.path.append('../../')
 
 # -- General configuration -----------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -8,5 +8,3 @@ except ImportError, e:
     os.system('mv simpeg/SimPEG temp')
     os.system('rm -rf simpeg')
     os.system('mv temp SimPEG')
-    os.system('mkdir docs/examples/SimPEG')
-    os.system('cp -r SimPEG simpegflow/docs/examples/SimPEG')
