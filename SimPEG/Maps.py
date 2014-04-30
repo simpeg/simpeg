6adcce86c8d1bb04b8a89712c82cf36ad9e80c2a
rowanc1 <rowan@visiblegeology.com>
2014-04-30 11:48:10 -0700

    Added SimPEGLinearOperator which has transpose functionality. Changed ComplexMap to use this operator.

--- SimPEG/Maps.py
+++ SimPEG/Maps.py
@@ -369,29 +369,35 @@ class ComboMap(IdentityMap):
         return deriv
 
 class ComplexMap(IdentityMap):
-    """docstring for ComplexMap"""
-    def __init__(self, mesh):
+    """docstring for ComplexMap
+
+        default nP is nC in the mesh times 2 [real, imag]
+
+    """
+    def __init__(self, mesh, nP=None):
         IdentityMap.__init__(self, mesh)
+        if nP is not None:
+            assert nP%2 == 0, 'nP must be even.'
+        self._nP = nP or (self.mesh.nC * 2)
 
     @property
     def nP(self):
-        return self.mesh.nC * 2
+        return self._nP
 
     def transform(self, m):
         nC = self.mesh.nC
         return m[:nC] + m[nC:]*1j
 
-    def transformDeriv(self, m, v=None, adjoint=False):
-        nC = self.mesh.nC
-        if v is None and adjoint is False:
-            return sp.hstack((sp.identity(nC), np.identity(nC,dtype=complex)*1j))
-
-        assert v is not None, 'Must have a vector to multiply by.'
-
-        if adjoint is False:
-            return sp.hstack((sp.identity(nC), np.identity(nC,dtype=complex)*1j)) * v
-        elif adjoint is True:
+    def transformDeriv(self, m):
+        nC = self.nP/2
+        shp = (nC, nC*2)
+        def fwd(v):
+            return v[:nC] + v[nC:]*1j
+        def adj(v):
             return np.r_[v.real,v.imag]
+        return Utils.SimPEGLinearOperator(shp,fwd,adj)
+
+    transformInverse = transformDeriv
 
 
 if __name__ == '__main__':
