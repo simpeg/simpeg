8b30a1fe6a2570436ca392cbf92dade1b531f217
rowanc1 <rowan@visiblegeology.com>
2014-04-30 18:29:41 -0700

    MVP choose down.

--- simpegEM/Utils/Sources/magneticDipole.py
+++ simpegEM/Utils/Sources/magneticDipole.py
@@ -36,7 +36,7 @@ def MagneticDipoleVectorPotential(txLoc, obsLoc, component, dipoleMoment=(0., 0.
         dR = obsLoc - txLoc[i, np.newaxis].repeat(nEdges, axis=0)
         mCr = np.cross(m, dR)
         r = np.sqrt((dR**2).sum(axis=1))
-        A[:, i] = -(mu_0/(4*pi)) * mCr[:,dimInd]/(r**3)
+        A[:, i] = +(mu_0/(4*pi)) * mCr[:,dimInd]/(r**3)
     if nTx == 1:
         return A.flatten()
     return A
