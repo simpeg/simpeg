ce7b0b24dadf79e43a3428e1c85a66bcd7bd32b7
Lindsey <lindseyheagy@gmail.com>
2015-05-07 14:44:57 -0700

    changed check of out.shape, since if out is a vector (not an array with width 1), out.shape[1] does not exsist

--- SimPEG/Problem.py
+++ SimPEG/Problem.py
@@ -160,9 +160,8 @@ class Fields(object):
                 assert hasattr(self, func), 'The alias field function is a string, but it does not exist in the Fields class.'
                 func = getattr(self, func)
             out = func(self._fields[alias][:,ind], srcII)
-
-        if out.shape[1] == 1:
-            out = Utils.mkvc(out)
+            if not out.size > out.shape[0]:
+                out = Utils.mkvc(out)
         return out
 
     def __contains__(self, other):
