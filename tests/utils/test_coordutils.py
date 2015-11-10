b7be3861a064867b48232bb60cddbf02dd4152fe
Lindsey Heagy <lheagy@eos.ubc.ca>
2015-11-10 10:51:58 -0800

    set seeds for the coord utils test

--- tests/utils/test_coordutils.py
+++ tests/utils/test_coordutils.py
@@ -7,10 +7,14 @@ tol = 1e-15
 class coorUtilsTest(unittest.TestCase):
 
     def test_rotationMatrixFromNormals(self):
+        np.random.seed(0)
         v0 = np.random.rand(3)
         v0 *= 1./np.linalg.norm(v0)
+
+        np.random.seed(5)
         v1 = np.random.rand(3)
         v1 *= 1./np.linalg.norm(v1)
+
         Rf = Utils.coordutils.rotationMatrixFromNormals(v0,v1)
         Ri = Utils.coordutils.rotationMatrixFromNormals(v1,v0)
 
@@ -18,8 +22,11 @@ class coorUtilsTest(unittest.TestCase):
         self.assertTrue(np.linalg.norm(Utils.mkvc(Ri.dot(v1) - v0)) < tol)
 
     def test_rotatePointsFromNormals(self):
+        np.random.seed(10)
         v0 = np.random.rand(3)
         v0*= 1./np.linalg.norm(v0)
+
+        np.random.seed(15)
         v1 = np.random.rand(3) 
         v1*= 1./np.linalg.norm(v1)   
 
@@ -28,11 +35,15 @@ class coorUtilsTest(unittest.TestCase):
         self.assertTrue(np.linalg.norm(v2-v1) < tol)
 
     def test_rotateMatrixFromNormals(self):
+        np.random.seed(20)
         n0 = np.random.rand(3)
-        n0*= 1./np.linalg.norm(n0)
-        n1 = np.random.rand(3) 
-        n1*= 1./np.linalg.norm(n1) 
+        n0 *= 1./np.linalg.norm(n0)
 
+        np.random.seed(25)
+        n1 = np.random.rand(3) 
+        n1 *= 1./np.linalg.norm(n1) 
+
+        np.random.seed(30)
         scale =  np.random.rand(100,1)
         XYZ0 = scale * n0
         XYZ1 = scale * n1
