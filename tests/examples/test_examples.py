dd5f5df69eefb8ed106376c73a37b0466fa3adfd
Rowan Cockett <rowan@3ptscience.com>
2016-01-03 12:48:01 -0800

    my_function.__name__ must be set to 'test_' for nosetests

--- tests/examples/test_examples.py
+++ tests/examples/test_examples.py
@@ -4,11 +4,11 @@ from SimPEG import Examples
 import numpy as np
 
 def get(test):
-    def func(self):
+    def test_func(self):
         print '\nTesting %s.run(plotIt=False)\n'%test
         getattr(Examples, test).run(plotIt=False)
         self.assertTrue(True)
-    return func
+    return test_func
 attrs = dict()
 for test in Examples.__examples__:
     attrs['test_'+test] = get(test)
