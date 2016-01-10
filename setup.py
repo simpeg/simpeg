d93a23306ce222eee6c3caab58ba08979e5b4154
Rowan Cockett <rowan@3ptscience.com>
2016-01-10 12:47:43 -0800

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ with open("README.rst") as f:
 
 setup(
     name = "SimPEG",
-    version = "0.1.3",
+    version = "0.1.4",
     packages = find_packages(),
     install_requires = ['numpy>=1.7',
                         'scipy>=0.13',
