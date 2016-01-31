2874e204ee3c4a3c42da17e18de5d25e49dccf49
Lindsey Heagy <lheagy@eos.ubc.ca>
2016-01-31 12:52:42 -0800

    exclude _static from warnings

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ release = '0.1.9'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build','_static']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
