429d8b119117e17daa6c63592c87ea7ec26ccba3
seogi_macbook <sgkang09@github.com>
2016-02-10 23:45:19 -0800

    Add EM_FDEM_SusEffects example

--- SimPEG/Examples/__init__.py
+++ SimPEG/Examples/__init__.py
@@ -1,6 +1,7 @@
 # Run this file to add imports.
 
 ##### AUTOIMPORTS #####
+import EM_FDEM_SusEffects
 import DC_PseudoSection_Simulation
 import EM_FDEM_Analytic_MagDipoleWholespace
 import EM_TDEM_1D_Inversion
@@ -15,7 +16,8 @@ import Mesh_QuadTree_FaceDiv
 import Mesh_QuadTree_HangingNodes
 import Mesh_Tensor_Creation
 
-__examples__ = ["DC_PseudoSection_Simulation", "EM_FDEM_Analytic_MagDipoleWholespace", "EM_TDEM_1D_Inversion", "FLOW_Richards_1D_Celia1990", "Forward_BasicDirectCurrent", "Inversion_Linear", "Mesh_Basic_PlotImage", "Mesh_Basic_Types", "Mesh_Operators_CahnHilliard", "Mesh_QuadTree_Creation", "Mesh_QuadTree_FaceDiv", "Mesh_QuadTree_HangingNodes", "Mesh_Tensor_Creation"]
+
+__examples__ = ["EM_FDEM_SusEffects","DC_PseudoSection_Simulation", "EM_FDEM_Analytic_MagDipoleWholespace", "EM_TDEM_1D_Inversion", "FLOW_Richards_1D_Celia1990", "Forward_BasicDirectCurrent", "Inversion_Linear", "Mesh_Basic_PlotImage", "Mesh_Basic_Types", "Mesh_Operators_CahnHilliard", "Mesh_QuadTree_Creation", "Mesh_QuadTree_FaceDiv", "Mesh_QuadTree_HangingNodes", "Mesh_Tensor_Creation"]
 
 ##### AUTOIMPORTS #####
 
