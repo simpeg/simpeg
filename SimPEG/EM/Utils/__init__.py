5c8fba424251b066d4893a827ea15089e9f0e068
Lindsey Heagy <lheagy@eos.ubc.ca>
2016-02-27 15:44:07 -0800

    changing btwn e,j and h,b depends on which formulation we are going between and is now in the fields object

--- SimPEG/EM/Utils/__init__.py
+++ SimPEG/EM/Utils/__init__.py
@@ -1,5 +1,2 @@
-# import Sources
-# import Ana
-# import Solver
 from EMUtils import omega, k
 from AnalyticUtils import MagneticDipoleFields, MagneticDipoleVectorPotential, MagneticLoopVectorPotential
