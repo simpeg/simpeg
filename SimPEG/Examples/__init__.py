f06ba238f8cb3f463b67990bdc7f69b80a4f0be7
GudniRos <gkrosen@gmail.com>
2016-06-07 13:14:36 -0700

    Fixing Examples/__init__.py after merge conflict

--- SimPEG/Examples/__init__.py
+++ SimPEG/Examples/__init__.py
@@ -1,38 +1,27 @@
 # Run this file to add imports.
 
 ##### AUTOIMPORTS #####
+import DC_Analytic_Dipole
+import DC_Forward_PseudoSection
 import EM_FDEM_1D_Inversion
-import Mesh_QuadTree_Creation
+import EM_FDEM_Analytic_MagDipoleWholespace
+import EM_Schenkel_Morrison_Casing
 import EM_TDEM_1D_Inversion
-import Mesh_QuadTree_FaceDiv
-import Mesh_Tensor_Creation
 import FLOW_Richards_1D_Celia1990
-<<<<<<< HEAD
 import Inversion_IRLS
 import Inversion_Linear
 import Mesh_Basic_ForwardDC
-=======
-import DC_Forward_PseudoSection
-import Mesh_Operators_CahnHilliard
-import Mesh_Basic_Types
-import Inversion_IRLS
-import Inversion_Linear
-import EM_Schenkel_Morrison_Casing
-import MT_3D_Foward
-import MT_1D_ForwardAndInversion
-import MT_1D_analytic_nlayer_Earth
-import Forward_BasicDirectCurrent
-import EM_FDEM_Analytic_MagDipoleWholespace
->>>>>>> mt/NSEMrefact
 import Mesh_Basic_PlotImage
-import DC_Analytic_Dipole
+import Mesh_Basic_Types
+import Mesh_Operators_CahnHilliard
+import Mesh_QuadTree_Creation
+import Mesh_QuadTree_FaceDiv
 import Mesh_QuadTree_HangingNodes
-<<<<<<< HEAD
 import Mesh_Tensor_Creation
+import MT_1D_analytic_nlayer_Earth
 import MT_1D_ForwardAndInversion
 import MT_3D_Foward
 import Utils_surface2ind_topo
-
 __examples__ = ["DC_Analytic_Dipole", "DC_Forward_PseudoSection", "EM_FDEM_1D_Inversion", "EM_FDEM_Analytic_MagDipoleWholespace", "EM_Schenkel_Morrison_Casing", "EM_TDEM_1D_Inversion", "FLOW_Richards_1D_Celia1990", "Inversion_IRLS", "Inversion_Linear", "Mesh_Basic_ForwardDC", "Mesh_Basic_PlotImage", "Mesh_Basic_Types", "Mesh_Operators_CahnHilliard", "Mesh_QuadTree_Creation", "Mesh_QuadTree_FaceDiv", "Mesh_QuadTree_HangingNodes", "Mesh_Tensor_Creation", "MT_1D_ForwardAndInversion", "MT_3D_Foward", "Utils_surface2ind_topo"]
 
 ##### AUTOIMPORTS #####
