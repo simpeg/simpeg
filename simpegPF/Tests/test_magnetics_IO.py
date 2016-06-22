7dd0b1b2df5d6b1be47dc6c8abbc552ea694e06d
D Fournier <fourndo@gmail.com>
2016-06-22 09:59:50 -0700

    Latest updates in preparation for merge with SimPEG

--- simpegPF/Tests/test_magnetics_IO.py
+++ simpegPF/Tests/test_magnetics_IO.py
@@ -4,7 +4,7 @@ from simpegPF import BaseMag
 import matplotlib.pyplot as plt
 import simpegPF as PF
 from scipy.constants import mu_0
-
+import os
 
 
 class MagSensProblemTests(unittest.TestCase):
@@ -14,16 +14,21 @@ class MagSensProblemTests(unittest.TestCase):
 
     def test_magnetics_inversion(self):
 
-        driver = PF.MagneticsDriver.MagneticsDriver_Inv('assets/magnetics/SimPEG_MAG3D_inv.inp')
+        driver = PF.MagneticsDriver.MagneticsDriver_Inv('assets' + os.path.sep + 'magnetics' + os.path.sep + 'PYMAG3D_inv.inp')
 
         print driver.mesh
         print driver.survey
         print driver.m0
         print driver.mref
         print driver.activeCells
-
-
-
+        print driver.staticCells
+        print driver.dynamicCells
+        print driver.chi
+        print driver.nC
+        print driver.alphas
+        print driver.bounds
+        print driver.lpnorms
+        print driver.eps
 
 if __name__ == '__main__':
     unittest.main()
