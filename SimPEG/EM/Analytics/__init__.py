8b44f8d96b91dec91a1f0d5dc1cad1c96e59cf43
seogi_macbook <sgkang09@github.com>
2016-06-23 10:14:13 -0700

    change FDEM_fields.py to FDEMDipolarfield.py

--- SimPEG/EM/Analytics/__init__.py
+++ SimPEG/EM/Analytics/__init__.py
@@ -2,4 +2,4 @@ from TDEM import hzAnalyticDipoleT
 from FDEM import hzAnalyticDipoleF
 from FDEMcasing import *
 from DC import DCAnalyticHalf, DCAnalyticSphere
-from FDEM_fields import *
+from FDEMDipolarfields import *
