2d992f32d4b3340d3fd918c13f272f2daeec2631
seogi_macbook <sgkang09@github.com>
2016-07-03 23:22:04 -0700

    Remove data class for Jv (possible memory leakage)

--- SimPEG/EM/Static/IP/ProblemIP.py
+++ SimPEG/EM/Static/IP/ProblemIP.py
@@ -45,7 +45,8 @@ class BaseIPProblem(BaseEMProblem):
 
         self.curModel = m
 
-        Jv = self.dataPair(self.survey) #same size as the data
+        # Jv = self.dataPair(self.survey) #same size as the data
+        Jv = []
 
         A = self.getA()
 
@@ -58,13 +59,16 @@ class BaseIPProblem(BaseEMProblem):
             for rx in src.rxList:
                 df_dmFun = getattr(f, '_%sDeriv'%rx.projField, None)
                 df_dm_v = df_dmFun(src, du_dm_v, v, adjoint=False)
-                Jv[src, rx] = rx.evalDeriv(src, self.mesh, f, df_dm_v)
+                # Jv[src, rx] = rx.evalDeriv(src, self.mesh, f, df_dm_v)
+                Jv.append(rx.evalDeriv(src, self.mesh, f, df_dm_v))
         # Conductivity (d u / d log sigma)
         if self._formulation is 'EB':
-            return -Utils.mkvc(Jv)
+            # return -Utils.mkvc(Jv)
+            return -np.hstack(Jv)
         # Conductivity (d u / d log rho)
         if self._formulation is 'HJ':
-            return Utils.mkvc(Jv)
+            # return Utils.mkvc(Jv)
+            return np.hstack(Jv)
 
     def Jtvec(self, m, v, f=None):
         if f is None:
