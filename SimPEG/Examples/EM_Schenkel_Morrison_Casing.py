bc236a7fccc34dc9fe3c1728d48053a4b6d5571c
Lindsey Heagy <lindseyheagy@gmail.com>
2016-07-30 20:38:08 -0700

    more cleanup of EM examples

--- SimPEG/Examples/EM_Schenkel_Morrison_Casing.py
+++ SimPEG/Examples/EM_Schenkel_Morrison_Casing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from SimPEG import Mesh, Utils
+from SimPEG import Mesh, Utils, SolverLU
 from SimPEG.EM import FDEM, Analytics, mu_0
 import time
 
@@ -92,7 +92,7 @@ def run(plotIt=True):
 
     # pad nicely to second cell size
     npadx1 = np.floor(np.log(csx2/csx1) / np.log(pfx1))
-    hx1a = Utils.meshTensor([(csx1, ncx1)]),
+    hx1a = Utils.meshTensor([(csx1, ncx1)])
     hx1b = Utils.meshTensor([(csx1, npadx1, pfx1)])
     dx1 = sum(hx1a)+sum(hx1b)
     dx1 = np.floor(dx1/csx2)
@@ -102,17 +102,19 @@ def run(plotIt=True):
     dx2 = 300.  # uniform mesh out to here
     ncx2 = np.ceil((dx2 - dx1)/csx2)
     npadx2 = 45
-    hx2a, = Utils.meshTensor([(csx2, ncx2)])
+    hx2a = Utils.meshTensor([(csx2, ncx2)])
     hx2b = Utils.meshTensor([(csx2, npadx2, pfx2)])
     hx = np.hstack([hx1a, hx1b, hx2a, hx2b])
 
     # z-direction
     csz = 0.05
     nza = 10
-    # cell size, number of core cells, number of padding cells in the x-direction
+    # cell size, number of core cells, number of padding cells in the
+    # x-direction
     ncz, npadzu, npadzd = np.int(np.ceil(np.diff(casing_z)[0]/csz))+10, 68, 68
     # vector of cell widths in the z-direction
-    hz = Utils.meshTensor([(csz, npadzd, -1.3), (csz, ncz), (csz, npadzu, 1.3)])
+    hz = Utils.meshTensor([(csz, npadzd, -1.3), (csz, ncz),
+                          (csz, npadzu, 1.3)])
 
     # Mesh
     mesh = Mesh.CylMesh([hx, 1., hz], [0., 0., -np.sum(hz[:npadzu+ncz-nza])])
@@ -130,26 +132,26 @@ def run(plotIt=True):
     sigWholespace = sigmaback*np.ones((mesh.nC))
 
     sigBack = sigWholespace.copy()
-    sigBack[mesh.gridCC[:,2] > 0.] = sigmaair
+    sigBack[mesh.gridCC[:, 2] > 0.] = sigmaair
 
     sigCasing = sigBack.copy()
-    iCasingZ = (mesh.gridCC[:,2] <= casing_z[1]) & (mesh.gridCC[:,2] >= casing_z[0])
-    iCasingX = (mesh.gridCC[:,0] >= casing_a) & (mesh.gridCC[:,0] <= casing_b)
+    iCasingZ = ((mesh.gridCC[:, 2] <= casing_z[1]) &
+                (mesh.gridCC[:, 2] >= casing_z[0]))
+    iCasingX = ((mesh.gridCC[:, 0] >= casing_a) &
+                (mesh.gridCC[:, 0] <= casing_b))
     iCasing = iCasingX & iCasingZ
     sigCasing[iCasing] = sigmacasing
 
-
     if plotIt is True:
-
         # plotting parameters
         xlim = np.r_[0., 0.2]
         zlim = np.r_[-350., 10.]
-        clim_sig = np.r_[-8,6]
+        clim_sig = np.r_[-8, 6]
 
         # plot models
-        fig, ax = plt.subplots(1,1,figsize=(4,4))
+        fig, ax = plt.subplots(1, 1, figsize=(4, 4))
 
-        f = plt.colorbar(mesh.plotImage(np.log10(sigCasing),ax=ax)[0], ax=ax)
+        f = plt.colorbar(mesh.plotImage(np.log10(sigCasing), ax=ax)[0], ax=ax)
         ax.grid(which='both')
         ax.set_title('Log_10 (Sigma)')
         ax.set_xlim(xlim)
@@ -158,58 +160,66 @@ def run(plotIt=True):
 
         plt.show()
 
-
     # -------------- Sources --------------------
     # Define Custom Current Sources
 
     # surface source
-    sg_x = np.zeros(mesh.vnF[0],dtype=complex)
-    sg_y = np.zeros(mesh.vnF[1],dtype=complex)
-    sg_z = np.zeros(mesh.vnF[2],dtype=complex)
+    sg_x = np.zeros(mesh.vnF[0], dtype=complex)
+    sg_y = np.zeros(mesh.vnF[1], dtype=complex)
+    sg_z = np.zeros(mesh.vnF[2], dtype=complex)
 
-    nza = 2 # put the wire two cells above the surface
+    nza = 2  # put the wire two cells above the surface
     ncin = 2
 
     # vertically directed wire
-    sgv_indx = (mesh.gridFz[:,0] > casing_a) & (mesh.gridFz[:,0] < casing_a + csx1)  # hook it up to casing at the surface
-    sgv_indz = (mesh.gridFz[:,2] <= +csz*nza) & (mesh.gridFz[:,2] >= -csz*2)
+    # hook it up to casing at the surface
+    sgv_indx = ((mesh.gridFz[:, 0] > casing_a) &
+                (mesh.gridFz[:, 0] < casing_a + csx1))
+    sgv_indz = ((mesh.gridFz[:, 2] <= +csz*nza) &
+                (mesh.gridFz[:, 2] >= -csz*2))
     sgv_ind = sgv_indx & sgv_indz
     sg_z[sgv_ind] = -1.
 
     # horizontally directed wire
-    sgh_indx = (mesh.gridFx[:,0] > casing_a)    & (mesh.gridFx[:,0] <= inf_loc[2])
-    sgh_indz = (mesh.gridFx[:,2] > csz*(nza-0.5)) & (mesh.gridFx[:,2] < csz*(nza+0.5))
+    sgh_indx = ((mesh.gridFx[:, 0] > casing_a) &
+                (mesh.gridFx[:, 0] <= inf_loc[2]))
+    sgh_indz = ((mesh.gridFx[:, 2] > csz*(nza-0.5)) &
+                (mesh.gridFx[:, 2] < csz*(nza+0.5)))
     sgh_ind = sgh_indx & sgh_indz
     sg_x[sgh_ind] = -1.
 
-    sgv2_indx = (mesh.gridFz[:,0] >= mesh.gridFx[sgh_ind,0].max()) & (mesh.gridFz[:,0] <= inf_loc[2]*1.2)  # hook it up to casing at the surface
-    sgv2_indz = (mesh.gridFz[:,2] <= +csz*nza) & (mesh.gridFz[:,2] >= -csz*2)
+    # hook it up to casing at the surface
+    sgv2_indx = ((mesh.gridFz[:, 0] >= mesh.gridFx[sgh_ind, 0].max()) &
+                 (mesh.gridFz[:, 0] <= inf_loc[2]*1.2))
+    sgv2_indz = ((mesh.gridFz[:, 2] <= +csz*nza) &
+                 (mesh.gridFz[:, 2] >= -csz*2))
     sgv2_ind = sgv2_indx & sgv2_indz
     sg_z[sgv2_ind] = 1.
 
     # assemble the source
-    sg = np.hstack([sg_x,sg_y,sg_z])
-    sg_p = [FDEM.Src.RawVec_e([],_,sg/mesh.area) for _ in freqs]
+    sg = np.hstack([sg_x, sg_y, sg_z])
+    sg_p = [FDEM.Src.RawVec_e([], _, sg/mesh.area) for _ in freqs]
 
     # downhole source
-    dg_x = np.zeros(mesh.vnF[0],dtype=complex)
-    dg_y = np.zeros(mesh.vnF[1],dtype=complex)
-    dg_z = np.zeros(mesh.vnF[2],dtype=complex)
+    dg_x = np.zeros(mesh.vnF[0], dtype=complex)
+    dg_y = np.zeros(mesh.vnF[1], dtype=complex)
+    dg_z = np.zeros(mesh.vnF[2], dtype=complex)
 
     # vertically directed wire
-    dgv_indx = (mesh.gridFz[:,0] < csx1)  # go through the center of the well
-    dgv_indz = (mesh.gridFz[:,2] <= +csz*nza) & (mesh.gridFz[:,2] > dsz + csz/2.)
+    dgv_indx = (mesh.gridFz[:, 0] < csx1)  # go through the center of the well
+    dgv_indz = ((mesh.gridFz[:, 2] <= +csz*nza) &
+                (mesh.gridFz[:, 2] > dsz + csz/2.))
     dgv_ind = dgv_indx & dgv_indz
     dg_z[dgv_ind] = -1.
 
     # couple to the casing downhole
-    dgh_indx = mesh.gridFx[:,0] < casing_a + csx1
-    dgh_indz = (mesh.gridFx[:,2] < dsz + csz)  & (mesh.gridFx[:,2] >= dsz)
+    dgh_indx = mesh.gridFx[:, 0] < casing_a + csx1
+    dgh_indz = (mesh.gridFx[:, 2] < dsz + csz)  & (mesh.gridFx[:, 2] >= dsz)
     dgh_ind = dgh_indx & dgh_indz
     dg_x[dgh_ind] = 1.
 
     # horizontal part at surface
-    dgh2_indx = mesh.gridFx[:,0] <= inf_loc[2]*1.2
+    dgh2_indx = mesh.gridFx[:, 0] <= inf_loc[2]*1.2
     dgh2_indz = sgh_indz.copy()
     dgh2_ind = dgh2_indx & dgh2_indz
     dg_x[dgh2_ind] = -1.
@@ -219,8 +229,8 @@ def run(plotIt=True):
     dg_z[dgv2_ind] = 1.
 
     # assemble the source
-    dg = np.hstack([dg_x,dg_y,dg_z])
-    dg_p = [FDEM.Src.RawVec_e([],_,dg/mesh.area) for _ in freqs]
+    dg = np.hstack([dg_x, dg_y, dg_z])
+    dg_p = [FDEM.Src.RawVec_e([], _, dg/mesh.area) for _ in freqs]
 
     # ------------ Problem and Survey ---------------
     survey = FDEM.Survey(sg_p + dg_p)
@@ -236,19 +246,20 @@ def run(plotIt=True):
     # Plot current
 
     # current density
-    jn0 = fieldsCasing[dg_p,'j']
-    jn1 = fieldsCasing[sg_p,'j']
+    jn0 = fieldsCasing[dg_p, 'j']
+    jn1 = fieldsCasing[sg_p, 'j']
 
     # current
-    in0 = [mesh.area*fieldsCasing[dg_p,'j'][:,i] for i in range(len(freqs))]
-    in1 = [mesh.area*fieldsCasing[sg_p,'j'][:,i] for i in range(len(freqs))]
+    in0 = [mesh.area*fieldsCasing[dg_p, 'j'][:, i] for i in range(len(freqs))]
+    in1 = [mesh.area*fieldsCasing[sg_p, 'j'][:, i] for i in range(len(freqs))]
 
     in0 = np.vstack(in0).T
     in1 = np.vstack(in1).T
 
     # integrate to get z-current inside casing
-    inds_inx = (mesh.gridFz[:,0] >= casing_a) & (mesh.gridFz[:,0] <= casing_b)
-    inds_inz = (mesh.gridFz[:,2] >= dsz )     & (mesh.gridFz[:,2] <= 0)
+    inds_inx = ((mesh.gridFz[:, 0] >= casing_a) &
+                (mesh.gridFz[:, 0] <= casing_b))
+    inds_inz = (mesh.gridFz[:, 2] >= dsz )     & (mesh.gridFz[:, 2] <= 0)
     inds_fz = inds_inx & inds_inz
 
     indsx = [False]*mesh.nFx
@@ -256,25 +267,25 @@ def run(plotIt=True):
 
     in0_in = in0[np.r_[inds]]
     in1_in = in1[np.r_[inds]]
-    z_in = mesh.gridFz[inds_fz,2]
+    z_in = mesh.gridFz[inds_fz, 2]
 
-    in0_in = in0_in.reshape([in0_in.shape[0]/3,3])
-    in1_in = in1_in.reshape([in1_in.shape[0]/3,3])
-    z_in = z_in.reshape([z_in.shape[0]/3,3])
+    in0_in = in0_in.reshape([in0_in.shape[0]/3, 3])
+    in1_in = in1_in.reshape([in1_in.shape[0]/3, 3])
+    z_in = z_in.reshape([z_in.shape[0]/3, 3])
 
     I0 = in0_in.sum(1).real
     I1 = in1_in.sum(1).real
-    z_in = z_in[:,0]
+    z_in = z_in[:, 0]
 
     if plotIt is True:
-        fig, ax = plt.subplots(1,2,figsize=(12,4))
+        fig, ax = plt.subplots(1, 2, figsize=(12, 4))
 
-        ax[0].plot(z_in,np.absolute(I0), z_in,np.absolute(I1))
-        ax[0].legend(['top casing', 'bottom casing'],loc='best')
+        ax[0].plot(z_in, np.absolute(I0), z_in, np.absolute(I1))
+        ax[0].legend(['top casing', 'bottom casing'], loc='best')
         ax[0].set_title('Magnitude of Vertical Current in Casing')
 
-        ax[1].semilogy(z_in,np.absolute(I0), z_in,np.absolute(I1))
-        ax[1].legend(['top casing', 'bottom casing'],loc='best')
+        ax[1].semilogy(z_in, np.absolute(I0), z_in, np.absolute(I1))
+        ax[1].legend(['top casing', 'bottom casing'], loc='best')
         ax[1].set_title('Magnitude of Vertical Current in Casing')
         ax[1].set_ylim([1e-2, 1.])
 
@@ -282,4 +293,3 @@ def run(plotIt=True):
 
 if __name__ == '__main__':
     run()
-
