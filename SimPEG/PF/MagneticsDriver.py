401a8872c91c05a607ad5aa6e0c30ba246b7033a
D Fournier <fourndo@gmail.com>
2016-07-31 11:00:02 -0700

    Review and improve Magnetic problem and driver.

--- SimPEG/PF/MagneticsDriver.py
+++ SimPEG/PF/MagneticsDriver.py
@@ -40,115 +40,120 @@ class MagneticsDriver_Inv(object):
 
         fid = open(self.basePath + input_file,'r')
 
-        # Line 1
+        # Line 1: Mesh
         line = fid.readline()
-        l_input  = line.split('!')
-        mshfile = l_input[0].rstrip()
+        l_input  = re.split('[!\s]',line)
+        mshfile = l_input[1].rstrip()
 
-        # Line 2
+        # Line 2: Observation file
         line = fid.readline()
-        l_input  = line.split('!')
-        obsfile = l_input[0].rstrip()
+        l_input  = re.split('[!\s]',line)
+        obsfile = l_input[1].rstrip()
 
-        # Line 3
+        # Line 3: Topo, active-dyn, active-static
+        topofile = None
+        staticInput = None
+
         line = fid.readline()
         l_input = re.split('[!\s]',line)
-        if l_input=='null':
-            topofile = []
+        if l_input[0]=='TOPO':
+            topofile = l_input[1].rstrip()
 
-        else:
-            topofile = l_input[0].rstrip()
+        elif l_input[0]=='VALUE':
+            staticInput = float(l_input[1])
 
+        elif l_input[0]=='FILE':
+            staticInput = l_input[1].rstrip()
 
-        # Line 4
+        # Line 4: Starting model
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
             mstart = float(l_input[1])
 
-        else:
-            mstart = l_input[0].rstrip()
+        elif l_input[0]=='FILE':
+            mstart = l_input[1].rstrip()
 
-        # Line 5
+        # Line 5: Reference model
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
             mref = float(l_input[1])
 
-        else:
-            mref = l_input[0].rstrip()
+        elif l_input[0]=='FILE':
+            mref = l_input[1].rstrip()
 
-        # Line 6
+        # Line 6: Magnetization model
+        line = fid.readline()
+        l_input = re.split('[!\s]',line)
+        if l_input[0]=='DEFAULT':
+            magfile = None
+
+        elif l_input[0]=='FILE':
+            magfile = l_input[1].rstrip()
+
+        # Line 7: Cell weights
+        line = fid.readline()
+        l_input = re.split('[!\s]',line)
+        if l_input[0]=='DEFAULT':
+            wgtfile = []
+
+        elif l_input[0]=='FILE':
+            wgtfile = l_input[1].rstrip()
+
+        # Line 8: Target chi-factor
+        line = fid.readline()
+        l_input = re.split('[!\s]',line)
+        if l_input[0]=='DEFAULT':
+            chi = 1.
+
+        elif l_input[0]=='VALUE':
+            chi = float(l_input[1])
+
+        # Line 9: Alpha values
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
-            staticInput = float(l_input[1])
+
+            val = np.array(l_input[1:5])
+            alphas = val.astype(np.float)
 
         elif l_input[0]=='DEFAULT':
-            staticInput = None
 
-        else:
-            staticInput = l_input[0].rstrip()
+            alphas = np.ones(4)
 
-
-        # Line 7
-        line = fid.readline()
-        l_input = re.split('[!\s]',line)
-        if l_input=='DEFAULT':
-            magfile = []
-
-        else:
-            magfile = l_input[0].rstrip()
-
-        # Line 8
-        line = fid.readline()
-        l_input = re.split('[!\s]',line)
-        if l_input=='DEFAULT':
-            wgtfile = []
-
-        else:
-            wgtfile = l_input[0].rstrip()
-
-        # Line 9
-        line = fid.readline()
-        l_input = re.split('[!\s]',line)
-        chi = float(l_input[0])
-
-        # Line 10
-        line = fid.readline()
-        l_input = re.split('[!\s]',line)
-        val = np.array(l_input[0:4])
-        alphas = val.astype(np.float)
-
-        # Line 11
+        # Line 10: Bounds
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
             val   = np.array(l_input[1:3])
             bounds = val.astype(np.float)
 
-        else:
-            bounds = l_input[0].rstrip()
+        elif l_input[0]=='FILE':
+            bounds = l_input[1].rstrip()
 
-        # Line 12
+        else:
+            bounds = [-np.inf,np.inf]
+
+        # Line 11: Norms
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
             val   = np.array(l_input[1:6])
             lpnorms = val.astype(np.float)
 
-        else:
-            lpnorms = l_input[0].rstrip()
+        elif l_input[0]=='FILE':
+            lpnorms = l_input[1].rstrip()
 
-        # Line 13
+        # Line 12: Treshold values
         line = fid.readline()
         l_input = re.split('[!\s]',line)
         if l_input[0]=='VALUE':
             val   = np.array(l_input[1:3])
             eps = val.astype(np.float)
 
-        else:
-            eps = [None,None]
+        elif l_input[0]=='DEFAULT':
+            eps = None
 
         self.mshfile  = mshfile
         self.obsfile  = obsfile
@@ -167,6 +172,7 @@ class MagneticsDriver_Inv(object):
     @property
     def mesh(self):
         if getattr(self, '_mesh', None) is None:
+            print self.basePath + self.mshfile
             self._mesh = Mesh.TensorMesh.readUBC(self.basePath + self.mshfile)
         return self._mesh
 
@@ -179,14 +185,23 @@ class MagneticsDriver_Inv(object):
     @property
     def activeCells(self):
         if getattr(self, '_activeCells', None) is None:
-            if self.topofile == 'null':
-                self._activeCells = np.arange(self.mesh.nC)
-            else:
+            if getattr(self, 'topofile', None) is not None:
                 topo = np.genfromtxt(self.basePath + self.topofile, skip_header=1)
                 # Find the active cells
                 active = Utils.surface2ind_topo(self.mesh,topo,'N')
-                inds = np.asarray([inds for inds, elem in enumerate(active, 1) if elem], dtype = int) - 1
-                self._activeCells = inds
+
+            elif isinstance(self._staticInput, float):
+                active = self.m0 != self._staticInput
+
+            else:
+                # Read from file active cells with 0:air, 1:dynamic, -1 static
+                active = self.activeModel != 0
+
+            inds = np.asarray([inds for inds, elem in enumerate(active, 1) if elem], dtype = int) - 1
+            self._activeCells = inds
+
+            # Reduce m0 to active space
+            self._m0 = self.m0[self._activeCells]
 
         return self._activeCells
 
@@ -194,23 +209,11 @@ class MagneticsDriver_Inv(object):
     def staticCells(self):
         if getattr(self, '_staticCells', None) is None:
 
-            if getattr(self, '_staticInput', None) is None:
-                # All cells are dynamic: 1's
-                self._dynamicCells = np.arange(len(self.m0))
-                self._staticCells = []
+            # Cells with value 1 in active model are dynamic
+            staticCells = self.activeModel[self._activeCells] == -1
 
-            # Cells with specific value are static: 0's
-            else:
-                if isinstance(self._staticInput, float):
-                    staticCells = self.m0 == self._staticInput
-
-                else:
-                    # Read from file active cells with 0:air, 1:dynamic, -1 static
-                    staticCells = Mesh.TensorMesh.readModelUBC(self.mesh, self.basePath + self._staticInput)
-                    staticCells = staticCells[self.activeCells] == -1
-
-                inds = np.asarray([inds for inds, elem in enumerate(staticCells, 1) if elem], dtype = int) - 1
-                self._staticCells = inds
+            inds = np.asarray([inds for inds, elem in enumerate(staticCells, 1) if elem], dtype = int) - 1
+            self._staticCells = inds
 
         return self._staticCells
 
@@ -218,22 +221,11 @@ class MagneticsDriver_Inv(object):
     def dynamicCells(self):
         if getattr(self, '_dynamicCells', None) is None:
 
-            if getattr(self, '_staticInput', None) is None:
-                # All cells are dynamic: 1's
-                self._dynamicCells = np.arange(len(self.m0))
+            # Cells with value 1 in active model are dynamic
+            dynamicCells = self.activeModel[self._activeCells] == 1
 
-            # Cells with specific value are static: 0's
-            else:
-                if isinstance(self._staticInput, float):
-                    dynamicCells = self.m0 != self._staticInput
-
-                else:
-                    # Read from file active cells with 0:air, 1:dynamic, -1 static
-                    dynamicCells = Mesh.TensorMesh.readModelUBC(self.mesh, self.basePath + self._staticInput)
-                    dynamicCells = dynamicCells[self.activeCells] == 1
-
-                inds = np.asarray([inds for inds, elem in enumerate(dynamicCells, 1) if elem], dtype = int) - 1
-                self._dynamicCells = inds
+            inds = np.asarray([inds for inds, elem in enumerate(dynamicCells, 1) if elem], dtype = int) - 1
+            self._dynamicCells = inds
 
         return self._dynamicCells
 
@@ -250,7 +242,6 @@ class MagneticsDriver_Inv(object):
                 self._m0 = np.ones(self.nC) * self.mstart
             else:
                 self._m0 = Mesh.TensorMesh.readModelUBC(self.mesh,self.basePath + self.mstart)
-                self._m0 = self._m0[self.activeCells]
 
         return self._m0
 
@@ -261,9 +252,23 @@ class MagneticsDriver_Inv(object):
                 self._mref = np.ones(self.nC) * self._mrefInput
             else:
                 self._mref = Mesh.TensorMesh.readModelUBC(self.mesh,self.basePath + self._mrefInput)
-                self._mref = self._mref[self.activeCells]
+
+                #Reduce to active space
+                self._mref = self._mref[self._activeCells]
+
         return self._mref
 
+    @property
+    def activeModel(self):
+        if getattr(self, '_activeModel', None) is None:
+            if self._staticInput=='FILE':
+                # Read from file active cells with 0:air, 1:dynamic, -1 static
+                self._activeModel = Mesh.TensorMesh.readModelUBC(self.mesh, self.basePath + self._staticInput)
+
+            else:
+                self._activeModel = np.ones(self._mesh.nC)
+
+        return self._activeModel
 
     @property
     def magnetizationModel(self):
@@ -271,7 +276,7 @@ class MagneticsDriver_Inv(object):
             magnetization vector
         """
 
-        if self.magfile == 'DEFAULT':
+        if getattr(self, 'magfile', None) is None:
             return Magnetics.dipazm_2_xyz(np.ones(self.nC) * self.survey.srcField.param[1], np.ones(self.nC) * self.survey.srcField.param[2])
 
         else:
@@ -311,7 +316,7 @@ class MagneticsDriver_Inv(object):
 
         d  = np.zeros(ndat, dtype=float)
         wd = np.zeros(ndat, dtype=float)
-        locXYZ = np.zeros( (ndat,3), dtype=float)
+        locXYZ = np.zeros( (ndat[0],3), dtype=float)
 
         for ii in range(ndat):
 
