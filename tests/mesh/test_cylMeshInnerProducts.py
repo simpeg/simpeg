7ca6534563cd90751804795fd134e1e501fb17bf
Lindsey <lindseyheagy@gmail.com>
2016-08-03 15:09:21 -0700

    also test edge inner product, and show ratio

--- tests/mesh/test_cylMeshInnerProducts.py
+++ tests/mesh/test_cylMeshInnerProducts.py
@@ -18,8 +18,8 @@ class CylInnerProducts_Test(unittest.TestCase):
 
         # Create an isotropic sigma vector
         Sig = sympy.Matrix([
-            [100/sympy.pi*(r*z)**2, 0       ],
-            [     0  , 100/sympy.pi*(r*z)**2],
+            [420/(sympy.pi)*(r*z)**2, 0       ],
+            [     0  , 420/(sympy.pi)*(r*z)**2],
         ])
 
         # Do the inner product! - we are in cyl coordinates!
@@ -27,7 +27,7 @@ class CylInnerProducts_Test(unittest.TestCase):
         ans  = sympy.integrate(
                                sympy.integrate(
                                                sympy.integrate(r * jTSj, (r,0,1)), # we are in cyl coordinates
-                                               (t,0,2.*sympy.pi)),
+                                               (t,0,2*sympy.pi)),
                                 (z,0,1))[0] # The `[0]` is to make it an int.
 
         def get_vectors(mesh):
@@ -53,7 +53,54 @@ class CylInnerProducts_Test(unittest.TestCase):
         numeric_ans = jv.T.dot(MfSig.dot(jv))
 
         print('------ Testing Face Inner Product-----------')
-        print(' Analytic: {analytic}, Numeric: {numeric}'.format(analytic=ans, numeric=numeric_ans))
+        print(' Analytic: {analytic}, Numeric: {numeric}, ratio (num/ana): {ratio}'.format(
+               analytic=ans, numeric=numeric_ans, ratio=float(numeric_ans)/ans))
+        assert(np.abs(ans-numeric_ans) < TOL)
+
+
+    def test_EdgeInnerProduct(self):
+        # Here we will make up some j vectors that vary in space
+        # j = [j_r, j_z] - to test face inner products
+        h = sympy.Matrix([
+            r**2 * z,
+        ])
+
+        # Create an isotropic sigma vector
+        Sig = sympy.Matrix([
+            [200/(sympy.pi)*(r*z)**2 ]
+        ])
+
+        # Do the inner product! - we are in cyl coordinates!
+        hTSh = h.T*Sig*h
+        ans  = sympy.integrate(
+                               sympy.integrate(
+                                               sympy.integrate(r * hTSh, (r,0,1)), # we are in cyl coordinates
+                                               (t,0,2*sympy.pi)),
+                                (z,0,1))[0] # The `[0]` is to make it an int.
+
+        def get_vectors(mesh):
+            """ Get Vectors sig, sr. jx from sympy"""
+
+            f_h = sympy.lambdify((r,z), h[0], 'numpy')
+            f_sig = sympy.lambdify((r,z), Sig[0], 'numpy')
+
+            ht = f_h(mesh.gridEy[:,0], mesh.gridEy[:,2])
+            sig = f_sig(mesh.gridCC[:,0], mesh.gridCC[:,2])
+
+            return sig, np.r_[ht]
+
+
+        n = 100.
+        mesh = Mesh.CylMesh([n, 1, n])
+
+        sig, hv = get_vectors(mesh)
+        MeSig = mesh.getEdgeInnerProduct(sig)
+
+        numeric_ans = hv.T.dot(MeSig.dot(hv))
+
+        print('------ Testing Edge Inner Product-----------')
+        print(' Analytic: {analytic}, Numeric: {numeric}, ratio (num/ana): {ratio}'.format(
+               analytic=ans, numeric=numeric_ans, ratio=float(numeric_ans)/ans))
         assert(np.abs(ans-numeric_ans) < TOL)
 
 
