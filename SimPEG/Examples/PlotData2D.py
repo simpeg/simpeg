af6ea660e4a3f90f74f87cc6a347db737809b2e8
seogi_macbook <sgkang09@github.com>
2016-08-25 08:31:28 -0700

    remove fig = plt.figure()

--- SimPEG/Examples/PlotData2D.py
+++ SimPEG/Examples/PlotData2D.py
@@ -1,7 +1,7 @@
 from SimPEG import EM, np, Utils
-import matplotlib.pyplot as plt
 
-def run(plotIt = False):
+
+def run(plotIt = True):
     """
 
         Plotting 2D data
@@ -29,28 +29,30 @@ def run(plotIt = False):
     # Use analytic fuction to compute Ex, Ey, Ez
     Ex, Ey, Ez = EM.Analytics.E_from_ElectricDipoleWholeSpace(xyz, srcLoc, sig, f)
 
-    fig = plt.figure()
-    ax1 = plt.subplot(121)
-    ax2 = plt.subplot(122)
-    # Plot Real Ex (scalar)
-    cont1, ax1 = Utils.plot2Ddata(xyz, Ex.real, dataloc=True, ax=ax1)
-
-    # Make it as (ndata,2) matrix
-    E = np.c_[Ex, Ey]
-    # Plot Real E (vector)
-    cont2, ax2 = Utils.plot2Ddata(xyz, E.real, vec=True, ax=ax2)
-    cb1 = plt.colorbar(cont1, ax=ax1, orientation="horizontal")
-    cb2 = plt.colorbar(cont2, ax=ax2, orientation="horizontal")
-    ax1.set_xlabel("x")
-    ax1.set_ylabel("y")
-    ax2.set_xlabel("x")
-    ax2.set_ylabel("y")
-
-    ax1.set_aspect('equal', adjustable='box')
-    ax2.set_aspect('equal', adjustable='box')
-
     if plotIt:
+        import matplotlib.pyplot as plt
+        plt.figure()
+        ax1 = plt.subplot(121)
+        ax2 = plt.subplot(122)
+        # Plot Real Ex (scalar)
+        cont1, ax1 = Utils.plot2Ddata(xyz, Ex.real, dataloc=True,
+                                      ax=ax1, contourOpts={"cmap": "viridis"})
+        # Make it as (ndata,2) matrix
+        E = np.c_[Ex, Ey]
+        # Plot Real E (vector)
+        cont2, ax2 = Utils.plot2Ddata(xyz, E.real, vec=True,
+                                      ax=ax2, contourOpts={"cmap": "viridis"})
+        cb1 = plt.colorbar(cont1, ax=ax1, orientation="horizontal")
+        cb2 = plt.colorbar(cont2, ax=ax2, orientation="horizontal")
+        ax1.set_xlabel("x")
+        ax1.set_ylabel("y")
+        ax2.set_xlabel("x")
+        ax2.set_ylabel("y")
+
+        ax1.set_aspect('equal', adjustable='box')
+        ax2.set_aspect('equal', adjustable='box')
         plt.show()
+
 
 
 if __name__ == '__main__':
