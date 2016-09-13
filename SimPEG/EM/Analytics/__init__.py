1d9504e0c9b8f6fcc0f886b3ea60844ddff7d89d
Michael Mitchell <micmitch44@gmail.com>
2016-09-12 17:29:51 -0700

    update function name in __init__.py

--- SimPEG/EM/Analytics/__init__.py
+++ SimPEG/EM/Analytics/__init__.py
@@ -1,5 +1,5 @@
 from TDEM import hzAnalyticDipoleT
 from FDEM import hzAnalyticDipoleF
 from FDEMcasing import *
-from DC import DCAnalyticHalf, DCAnalyticSphere
+from DC import DCAnalytic_PoleTx, DCAnalyticSphere
 from FDEMDipolarfields import *
