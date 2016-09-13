fixed derivatives caluculation

SimPEG/NSEM/RxNSEM.py
@@ -233,7 +233,7 @@ class Point_impedance3D(Point_NSEM):
 
 
 
-    def eval(self, src, mesh, f):
+    def eval(self, src, mesh, f, return_full=False):
         '''
         Project the fields to natural source data.
 
@@ -255,9 +255,12 @@ class Point_impedance3D(Point_NSEM):
         elif 'yy' in self.orientation:
             Zij = (-self.ey_px * self.hx_py + self.ey_py * self.hx_px)
         # Calculate the complex value
-        f_part_complex = self.Hd * Zij
+        eval_complex = self.Hd * Zij
 
-        return getattr(f_part_complex, self.component)
+        # Return the full impedance
+        if return_full:
+            return eval_complex
+        return getattr(eval_complex, self.component)
 
     def evalDeriv(self, src, mesh, f, v, adjoint=False):
         """
@@ -357,7 +360,7 @@ class Point_impedance3D(Point_NSEM):
                     self.sDiag(self.hx_px) * self.ey_py_u(v)
                 )
 
-            Zij = self.eval(src, self.mesh, self.f)
+            Zij = self.eval(src, self.mesh, self.f, True)
             # Calculate the complex derivative
             PDeriv_complex = self.Hd * (ZijN_uV - self.sDiag(Zij) * self.Hd_uV(v) )
             Pv = SimPEG.np.array(getattr(PDeriv_complex, self.component))

tests/em/nsem/inversion/test_Problem3D_Adjoint.py
@@ -24,7 +24,7 @@ def JvecAdjointTest(inputSetup,comp='All',freq=False):
 
     m  = sig
     u = problem.fields(m)
-
+    np.random.seed(1983)
     v = np.random.rand(survey.nD,)
     # print problem.PropMap.PropModel.nP
     w = np.random.rand(problem.mesh.nC,)

tests/em/nsem/inversion/test_Problem3D_Derivs.py
@@ -7,7 +7,7 @@ from SimPEG import NSEM
 from SimPEG.Utils import meshTensor
 from scipy.constants import mu_0
 
-np.random.seed(1983)
+
 
 TOLr = 5e-2
 TOL = 1e-4
@@ -44,7 +44,7 @@ def DerivProjfieldsTest(inputSetup,comp='All',freq=False):
     # problem.mapping = simpeg.Maps.ExpMap(problem.mesh)
     # Initate things for the derivs Test
     src = survey.srcList[0]
-
+    np.random.seed(1983)
     u0x = np.random.randn(survey.mesh.nE)+np.random.randn(survey.mesh.nE)*1j
     u0y = np.random.randn(survey.mesh.nE)+np.random.randn(survey.mesh.nE)*1j
     u0 = np.vstack((simpeg.mkvc(u0x,2),simpeg.mkvc(u0y,2)))