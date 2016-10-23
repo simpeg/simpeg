dc14aaff9912737a53078ca6a40b438699e108e2
Rowan Cockett <rowanc1@gmail.com>
2016-10-23 12:56:02 -0600

    Mesh_Operators_CahnHilliard

--- SimPEG/Examples/Mesh_Operators_CahnHilliard.py
+++ SimPEG/Examples/Mesh_Operators_CahnHilliard.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
-from SimPEG import *
+from SimPEG import Mesh, Utils, Solver
+import numpy as np
+
 
 def run(plotIt=True, n=60):
     """
@@ -7,9 +9,11 @@ def run(plotIt=True, n=60):
         ==============================
 
         This example is based on the example in the FiPy_ library.
-        Please see their documentation for more information about the Cahn-Hilliard equation.
+        Please see their documentation for more information about the
+        Cahn-Hilliard equation.
 
-        The "Cahn-Hilliard" equation separates a field \\\\( \\\\phi \\\\) into 0 and 1 with smooth transitions.
+        The "Cahn-Hilliard" equation separates a field \\\\( \\\\phi \\\\)
+        into 0 and 1 with smooth transitions.
 
         .. math::
 
@@ -32,10 +36,12 @@ def run(plotIt=True, n=60):
             \\frac{\partial f}{\partial \phi} = (a^2/2)2\phi(1-\phi)(1-2\phi)
             \\frac{\partial^2 f}{\partial \phi^2} = (a^2/2)2[1-6\phi(1-\phi)]
 
-        The implementation below uses backwards Euler in time with an exponentially increasing time step.
-        The initial \\\\( \\\\phi \\\\) is a normally distributed field with a standard deviation of 0.1 and mean of 0.5.
-        The grid is 60x60 and takes a few seconds to solve ~130 times. The results are seen below, and you can see the
-        field separating as the time increases.
+        The implementation below uses backwards Euler in time with an
+        exponentially increasing time step. The initial \\\\( \\\\phi \\\\)
+        is a normally distributed field with a standard deviation of 0.1 and
+        mean of 0.5. The grid is 60x60 and takes a few seconds to solve ~130
+        times. The results are seen below, and you can see the field separating
+        as the time increases.
 
         .. _FiPy: http://www.ctcms.nist.gov/fipy/examples/cahnHilliard/generated/examples.cahnHilliard.mesh2DCoupled.html
 
@@ -55,8 +61,8 @@ def run(plotIt=True, n=60):
     # (dt*A*d2fdphi2 - I) * phi_ =  dt*A*d2fdphi2*phi + dt*A*L*phi_ - phi - dt*A*dfdphi
     # (dt*A*d2fdphi2 - I - dt*A*L) * phi_ =  (dt*A*d2fdphi2 - I)*phi - dt*A*dfdphi
 
-    h = [(0.25,n)]
-    M = Mesh.TensorMesh([h,h])
+    h = [(0.25, n)]
+    M = Mesh.TensorMesh([h, h])
 
     # Constants
     D = a = epsilon = 1.
@@ -69,16 +75,16 @@ def run(plotIt=True, n=60):
     duration = 75
     elapsed = 0.
     dexp = -5
-    phi = np.random.normal(loc=0.5,scale=0.01,size=M.nC)
+    phi = np.random.normal(loc=0.5, scale=0.01, size=M.nC)
     ii, jj = 0, 0
     PHIS = []
-    capture = np.logspace(-1,np.log10(duration),8)
+    capture = np.logspace(-1, np.log10(duration), 8)
     while elapsed < duration:
         dt = min(100, np.exp(dexp))
         elapsed += dt
         dexp += 0.05
 
-        dfdphi   = a**2 * 2 * phi * (1 - phi) * (1 - 2 * phi)
+        dfdphi = a**2 * 2 * phi * (1 - phi) * (1 - 2 * phi)
         d2fdphi2 = Utils.sdiag(a**2 * 2 * (1 - 6 * phi * (1 - phi)))
 
         MAT = (dt*A*d2fdphi2 - I - dt*A*L)
@@ -88,16 +94,17 @@ def run(plotIt=True, n=60):
         if elapsed > capture[jj]:
             PHIS += [(elapsed, phi.copy())]
             jj += 1
-        if ii % 10 == 0: print(ii, elapsed)
+        if ii % 10 == 0:
+            print(ii, elapsed)
         ii += 1
 
     if plotIt:
         import matplotlib.pyplot as plt
-        fig, axes = plt.subplots(2,4,figsize=(14,6))
+        fig, axes = plt.subplots(2, 4, figsize=(14, 6))
         axes = np.array(axes).flatten().tolist()
-        for ii, ax in zip(np.linspace(0,len(PHIS)-1,len(axes)),axes):
+        for ii, ax in zip(np.linspace(0, len(PHIS)-1, len(axes)), axes):
             ii = int(ii)
-            out = M.plotImage(PHIS[ii][1],ax=ax)
+            M.plotImage(PHIS[ii][1], ax=ax)
             ax.axis('off')
             ax.set_title('Elapsed Time: {0:4.1f}'.format(PHIS[ii][0]))
         plt.show()
