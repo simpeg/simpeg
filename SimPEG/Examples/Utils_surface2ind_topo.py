cf03c23a4e149f77f0729bf7bcfbc03f5f5b901d
Rowan Cockett <rowanc1@gmail.com>
2016-10-23 13:10:16 -0600

    Utils_surface2ind_topo

--- SimPEG/Examples/Utils_surface2ind_topo.py
+++ SimPEG/Examples/Utils_surface2ind_topo.py
@@ -1,4 +1,6 @@
-from SimPEG import *
+import numpy as np
+from SimPEG import Mesh
+from SimPEG import Utils
 from SimPEG.Utils import surface2ind_topo
 
 
@@ -8,26 +10,35 @@ def run(plotIt=True, nx=5, ny=5):
         Utils: surface2ind_topo
         =======================
 
-        Here we show how to use :code:`Utils.surface2ind_topo` to identify cells below
-        a topographic surface.
+        Here we show how to use :code:`Utils.surface2ind_topo` to identify
+        cells below a topographic surface.
 
     """
 
-    mesh = Mesh.TensorMesh([nx,ny], x0='CC') # 2D mesh
-    xtopo = np.linspace(mesh.gridN[:,0].min(), mesh.gridN[:,0].max())
-    topo = 0.4*np.sin(xtopo*5) # define a topographic surface
+    # 2D mesh
+    mesh = Mesh.TensorMesh([nx, ny], x0='CC')
+    xtopo = np.linspace(mesh.gridN[:, 0].min(), mesh.gridN[:, 0].max())
 
-    Topo = np.hstack([Utils.mkvc(xtopo,2), Utils.mkvc(topo,2)]) #make it an array
+    # define a topographic surface
+    topo = 0.4*np.sin(xtopo*5)
+
+    # make it an array
+    Topo = np.hstack([Utils.mkvc(xtopo, 2), Utils.mkvc(topo, 2)])
 
     indcc = surface2ind_topo(mesh, Topo, 'CC')
 
     if plotIt:
         from matplotlib.pylab import plt
         from scipy.interpolate import interp1d
-        fig, ax = plt.subplots(1,1, figsize=(6,6))
+        fig, ax = plt.subplots(1, 1, figsize=(6, 6))
         mesh.plotGrid(ax=ax, nodes=True, centers=True)
-        ax.plot(xtopo,topo,'k',linewidth=1)
-        ax.plot(mesh.vectorCCx, interp1d(xtopo,topo)(mesh.vectorCCx),'--k',linewidth=3)
+        ax.plot(xtopo, topo, 'k', linewidth=1)
+        ax.plot(
+            mesh.vectorCCx,
+            interp1d(xtopo, topo)(mesh.vectorCCx),
+            '--k',
+            linewidth=3
+        )
 
         aveN2CC = Utils.sdiag(mesh.aveN2CC.T.sum(1))*mesh.aveN2CC.T
         a = aveN2CC * indcc
@@ -35,7 +46,13 @@ def run(plotIt=True, nx=5, ny=5):
         a[a < 0.25] = np.nan
         a = a.reshape(mesh.vnN, order='F')
         masked_array = np.ma.array(a, mask=np.isnan(a))
-        ax.pcolor(mesh.vectorNx,mesh.vectorNy,masked_array.T, cmap=plt.cm.gray, alpha=0.2)
+        ax.pcolor(
+            mesh.vectorNx,
+            mesh.vectorNy,
+            masked_array.T,
+            cmap=plt.cm.gray,
+            alpha=0.2
+        )
         plt.show()
 
 
