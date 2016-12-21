f1f70a5808777cb3bdea8f74192d35439636cc66
seogi_macbook <sgkang09@github.com>
2016-12-20 19:44:41 -0800

    Incorporate Lindsey's comments

--- SimPEG/EM/Analytics/__init__.py
+++ SimPEG/EM/Analytics/__init__.py
@@ -1,5 +1,5 @@
 from .TDEM import hzAnalyticDipoleT, hzAnalyticCentLoopT
 from .FDEM import hzAnalyticDipoleF
 from .FDEMcasing import *
-from .DC import DCAnalytic_Dipole_Dipole, DCAnalytic_Pole_Dipole, DCAnalytic_Pole_Pole, DCAnalyticSphere
+from .DC import DCAnalytic_Dipole_Dipole, DCAnalytic_Pole_Dipole, DCAnalytic_Pole_Pole, DCAnalyticSphere, DCAnalytic_Dipole_Pole
 from .FDEMDipolarfields import *
--- SimPEG/EM/Static/Utils/__init__.py
+++ SimPEG/EM/Static/Utils/__init__.py
@@ -1,2 +1,4 @@
-from .StaticUtils import *
-from .PlotUtils import *
+from .StaticUtils import plot_pseudoSection, gen_DCIPsurvey,
+writeUBC_DCobs, convertObs_DC3D_to_2D, readUBC_DC2DModel,
+readUBC_DC2Dpre, readUBC_DC3Dobs, xy_2_lineID,
+r_unit, getSrc_locs
