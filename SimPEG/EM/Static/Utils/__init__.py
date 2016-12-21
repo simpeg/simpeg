9117eba81d4bec57d54490923b5857f0dd92fa1b
seogi_macbook <sgkang09@github.com>
2016-12-20 19:55:27 -0800

    revert back import *

--- SimPEG/EM/Static/Utils/__init__.py
+++ SimPEG/EM/Static/Utils/__init__.py
@@ -1,4 +1,1 @@
-from .StaticUtils import plot_pseudoSection, gen_DCIPsurvey,
-writeUBC_DCobs, convertObs_DC3D_to_2D, readUBC_DC2DModel,
-readUBC_DC2Dpre, readUBC_DC3Dobs, xy_2_lineID,
-r_unit, getSrc_locs
+from .StaticUtils import *
