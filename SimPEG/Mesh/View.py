494899a093b053091e9753fea03159cfc7c19582
Lindsey Heagy <lindseyheagy@gmail.com>
2016-12-27 23:27:26 -0700

    add a mirror option for plotting on the cyl mesh

--- SimPEG/Mesh/View.py
+++ SimPEG/Mesh/View.py
@@ -513,7 +513,38 @@ class CylView(object):
         # Hackity Hack:
         # Just create a TM and use its view.
         from SimPEG.Mesh import TensorMesh
-        M = TensorMesh([self.hx, self.hz], x0=[self.x0[0], self.x0[2]])
+
+        # if mirroring about the x-axis
+
+        # v, vType='CC',
+        #           normal='Z', ind=None, grid=False, view='real',
+
+        mirror = kwargs.pop('mirror', None)
+        if mirror is True:
+            if kwargs.get('vType', None) is not None:
+                if kwargs.get('vType', None) != 'CC':
+                    raise NotImplementedError(
+                        'Mirroring has not yet been implemented for non-cell '
+                        'centered values'
+                    )
+
+            # create a mirrored mesh
+            hx = np.hstack([np.flipud(self.hx), self.hx])
+            x00 = self.x0[0] - self.hx.sum()
+            M = TensorMesh([hx, self.hz], x0=[x00, self.x0[2]])
+
+            # mirror the data
+            if len(args) > 0:
+                # if len(args) > 1:
+                #     raise NotImplementedError(
+                #         'More than one input argument is not supported when '
+                #         'mirroring'
+                #     )
+                tmp = args[0].reshape(self.vnC[0], self.vnC[2], order='F')
+                tmp = mkvc(np.vstack([np.flipud(tmp), tmp]))
+                args = (tmp,) + args[1:]
+        else:
+            M = TensorMesh([self.hx, self.hz], x0=[self.x0[0], self.x0[2]])
 
         ax = kwargs.get('ax', None)
         if ax is None:
@@ -533,7 +564,8 @@ class CylView(object):
         ax.set_xlabel('x')
         ax.set_ylabel('z')
 
-        if showIt: plt.show()
+        if showIt:
+            plt.show()
 
         return out
 
