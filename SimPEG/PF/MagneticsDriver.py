1f56ecab002c16124870e9b62fdca9ec29ac7a27
Lindsey Heagy <lindseyheagy@gmail.com>
2017-02-13 10:35:21 -0800

    ensure we are using ints in the MagDriver

--- SimPEG/PF/MagneticsDriver.py
+++ SimPEG/PF/MagneticsDriver.py
@@ -352,7 +352,7 @@ class MagneticsDriver_Inv(object):
 
         # Third line has the number of rows
         line = fid.readline()
-        ndat = np.array(line.split(), dtype=int)
+        ndat = int(line.strip())
 
         # Pre-allocate space for obsx, obsy, obsz, data, uncert
         line = fid.readline()
@@ -360,7 +360,7 @@ class MagneticsDriver_Inv(object):
 
         d = np.zeros(ndat, dtype=float)
         wd = np.zeros(ndat, dtype=float)
-        locXYZ = np.zeros((ndat[0], 3), dtype=float)
+        locXYZ = np.zeros((ndat, 3), dtype=float)
 
         for ii in range(ndat):
 
