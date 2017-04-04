Fix forward test for changes in Gravity. Add silent switch to remove progress update in tests

SimPEG/PF/Magnetics.py
@@ -27,6 +27,7 @@ class MagneticIntegral(Problem.LinearProblem):
     recType = 'tmi'  #: Receiver type either "tmi" | "xyz"
     magType = 'H0'
     equiSourceLayer = False
+    silent = False  # Don't display progress on screen
 
     def __init__(self, mesh, **kwargs):
         Problem.BaseProblem.__init__(self, mesh, **kwargs)
@@ -192,8 +193,8 @@ class MagneticIntegral(Problem.LinearProblem):
 
             F = np.empty((ndata, Mxyz.shape[1]), dtype=np.float32)
 
-            # Loop through all observations and create forward operator (nD-by-nC)
-            print("Begin calculation of forward operator: " + magType)
+        # Loop through all observations and create forward operator (nD-by-nC)
+        print("Begin forward: M=" + magType + ", Rx type= " + recType)
 
         # Add counter to dsiplay progress. Good for large problems
         count = -1
@@ -246,8 +247,9 @@ class MagneticIntegral(Problem.LinearProblem):
                 #     elif self.recType == 'z':
                 #         F[ii, :] = tz * survey.srcField.param[0]
 
-            # Display progress
-            count = progress(ii, count, ndata)
+            if not self.silent:
+                # Display progress
+                count = progress(ii, count, ndata)
 
         print("Done 100% ...forward operator completed!!\n")
 
@@ -260,8 +262,9 @@ class MagneticVector(MagneticIntegral):
     actInd = None  #: Active cell indices provided
     M = None  #: magType matrix provided, otherwise all induced
     ptype = 'Cartesian'  # Formulation either "Cartesian" | "Spherical"
-    magType = 'full' # magType component
+    magType = 'full'  # magType component
     chi = None
+    silent = False  # Don't display progress on screen
 
     def __init__(self, mesh, **kwargs):
         Problem.BaseProblem.__init__(self, mesh, **kwargs)
@@ -392,6 +395,7 @@ class MagneticAmplitude(MagneticIntegral):
     M = None  #: magType matrix provided, otherwise all induced
     magType = 'H0'  #: Receivers must be "xyz"
     chi = None
+    silent = False  # Don't display progress on screen
 
     def __init__(self, mesh, **kwargs):
         Problem.BaseProblem.__init__(self, mesh, **kwargs)

examples/05-mag/plot_Amplitude_Inversion.py
@@ -109,7 +109,7 @@ def run(plotIt=True):
 
     # Create the forward problem (forwardOnly)
     prob = PF.Magnetics.MagneticIntegral(mesh, chiMap=idenMap, actInd=actv,
-                                         M=M)
+                                         M=M, silent=True)
 
     # Pair the survey and problem
     survey.pair(prob)
@@ -177,7 +177,7 @@ def run(plotIt=True):
 
     # Create MAG equivalent layer problem
     prob = PF.Magnetics.MagneticIntegral(mesh, chiMap=idenMap, actInd=surf,
-                                         equiSourceLayer=True)
+                                         equiSourceLayer=True, silent=True)
     prob.solverOpts['accuracyTol'] = 1e-4
 
     # Pair the survey and problem
@@ -251,7 +251,7 @@ def run(plotIt=True):
 
     # Create the forward model operator
     prob = PF.Magnetics.MagneticAmplitude(mesh, chiMap=idenMap,
-                                          actInd=actv)
+                                          actInd=actv, silent=True)
 
     # Define starting model
     mstart = np.ones(len(actv))*1e-4

tests/pf/test_forward_Grav_Linear.py
@@ -41,30 +41,34 @@ class GravFwdProblemTests(unittest.TestCase):
         srcField = PF.BaseGrav.SrcField([rxLoc])
         self.survey = PF.BaseGrav.LinearSurvey(srcField)
 
-        self.prob_xyz = PF.Gravity.GravityIntegral(mesh, rhoMap=idenMap,
-                                                   actInd=sph_ind,
-                                                   forwardOnly=True,
-                                                   rtype='xyz')
+        self.prob_x = PF.Gravity.GravityIntegral(mesh, rhoMap=idenMap,
+                                                 actInd=sph_ind,
+                                                 forwardOnly=True,
+                                                 rType='x', silent=True)
+
+        self.prob_y = PF.Gravity.GravityIntegral(mesh, rhoMap=idenMap,
+                                                 actInd=sph_ind,
+                                                 forwardOnly=True,
+                                                 rType='y', silent=True)
 
         self.prob_z = PF.Gravity.GravityIntegral(mesh, rhoMap=idenMap,
                                                  actInd=sph_ind,
                                                  forwardOnly=True,
-                                                 rtype='z')
+                                                 rType='z', silent=True)
 
     def test_ana_forward(self):
 
         # Compute 3-component grav data
-        self.survey.pair(self.prob_xyz)
-        d = self.prob_xyz.fields(self.model)
+        self.survey.pair(self.prob_x)
+        dgx = self.prob_x.fields(self.model)
+
+        self.survey.pair(self.prob_y)
+        dgy = self.prob_y.fields(self.model)
+
+        self.survey.pair(self.prob_z)
+        dgz = self.prob_z.fields(self.model)
 
         ndata = self.locXyz.shape[0]
-        dgx = d[0:ndata]
-        dgy = d[ndata:2*ndata]
-        dgz = d[2*ndata:]
-
-        # Compute gz data only
-        self.survey.pair(self.prob_z)
-        dz = self.prob_z.fields(self.model)
 
         # Compute analytical response from mass sphere
         AnaSphere = PF.GravAnalytics.GravSphereFreeSpace(self.locXyz[:, 0],
@@ -74,17 +78,16 @@ class GravFwdProblemTests(unittest.TestCase):
                                                          self.rho)
 
         # Compute residual
-        err_xyz = (np.linalg.norm(d-np.r_[AnaSphere['gx'],
-                                          AnaSphere['gy'],
-                                          AnaSphere['gz']]) /
-                   np.linalg.norm(np.r_[AnaSphere['gx'],
-                                        AnaSphere['gy'],
-                                        AnaSphere['gz']]))
+        err_x = (np.linalg.norm(dgx-AnaSphere['gx']) /
+                 np.linalg.norm(AnaSphere['gx']))
 
-        err_tmi = (np.linalg.norm(dz-AnaSphere['gz']) /
-                   np.linalg.norm(AnaSphere['gz']))
+        err_y = (np.linalg.norm(dgy-AnaSphere['gy']) /
+                 np.linalg.norm(AnaSphere['gy']))
 
-        self.assertTrue(err_xyz < 0.005 and err_tmi < 0.005)
+        err_z = (np.linalg.norm(dgz-AnaSphere['gz']) /
+                 np.linalg.norm(AnaSphere['gz']))
+
+        self.assertTrue(err_x < 0.005 and err_y < 0.005 and err_z < 0.005)
 
 
 if __name__ == '__main__':