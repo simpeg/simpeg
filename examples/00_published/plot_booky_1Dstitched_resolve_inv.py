002332c267b3de98bdd428728b5a130a06d89e72
Lindsey Heagy <lindseyheagy@gmail.com>
2017-06-12 13:00:21 -0700

    update plot so it is the same as that shown in the paper

--- examples/00_published/plot_booky_1Dstitched_resolve_inv.py
+++ examples/00_published/plot_booky_1Dstitched_resolve_inv.py
@@ -1,15 +1,19 @@
 import numpy as np
 import cPickle as pickle
+import matplotlib.pyplot as plt
+from pymatsolver import PardisoSolver
+from scipy.constants import mu_0
+import h5py
+import tarfile
+import os
+import shutil
+from scipy.spatial import cKDTree
+
 from SimPEG import (
     Mesh, Maps, Utils, DataMisfit, Regularization,
     Optimization, Inversion, InvProblem, Directives
 )
 import SimPEG.EM as EM
-import matplotlib.pyplot as plt
-from pymatsolver import PardisoSolver
-from scipy.constants import mu_0
-import h5py
-from scipy.spatial import cKDTree
 
 """
 Heagy et al., 2017 1D RESOLVE Bookpurnong Inversion
@@ -30,6 +34,27 @@ The paper is available at:
 https://arxiv.org/abs/1610.00804
 
 """
+
+
+def download_and_unzip_data(
+    url = "https://storage.googleapis.com/simpeg/bookpurnong/bookpurnong_inversion.tar.gz"
+):
+    """
+    Download the data from the storage bucket, unzip the tar file, return
+    the directory where the data are
+    """
+    # download the data
+    downloads = Utils.download(url)
+
+    # directory where the downloaded files are
+    directory = downloads.split(".")[0]
+
+    # unzip the tarfile
+    tar = tarfile.open(downloads, "r")
+    tar.extractall()
+    tar.close()
+
+    return downloads, directory
 
 
 def resolve_1Dinversions(
@@ -113,18 +138,38 @@ def resolve_1Dinversions(
     return mopt, invProb.dpred, survey.dobs
 
 
-def run(runIt=False, plotIt=True, saveFig=False):
+def run(runIt=False, plotIt=True, saveIt=False, saveFig=False, cleanup=True):
+    """
+    Run the bookpurnong 1D stitched RESOLVE inversions.
+
+    :param bool runIt: re-run the inversions? Default downloads and plots saved results
+    :param bool plotIt: show the plots?
+    :param bool saveIt: save the re-inverted results?
+    :param bool saveFig: save the figure
+    :param bool cleanup: remove the downloaded results
+    """
+
+    # download the data
+    downloads, directory = download_and_unzip_data()
+
     # Load resolve data
-    resolve = h5py.File("./downloads/booky_resolve.hdf5", "r")
+    resolve = h5py.File(
+        os.path.sep.join([directory, "booky_resolve.hdf5"]),
+        "r"
+    )
+
     river_path = resolve["river_path"].value    # River path
     nSounding = resolve["data"].shape[0]    # the # of soundings
+
     # Bird height from surface
     b_height_resolve = resolve["src_elevation"].value
 
+    # fetch the frequencies we are considering
     cpi_inds = [0, 2, 6, 8, 10]  # Indices for HCP in-phase
     cpq_inds = [1, 3, 7, 9, 11]  # Indices for HCP quadrature
     frequency_cp = resolve["frequency_cp"].value
 
+    # build a mesh
     cs, ncx, ncz, npad = 1., 10., 10., 20
     hx = [(cs, ncx), (cs, npad, 1.3)]
     npad = 12
@@ -133,171 +178,186 @@ def run(runIt=False, plotIt=True, saveFig=False):
     hz = np.r_[temp_pad[::-1], temp[::-1], temp, temp_pad]
     mesh = Mesh.CylMesh([hx, 1, hz], '00C')
     active = mesh.vectorCCz < 0.
-    rxOffset = 7.86
-    bp = -mu_0/(4*np.pi*rxOffset**3)
 
+    # survey parameters
+    rxOffset = 7.86  # tx-rx separation
+    bp = -mu_0/(4*np.pi*rxOffset**3)  # primary magnetic field
+
+    # re-run the inversion
     if runIt:
 
+        # set up the mappings - we are inverting for 1D log conductivity
+        # below the earth's surface.
         actMap = Maps.InjectActiveCells(
             mesh, active, np.log(1e-8), nC=mesh.nCz
         )
         mapping = Maps.ExpMap(mesh) * Maps.SurjectVertical1D(mesh) * actMap
+
+        # build starting and reference model
         sig_half = 1e-1
         sig_air = 1e-8
         sigma = np.ones(mesh.nCz)*sig_air
         sigma[active] = sig_half
-        m0 = np.log(1e-1) * np.ones(active.sum())
-        mref = np.log(1e-1) * np.ones(active.sum())
+        m0 = np.log(1e-1) * np.ones(active.sum())  # starting model
+        mref = np.log(1e-1) * np.ones(active.sum())  # reference model
 
-        mopt_re = []
-        dpred_re = []
-        dobs_re = []
+        # initalize empty lists for storing inversion results
+        mopt_re = []  # recovered model
+        dpred_re = []  # predicted data
+        dobs_re = []  # observed data
 
+        # downsample the data for the inversion
         nskip = 40
-        std = np.repeat(np.r_[np.ones(3)*0.1, np.ones(2)*0.15], 2)
-        floor = abs(20 * bp * 1e-6)
 
+        # set up a noise model
+        # 10% for the 3 lowest frequencies, 15% for the two highest
+        std = np.repeat(np.r_[np.ones(3)*0.1, np.ones(2)*0.15], 2)
+        floor = abs(20 * bp * 1e-6)   # floor of 20ppm
+
+        # loop over the soundings and invert each
         for rxind in range(nSounding):
+
+            # convert data from ppm to magnetic field (A/m^2)
             dobs = np.c_[
                 resolve["data"][rxind, :][cpi_inds].astype(float),
                 resolve["data"][rxind, :][cpq_inds].astype(float)
             ].flatten() * bp * 1e-6
+
+            # perform the inversion
             src_height = b_height_resolve[rxind].astype(float)
             mopt, dpred, dobs = resolve_1Dinversions(
                 mesh, dobs, src_height, frequency_cp, m0, mref, mapping,
                 std=std, floor=floor
-                )
+            )
 
+            # add results to our list
             mopt_re.append(mopt)
             dpred_re.append(dpred)
             dobs_re.append(dobs)
 
+        # save results
         mopt_re = np.vstack(mopt_re)
         dpred_re = np.vstack(dpred_re)
         dobs_re = np.vstack(dobs_re)
 
-        np.save("mopt_re_final", mopt_re)
-        np.save("dobs_re_final", dobs_re)
-        np.save("dpred_re_final", dpred_re)
+        if saveIt:
+            np.save("mopt_re_final", mopt_re)
+            np.save("dobs_re_final", dobs_re)
+            np.save("dpred_re_final", dpred_re)
 
-    if plotIt:
 
-        mopt_re = resolve["mopt"].value
-        dobs_re = resolve["dobs"].value
-        dpred_re = resolve["dpred"].value
+    mopt_re = resolve["mopt"].value
+    dobs_re = resolve["dobs"].value
+    dpred_re = resolve["dpred"].value
 
-        sigma = np.exp(mopt_re)
-        indz = -7
-        cmap = "jet"
+    sigma = np.exp(mopt_re)
+    indz = -7  # depth index
 
-        # dummy figure for colobar
-        fig = plt.figure()
-        out = plt.scatter(
-            np.ones(3), np.ones(3), c=np.linspace(-2, 1, 3), cmap=cmap
-        )
-        plt.close(fig)
+    # so that we can visually compare with literature (eg Viezzoli, 2010)
+    cmap = "jet"
 
-        # plot recovered stitched conductivity model
-        fig = plt.figure(figsize=(5, 5))
-        ax = plt.subplot(111)
-        temp = sigma[:, indz]
-        tree = cKDTree(zip(resolve["xy"][:, 0], resolve["xy"][:, 1]))
-        d, d_inds = tree.query(
-            zip(resolve["xy"][:, 0], resolve["xy"][:, 1]), k=20
-        )
-        w = 1. / (d+100.)**2.
-        w = Utils.sdiag(1./np.sum(w, axis=1)) * (w)
-        xy = resolve["xy"]
-        temp = (temp.flatten()[d_inds] * w).sum(axis=1)
-        Utils.plot2Ddata(
-            xy, temp, ncontour=100, scale="log", dataloc=False,
-            contourOpts={"cmap": cmap, "vmin": -2, "vmax": 1.}, ax=ax
-        )
-        ax.plot(
-            resolve["xy"][:, 0], resolve["xy"][:, 1], 'k.', alpha=0.02, ms=1
-        )
-        ax.set_title(
-            ("%.1f m below surface") % (-mesh.vectorCCz[active][indz])
-        )
-        ax.set_xlabel("Easting (m)")
-        ax.set_ylabel("Northing (m)")
+    # dummy figure for colobar
+    fig = plt.figure()
+    out = plt.scatter(
+        np.ones(3), np.ones(3), c=np.linspace(-2, 1, 3), cmap=cmap
+    )
+    plt.close(fig)
+
+    # plot from the paper
+    fs = 13  # fontsize
+    # matplotlib.rcParams['font.size'] = fs
+    plt.figure(figsize=(13, 7))
+    ax0 = plt.subplot2grid((2, 3), (0, 0), rowspan=2, colspan=2)
+    ax1 = plt.subplot2grid((2, 3), (0, 2) )
+    ax2 = plt.subplot2grid((2, 3), (1, 2))
+
+    # titles of plots
+    title = [
+        ("(a) Recovered model, %.1f m depth")%(-mesh.vectorCCz[active][indz]),
+        "(b) Obs (Real 400 Hz)", "(c) Pred (Real 400 Hz)"
+    ]
+
+    temp = sigma[:, indz]
+    tree = cKDTree(zip(resolve["xy"][:, 0], resolve["xy"][:, 1]))
+    d, d_inds = tree.query(
+        zip(resolve["xy"][:, 0], resolve["xy"][:, 1]), k=20
+    )
+    w = 1. / (d+100.)**2.
+    w = Utils.sdiag(1./np.sum(w, axis=1)) * (w)
+    xy = resolve["xy"]
+    temp = (temp.flatten()[d_inds] * w).sum(axis=1)
+    Utils.plot2Ddata(
+        xy, temp, ncontour=100, scale="log", dataloc=False,
+        contourOpts={"cmap": cmap, "vmin": -2, "vmax": 1.}, ax=ax0
+    )
+    ax0.plot(
+        resolve["xy"][:, 0], resolve["xy"][:, 1], 'k.', alpha=0.02, ms=1
+    )
+
+    cb = plt.colorbar(
+        out, ax=ax0, ticks=np.linspace(-2, 1, 4), format="$10^{%.1f}$"
+    )
+    cb.set_ticklabels(["0.01", "0.1", "1", "10"])
+    cb.set_label("Conductivity (S/m)")
+    ax0.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
+
+    # plot observed and predicted data
+    freq_ind = 0
+    axs = [ax1, ax2]
+    temp_dobs = dobs_re[:, freq_ind].copy()
+    ax1.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
+    out = Utils.plot2Ddata(
+        resolve["xy"].value, temp_dobs/abs(bp)*1e6, ncontour=100,
+        scale="log", dataloc=False, ax=ax1, contourOpts={"cmap": "viridis"}
+    )
+    vmin, vmax = out[0].get_clim()
+    cb = plt.colorbar(out[0], ticks=np.linspace(vmin, vmax, 3), ax=ax1, format="%.1e", fraction=0.046, pad=0.04)
+    cb.set_label("Bz (ppm)")
+    temp_dpred = dpred_re[:, freq_ind].copy()
+    # temp_dpred[mask_:_data] = np.nan
+    ax2.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
+    Utils.plot2Ddata(
+        resolve["xy"].value, temp_dpred/abs(bp)*1e6, ncontour=100,
+        scale="log", dataloc=False,
+        contourOpts={"vmin": vmin, "vmax": vmax, "cmap": "viridis"}, ax=ax2
+    )
+    cb = plt.colorbar(
+        out[0], ticks=np.linspace(vmin, vmax, 3), ax=ax2,
+        format="%.1e", fraction=0.046, pad=0.04
+    )
+    cb.set_label("Bz (ppm)")
+
+    for i, ax in enumerate([ax0, ax1, ax2]):
         xticks = [460000, 463000]
         yticks = [6195000, 6198000, 6201000]
+        xloc, yloc = 462100.0, 6196500.0
         ax.set_xticks(xticks)
         ax.set_yticks(yticks)
-        ax.set_xticklabels([str(f) for f in xticks])
+        # ax.plot(xloc, yloc, 'wo')
+        ax.plot(river_path[:, 0], river_path[:, 1], 'k', lw=0.5)
+
+        ax.set_aspect("equal")
+        ax.plot(
+            resolve["xy"][:, 0], resolve["xy"][:, 1], 'k.', alpha=0.02,
+            ms=1
+        )
+
         ax.set_yticklabels([str(f) for f in yticks])
-        cb = plt.colorbar(
-            out, ax=ax, ticks=np.linspace(-2, 1, 4), format="$10^{%.1f}$"
-        )
-        cb.set_ticklabels(["0.01", "0.1", "1", "10"])
-        cb.set_label("Conductivity (S/m)")
-        ax.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
-        plt.tight_layout()
+        ax.set_ylabel("Northing (m)")
+        ax.set_xlabel("Easting (m)")
+        ax.set_title(title[i])
+
+    plt.tight_layout()
+
+    if plotIt:
         plt.show()
 
-        if saveFig is True:
-            fig.savefig("resolve_cond_9_9m.png", dpi=200)
+    if saveFig is True:
+        fig.savefig("obspred_resolve.png", dpi=200)
 
-        # plot observed and predicted data
-        freq_ind = 0
-        fig = plt.figure(figsize=(11*0.8, 6*0.8))
-        title = ["Observed (In-phase 400 Hz)", "Predicted (In-phase 400 Hz)"]
-        ax1 = plt.subplot(121)
-        ax2 = plt.subplot(122)
-        axs = [ax1, ax2]
-        temp_dobs = dobs_re[:, freq_ind].copy()
-        # temp_dobs[mask_:_data] = np.nan
-        ax.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
-        out = Utils.plot2Ddata(
-            resolve["xy"].value, temp_dobs/abs(bp)*1e6, ncontour=100,
-            scale="log", dataloc=False, ax=ax1, contourOpts={"cmap": "viridis"}
-        )
-        vmin, vmax = out[0].get_clim()
-        # cb = plt.colorbar(out[0], ticks=np.linspace(vmin, vmax, 3), ax=ax1, format="%.1e", fraction=0.046, pad=0.04)
-        # cb.set_label("Bz (ppm)")
-        temp_dpred = dpred_re[:, freq_ind].copy()
-        # temp_dpred[mask_:_data] = np.nan
-        ax.plot(river_path[:, 0], river_path[:, 1], 'k-', lw=0.5)
-        Utils.plot2Ddata(
-            resolve["xy"].value, temp_dpred/abs(bp)*1e6, ncontour=100,
-            scale="log", dataloc=False,
-            contourOpts={"vmin": vmin, "vmax": vmax, "cmap": "viridis"}, ax=ax2
-        )
-        cb = plt.colorbar(
-            out[0], ticks=np.linspace(vmin, vmax, 3), ax=ax2,
-            format="%.1e", fraction=0.046, pad=0.04
-        )
-        cb.set_label("Bz (ppm)")
-
-        for i, ax in enumerate(axs):
-            xticks = [460000, 463000]
-            yticks = [6195000, 6198000, 6201000]
-            xloc, yloc = 462100.0, 6196500.0
-            ax.set_xticks(xticks)
-            ax.set_yticks(yticks)
-            ax.plot(xloc, yloc, 'wo')
-            ax.plot(river_path[:, 0], river_path[:, 1], 'k', lw=0.5)
-
-            ax.set_aspect("equal")
-            ax.plot(
-                resolve["xy"][:, 0], resolve["xy"][:, 1], 'k.', alpha=0.02,
-                ms=1
-            )
-
-            if i == 1:
-                ax.set_yticklabels([str(" ") for f in yticks])
-            else:
-                ax.set_yticklabels([str(f) for f in yticks])
-                ax.set_ylabel("Northing (m)")
-            ax.set_xlabel("Easting (m)")
-            ax.set_title(title[i])
-
-        plt.tight_layout()
-        plt.show()
-
-        if saveFig is True:
-            fig.savefig("obspred_resolve.png", dpi=200)
+    if cleanup:
+        os.remove(downloads)
+        shutil.rmtree(directory)
 
 if __name__ == '__main__':
-    run(runIt=False, plotIt=True, saveFig=True)
+    run(runIt=False, plotIt=True, saveIt=True, saveFig=False, cleanup=True)
