cc8806d0e0215b6dc6ca724c10fd419e4108f8cb
fourndo <fourndo@gmail.com>
2017-10-28 11:37:38 -0700

    Add optional switch for bfgs in InvProblem

--- SimPEG/InvProblem.py
+++ SimPEG/InvProblem.py
@@ -32,6 +32,9 @@ class BaseInvProblem(Props.BaseSimPEG):
     #: Optimization program
     opt = None
 
+    #: Use BFGS
+    bfgs = True
+
     #: List of strings, e.g. ['_MeSigma', '_MeSigmaI']
     deleteTheseOnModelUpdate = []
 
@@ -91,28 +94,28 @@ class BaseInvProblem(Props.BaseSimPEG):
 
         self.model = m0
 
-        if isinstance(self.dmisfit, DataMisfit.BaseDataMisfit):
-            print("""
-    SimPEG.InvProblem is setting bfgsH0 to the inverse of the eval2Deriv.
-    ***Done using same Solver and solverOpts as the problem***"""
-            )
-            self.opt.bfgsH0 = self.dmisfit.prob.Solver(
-                self.reg.deriv2(self.model), **self.dmisfit.prob.solverOpts
-            )
-        elif isinstance(self.dmisfit, ObjectiveFunction.BaseObjectiveFunction):
-            for objfct in self.dmisfit.objfcts:
-                if isinstance(objfct, DataMisfit.BaseDataMisfit):
-                    print("""
-    SimPEG.InvProblem is setting bfgsH0 to the inverse of the eval2Deriv.
-    ***Done using same Solver and solverOpts as the {} problem***""".format(
-                            objfct.prob.__class__.__name__
+        if self.bfgs:
+            if isinstance(self.dmisfit, DataMisfit.BaseDataMisfit):
+                print("""SimPEG.InvProblem is setting bfgsH0 to the inverse of the eval2Deriv.
+                      ***Done using same Solver and solverOpts as the problem***"""
+                      )
+                self.opt.bfgsH0 = self.dmisfit.prob.Solver(
+                    self.reg.deriv2(self.model), **self.dmisfit.prob.solverOpts
+                                                           )
+            elif isinstance(self.dmisfit,
+                            ObjectiveFunction.BaseObjectiveFunction
+                            ):
+                for objfct in self.dmisfit.objfcts:
+                    if isinstance(objfct, DataMisfit.BaseDataMisfit):
+                        print("""SimPEG.InvProblem is setting bfgsH0 to the inverse of the eval2Deriv.
+                              ***Done using same Solver and solverOpts as the {} problem***""".format(
+                                objfct.prob.__class__.__name__
+                            )
                         )
-                    )
-                    self.opt.bfgsH0 = objfct.prob.Solver(
-                        self.reg.deriv2(self.model), **objfct.prob.solverOpts
-                    )
-                    break
-
+                        self.opt.bfgsH0 = objfct.prob.Solver(
+                            self.reg.deriv2(self.model), **objfct.prob.solverOpts
+                        )
+                        break
 
     @property
     def warmstart(self):
