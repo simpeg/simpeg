4a7fd81ee4a8de8472200a78e1a178107ccfca6e
seogi_macbook <sgkang09@github.com>
2017-11-07 11:33:01 -0800

    minor change

--- tests/em/static/test_SIP_jvecjtvecadj.py
+++ tests/em/static/test_SIP_jvecjtvecadj.py
@@ -36,7 +36,7 @@ class IPProblemTestsCC(unittest.TestCase):
         tau[blkind1] = 0.01
 
         x = mesh.vectorCCx[(mesh.vectorCCx > -155.) & (mesh.vectorCCx < 155.)]
-        y = mesh.vectorCCx[(mesh.vectorCCy > -155.) & (mesh.vectorCCy < 155.)]
+        y = mesh.vectorCCy[(mesh.vectorCCy > -155.) & (mesh.vectorCCy < 155.)]
         Aloc = np.r_[-200., 0., 0.]
         Bloc = np.r_[200., 0., 0.]
         M = Utils.ndgrid(x-25., y, np.r_[0.])
