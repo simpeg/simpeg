d67db72a9eebc5f882dbd8615dd653a0a29904e5
seogi_macbook <sgkang09@github.com>
2017-12-08 17:31:58 -0800

    add comments for 2D DC inversion

--- examples/06-dc/plot_dipoledipole_2D.py
+++ examples/06-dc/plot_dipoledipole_2D.py
@@ -14,18 +14,26 @@ except ImportError:
 
 showIt = True
 
+# Initiate I/O class for DC
 IO = DC.IO()
+# Obtain ABMN locations
 A, B, M, N = IO.genLocs_2D('dipole-dipole', 0., 200., 10., 10)
+
+# Generate DC survey object
 survey = IO.fromABMN_to_survey(
     A, B, M, N, 'dipole-dipole', dataType='volt'
 )
+
+# Obtain 2D TensorMesh
 mesh, actind = IO.setMesh()
 
+# Build a conductivity model
 inds = Utils.ModelBuilder.getIndicesSphere(np.r_[100., -25.], 15., mesh.gridCC)
 sigma = np.ones(mesh.nC)*1e-2
 sigma[inds] = 0.1
 rho = 1./sigma
 
+# Show the true conductivity model
 if showIt:
     fig = plt.figure(figsize = (12, 3))
     ax = plt.subplot(111)
@@ -33,42 +41,54 @@ if showIt:
     plt.plot(IO.uniqElecLocs[:,0], IO.uniqElecLocs[:,1], 'k.')
     plt.show()
 
+# Use Exponential Map: m = log(rho)
 mapping = Maps.ExpMap(mesh)
-mtrue = np.ones(mesh.nC)*np.log(sigma)
+# Generate mtrue
+mtrue = np.ones(mesh.nC)*np.log(rho)
 
+# Generate 2.5D DC problem
+# "N" means potential is defined at nodes
 prb = DC.Problem2D_N(
-    mesh, sigmaMap=mapping, storeJ=True,
+    mesh, rhoMap=mapping, storeJ=True,
     Solver = PardisoSolver
 )
+# Pair problem with survey
 try:
     prb.pair(survey)
 except:
     survey.unpair()
     prb.pair(survey)
 
+# Make synthetic DC data with 5% Gaussian noise
 dtrue = survey.makeSyntheticData(mtrue, std=0.05)
+
+# Show apparent resisitivty pseudo-section
 if showIt:
     IO.plotPseudoSection(dobs=survey.dobs)
 
-m0 = np.ones(mesh.nC)*np.log(1./10**2.05)
-
+# Show apparent resisitivty histogram
 if showIt:
     fig = plt.figure()
     out = hist(survey.dobs/IO.G, bins=20)
     plt.show()
 
-m0 = np.ones(mesh.nC)*np.log(1./110.)
+# Set initial model based upon histogram
+m0 = np.ones(mesh.nC)*np.log(110.)
 
-eps = 10**-3.2
-std = 0.05
+# Set uncertainty
+eps = 10**-3.2 # floor
+std = 0.05     # percentage
 dmisfit = DataMisfit.l2_DataMisfit(survey)
 uncert = abs(survey.dobs) * std + eps
 dmisfit.W = 1./uncert
+
+# Map for a regularization
 regmap = Maps.IdentityMap(nP=int(actind.sum()))
+
+# Related to inversion
 reg = Regularization.Simple(mesh, indActive=actind, mapping=regmap)
 opt = Optimization.InexactGaussNewton(maxIter=15)
 invProb = InvProblem.BaseInvProblem(dmisfit, reg, opt)
-# Create an inversion object
 beta = Directives.BetaSchedule(coolingFactor=5, coolingRate=2)
 betaest = Directives.BetaEstimate_ByEig(beta0_ratio=1e0)
 target = Directives.TargetMisfit()
@@ -76,11 +96,17 @@ inv = Inversion.BaseInversion(invProb, directiveList=[beta, betaest, target])
 prb.counter = opt.counter = Utils.Counter()
 opt.LSshorten = 0.5
 opt.remember('xc')
+
+# Run inversion
 mopt = inv.run(m0)
 
-rho_est = 1./(mapping*mopt)
+# Convert obtained inversion model to resistivity
+# rho = M(m), where M(.) is a mapping
+
+rho_est = mapping*mopt
 rho_est[~actind] = np.nan
 
+# show recovered conductivity
 if showIt:
     vmin, vmax = rho.min(), rho.max()
     fig, ax = plt.subplots(figsize=(20, 3))
@@ -93,5 +119,6 @@ if showIt:
     ax.set_ylabel("Elevation (m)")
     plt.show()
 
+# show predicted data
 if showIt:
     IO.plotPseudoSection(dobs=invProb.dpred)
