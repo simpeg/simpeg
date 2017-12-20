2d20359770147dad78e26def1faeb281a0a24b04
seogi_macbook <sgkang09@github.com>
2017-12-20 15:10:18 -0800

    remove redundant part in   def deleteTheseOnModelUpdate

--- SimPEG/EM/Static/DC/ProblemDC_2D.py
+++ SimPEG/EM/Static/DC/ProblemDC_2D.py
@@ -258,9 +258,7 @@ class BaseDCProblem_2D(BaseEMProblem):
 
     @property
     def deleteTheseOnModelUpdate(self):
-        toDelete = []
-        if self.sigmaMap is not None or self.rhoMap is not None:
-            toDelete += ['_MeSigma', '_MeSigmaI', '_MfRho', '_MfRhoI']
+        toDelete = super(BaseDCProblem, self).deleteTheseOnModelUpdate
         if self.Jmat is not None:
             toDelete += ['Jmat']
         return toDelete
