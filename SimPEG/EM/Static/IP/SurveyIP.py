720fd668f5b7f0599c924d7a6a96aa99936f6bc9
seogi_macbook <sgkang09@github.com>
2017-12-20 23:33:25 -0800

    working on conversion from dc survey to ip survey

--- SimPEG/EM/Static/IP/SurveyIP.py
+++ SimPEG/EM/Static/IP/SurveyIP.py
@@ -6,6 +6,8 @@ from __future__ import unicode_literals
 from SimPEG.EM.Base import BaseEMSurvey
 from SimPEG.EM.Static.DC.SrcDC import BaseSrc
 from SimPEG.EM.Static.DC.RxDC import BaseRx
+import SimPEG.EM.Static.DC.RxDC as Rx
+import SimPEG.EM.Static.DC.SrcDC as Src
 from SimPEG.EM.Static.DC import Survey as SurveyDC
 
 
@@ -25,9 +27,27 @@ class Survey(SurveyDC):
         return self.prob.Jvec(m, m, f=f)
 
 
-def from_dc_to_ip_survey(dc_survey):
+def from_dc_to_ip_survey(dc_survey, flag=None):
     srcList = dc_survey.srcList
-    srcList_ip = []
-    for src in srcList:
-        srcList_ip.append(src_ip)
+    if flag == "2.5D":
+        ip_survey = Survey([srcList])
+    else:
+        srcList_ip = []
+        for src in srcList:
+            for rx in src.rxList:
+                if isinstance(rx, Rx.Pole):
+                    rx_ip = Rx.Pole(rxList_ip, locs=rx.locs)
+                elif isinstance(rx, Rx.Dipole):
+                    rx_ip = Rx.Dipole(rxList_ip, locs=rx.locs)
+                else:
+                    raise NotImplementedError()
+                rxList_ip.append(rx_ip)
+            if isinstance(src, Src.Pole):
+                src_ip = Src.Pole(rxList_ip, loc=src_ip.loc)
+            elif isinstance(src, Src.Dipole):
+                src_ip = Src.Dipole(rxList_ip)
+            else:
+                raise NotImplementedError()
+            srcList_ip.append(src_ip)
+        ip_survey = Survey(srcList_ip)
     return ip_survey
