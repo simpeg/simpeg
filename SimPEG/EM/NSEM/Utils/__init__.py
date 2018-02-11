ec9b1f0ebda55e3e02e597e10ac28d62286b922f
GudniRos <gkrosen@gmail.com>
2018-02-11 15:56:33 -0800

    Fix import issue due to name changes

--- SimPEG/EM/NSEM/Utils/__init__.py
+++ SimPEG/EM/NSEM/Utils/__init__.py
@@ -12,7 +12,7 @@ from .MT1Danalytic import getEHfields, getImpedance
 from .dataUtils import (getAppRes, appResPhs, rec_to_ndarr, rotateData,
                         skindepth, makeAnalyticSolution, plotMT1DModelData,
                         plotImpAppRes, printTime, convert3Dto1Dobject,
-                        resampleNSEMdataAtFreq, extract_data_info)
+                        resample_data, extract_data_info)
 from .ediFilesUtils import (EDIimporter, _findLatLong, _findLine, _findEDIcomp)
 from .testUtils import (getAppResPhs, setup1DSurvey, setupSimpegNSEM_ePrimSec,
                         random, halfSpace, blockInhalfSpace, twoLayer)
