8e3ef9395931eb4a1b7862487e5958239c1067d8
GudniRos <gkrosen@gmail.com>
2018-02-14 20:42:38 -0800

    Adding appResPhs to the __init__ imports to fix travis issue

--- SimPEG/EM/NSEM/Utils/__init__.py
+++ SimPEG/EM/NSEM/Utils/__init__.py
@@ -9,10 +9,11 @@ from __future__ import absolute_import
 
 from .MT1Dsolutions import get1DEfields  # Add the names of the functions
 from .MT1Danalytic import getEHfields, getImpedance
-from .dataUtils import (rec_to_ndarr, rotate_data,
+from .dataUtils import (appResPhs, rec_to_ndarr, rotate_data,
                         skindepth, makeAnalyticSolution, plotMT1DModelData,
                         plotImpAppRes, printTime, convert3Dto1Dobject,
                         resample_data, extract_data_info)
-from .ediFilesUtils import (EDIimporter, _findLatLong, _findLine, _findEDIcomp)
+from .ediFilesUtils import (EDIimporter,
+                            _findLatLong, _findLine, _findEDIcomp)
 from .testUtils import (getAppResPhs, setup1DSurvey, setupSimpegNSEM_ePrimSec,
                         random, halfSpace, blockInhalfSpace, twoLayer)
