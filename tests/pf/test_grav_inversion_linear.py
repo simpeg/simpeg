c0287e13bdcaeb3284620ee5a466256d2210c858
fourndo <fourndo@gmail.com>
2018-02-19 23:35:56 -0800

    Clean test grav

--- tests/pf/test_grav_inversion_linear.py
+++ tests/pf/test_grav_inversion_linear.py
@@ -106,6 +106,7 @@ class GravInvLinProblemTest(unittest.TestCase):
         reg = Regularization.Sparse(mesh, indActive=actv, mapping=idenMap)
         reg.cell_weights = wr
         reg.norms = [0, 0, 0, 0]
+
         # reg.eps_p, reg.eps_q = 5e-2, 1e-2
 
         # Data misfit function
@@ -127,108 +128,17 @@ class GravInvLinProblemTest(unittest.TestCase):
                                            directiveList=[IRLS,
                                                           update_Jacobi])
         self.mesh = mesh
+
     def test_mag_inverse(self):
 
         # Run the inversion
-        mesh= self.mesh
-        model = self.model
         mrec = self.inv.run(self.model)
+
         residual = np.linalg.norm(mrec-self.model) / np.linalg.norm(self.model)
         print(residual)
-        midx = int(self.mesh.nCx/2)
-        midy = int(self.mesh.nCy/2)
-        # self.assertTrue(residual < 0.05)
-        ypanel = midx
 
-        zpanel = -7
-        m_l2 = self.actvMap * self.inv.invProb.l2model
-        m_l2[m_l2 == -100] = np.nan
+        self.assertTrue(residual < 0.1)
 
-        m_lp = self.actvMap * mrec
-        m_lp[m_lp == -100] = np.nan
-
-        m_true = self.actvMap * model
-        m_true[m_true == -100] = np.nan
-
-        vmin, vmax = mrec.min(), mrec.max()
-
-        # Plot the data
-        # PF.Gravity.plot_obs_2D(rxLoc, d=data)
-
-        plt.figure()
-
-        # Plot L2 model
-        ax = plt.subplot(321)
-        mesh.plotSlice(m_l2, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan l2-model.')
-        plt.gca().set_aspect('equal')
-        plt.ylabel('y')
-        ax.xaxis.set_visible(False)
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertica section
-        ax = plt.subplot(322)
-        mesh.plotSlice(m_l2, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W l2-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Plot Lp model
-        ax = plt.subplot(323)
-        mesh.plotSlice(m_lp, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan lp-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('y')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertical section
-        ax = plt.subplot(324)
-        mesh.plotSlice(m_lp, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W lp-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Plot True model
-        ax = plt.subplot(325)
-        mesh.plotSlice(m_true, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan true model.')
-        plt.gca().set_aspect('equal')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertical section
-        ax = plt.subplot(326)
-        mesh.plotSlice(m_true, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(vmin, vmax))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W true model.')
-        plt.gca().set_aspect('equal')
-        plt.xlabel('x')
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
