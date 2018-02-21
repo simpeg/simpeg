fix bugs for gradient operator when computing streaming current (js) needed to set on neuman boundary condition on topographic cells.

SimPEG/EM/Static/SP/SrcSP.py
@@ -1,6 +1,7 @@
 from SimPEG.EM.Static.DC import Src
 from SimPEG import Props
 from SimPEG.Utils import sdiag
+from SimPEG import Utils
 import scipy.sparse as sp
 import numpy as np
 from SimPEG.EM.Static.DC import Survey
@@ -11,17 +12,30 @@ class StreamingCurrents(Src.BaseSrc):
     L = None
     mesh = None
     modelType = None
+    indActive = None
 
     def __init__(self, rxList, **kwargs):
         Src.BaseSrc.__init__(self, rxList, **kwargs)
-        if self.L is None:
-            raise Exception("SP source requires cross coupling coefficient L")
+        if self.modelType == "Head":
+            if self.L is None:
+                raise Exception(
+                    "SP source requires cross coupling coefficient L"
+                )
+        elif self.modelType == "CurrentDensity":
+            if self.indActive is None:
+                self.indActive = np.ones(self.mesh.nC, dtype=bool)
+            # This is for setting a Neuman condition on the topographic faces
+            V = Utils.sdiag(self.mesh.vol)
+            self.Grad = sp.vstack(
+                (
+                    self.Pafx * self.mesh.faceDivx.T * V * self.Pac,
+                    self.Pafy * self.mesh.faceDivy.T * V * self.Pac,
+                    self.Pafz * self.mesh.faceDivz.T * V * self.Pac,
+                )
+            )
+
         if self.mesh is None:
             raise Exception("SP source requires mesh")
-        # TODO: remove below when confirmed
-        # self.mesh.setCellGradBC("neumann")
-        # self.V = sp.block_diag([sdiag(self.mesh.vol)]*3)
-        # self.Div = -self.mesh.cellGrad.T
 
     def eval(self, prob):
         """
@@ -42,7 +56,7 @@ class StreamingCurrents(Src.BaseSrc):
             elif self.modelType == "CurrentSource":
                 q = prob.q
             elif self.modelType == "CurrentDensity":
-                q = prob.Grad.T*prob.mesh.aveCCV2F*np.r_[
+                q = self.Grad.T*self.mesh.aveCCV2F*np.r_[
                     prob.jsx, prob.jsy, prob.jsz
                 ]
             else:
@@ -64,7 +78,7 @@ class StreamingCurrents(Src.BaseSrc):
                     jsDeriv = sp.vstack(
                         (prob.jsxDeriv, prob.jsyDeriv, prob.jszDeriv)
                     )
-                    srcDeriv = jsDeriv.T * prob.mesh.aveCCV2F.T * (prob.Grad*v)
+                    srcDeriv = jsDeriv.T * self.mesh.aveCCV2F.T * (self.Grad*v)
                 else:
                     raise NotImplementedError()
             else:
@@ -76,7 +90,7 @@ class StreamingCurrents(Src.BaseSrc):
                     jsDeriv = sp.vstack(
                         (prob.jsxDeriv, prob.jsyDeriv, prob.jszDeriv)
                     )
-                    srcDeriv = prob.Grad.T * prob.mesh.aveCCV2F*(jsDeriv*v)
+                    srcDeriv = self.Grad.T * self.mesh.aveCCV2F*(jsDeriv*v)
                 else:
                     raise NotImplementedError()
         elif prob._formulation == 'EB':
@@ -100,6 +114,82 @@ class StreamingCurrents(Src.BaseSrc):
         if getattr(self, '_MfLiI', None) is None:
             self._MfLiI = self.mesh.getFaceInnerProduct(1./self.L, invMat=True)
         return self._MfLiI
+
+    @property
+    def Pac(self):
+        """
+        diagonal matrix that nulls out inactive cells
+
+        :rtype: scipy.sparse.csr_matrix
+        :return: active cell diagonal matrix
+        """
+        if getattr(self, '_Pac', None) is None:
+            if self.indActive is None:
+                self._Pac = Utils.speye(self.mesh.nC)
+            else:
+                e = np.zeros(self.mesh.nC)
+                e[self.indActive] = 1.
+                # self._Pac = Utils.speye(self.mesh.nC)[:, self.indActive]
+                self._Pac = Utils.sdiag(e)
+        return self._Pac
+
+    @property
+    def Pafx(self):
+        """
+        diagonal matrix that nulls out inactive x-faces
+        to full modelling space (ie. nFx x nindActive_Fx )
+
+        :rtype: scipy.sparse.csr_matrix
+        :return: active face-x diagonal matrix
+        """
+        if getattr(self, '_Pafx', None) is None:
+            if self.indActive is None:
+                self._Pafx = Utils.speye(self.mesh.nFx)
+            else:
+                indActive_Fx = self.mesh.aveFx2CC.T * self.indActive >= 1
+                e = np.zeros(self.mesh.nFx)
+                e[indActive_Fx] = 1.
+                self._Pafx = Utils.sdiag(e)
+        return self._Pafx
+
+    @property
+    def Pafy(self):
+        """
+        diagonal matrix that nulls out inactive y-faces
+        to full modelling space (ie. nFy x nindActive_Fy )
+
+        :rtype: scipy.sparse.csr_matrix
+        :return: active face-y diagonal matrix
+        """
+        if getattr(self, '_Pafy', None) is None:
+            if self.indActive is None:
+                self._Pafy = Utils.speye(self.mesh.nFy)
+            else:
+                indActive_Fy = (self.mesh.aveFy2CC.T * self.indActive) >= 1
+                e = np.zeros(self.mesh.nFy)
+                e[indActive_Fy] = 1.
+                self._Pafy = Utils.sdiag(e)
+        return self._Pafy
+
+    @property
+    def Pafz(self):
+        """
+        diagonal matrix that nulls out inactive z-faces
+        to full modelling space (ie. nFz x nindActive_Fz )
+
+        :rtype: scipy.sparse.csr_matrix
+        :return: active face-z diagonal matrix
+        """
+        if getattr(self, '_Pafz', None) is None:
+            if self.indActive is None:
+                self._Pafz = Utils.speye(self.mesh.nFz)
+            else:
+                indActive_Fz = (self.mesh.aveFz2CC.T * self.indActive) >= 1
+                e = np.zeros(self.mesh.nFz)
+                e[indActive_Fz] = 1.
+                self._Pafz = Utils.sdiag(e)
+        return self._Pafz
+
 
 if __name__ == '__main__':
     from SimPEG import Mesh, np