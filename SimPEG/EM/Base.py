9adf2fde3c0b44ee93b1c94298559892df55def0
Lindsey Heagy <lindseyheagy@gmail.com>
2018-02-28 21:33:40 -0800

    don't clear matrices if models are the same

--- SimPEG/EM/Base.py
+++ SimPEG/EM/Base.py
@@ -88,24 +88,56 @@ class BaseEMProblem(Problem.BaseProblem):
 
     @properties.observer('mu')
     def _clear_mu_mats_on_mu_update(self, change):
+        if change['previous'] is change['value']:
+            return
+        if (
+            isinstance(change['previous'], np.ndarray) and
+            isinstance(change['value'], np.ndarray) and
+            np.allclose(change['previous'], change['value'])
+        ):
+            return
         for mat in self._clear_on_mu_update:
             if hasattr(self, mat):
                 delattr(self, mat)
 
     @properties.observer('mui')
     def _clear_mu_mats_on_mui_update(self, change):
+        if change['previous'] is change['value']:
+            return
+        if (
+            isinstance(change['previous'], np.ndarray) and
+            isinstance(change['value'], np.ndarray) and
+            np.allclose(change['previous'], change['value'])
+        ):
+            return
         for mat in self._clear_on_mu_update:
             if hasattr(self, mat):
                 delattr(self, mat)
 
     @properties.observer('sigma')
     def _clear_sigma_mats_on_sigma_update(self, change):
+        if change['previous'] is change['value']:
+            return
+        if (
+            isinstance(change['previous'], np.ndarray) and
+            isinstance(change['value'], np.ndarray) and
+            np.allclose(change['previous'], change['value'])
+        ):
+            return
         for mat in self._clear_on_sigma_update:
             if hasattr(self, mat):
                 delattr(self, mat)
 
     @properties.observer('rho')
     def _clear_sigma_mats_on_rho_update(self, change):
+        if change['previous'] is change['value']:
+            return
+        if (
+            isinstance(change['previous'], np.ndarray) and
+            isinstance(change['value'], np.ndarray) and
+            np.allclose(change['previous'], change['value'])
+        ):
+            return
         for mat in self._clear_on_sigma_update:
             if hasattr(self, mat):
                 delattr(self, mat)
