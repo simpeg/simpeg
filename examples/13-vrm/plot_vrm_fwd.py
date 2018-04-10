b85fadd52b7e5a006961b93f6492984c71fe11e0
Devin Cowan <devinccowan@gmail.com>
2018-04-09 20:36:48 -0700

    Fix spacing

--- examples/13-vrm/plot_vrm_fwd.py
+++ examples/13-vrm/plot_vrm_fwd.py
@@ -97,126 +97,127 @@ def run(plotIt=True):
     C = np.kron(np.reshape(C, (len(C), 1)), np.ones((1, n_times)))
     FieldsTEM = C*FieldsTEM
 
-
-    Fig = plt.figure(figsize=(10, 10))
-    Ax11 = Fig.add_axes([0.07, 0.7, 0.24, 0.24])
-    Ax12 = Fig.add_axes([0.37, 0.7, 0.24, 0.24])
-    Ax13 = Fig.add_axes([0.67, 0.7, 0.24, 0.24])
-    Ax14 = Fig.add_axes([0.92, 0.7, 0.01, 0.24])
-
-    Ax21 = Fig.add_axes([0.1, 0.35, 0.4, 0.28])
-    Ax22 = Fig.add_axes([0.6, 0.35, 0.4, 0.28])
-
-    Ax31 = Fig.add_axes([0.05, 0.05, 0.25, 0.21])
-    Ax32 = Fig.add_axes([0.4, 0.05, 0.25, 0.21])
-    Ax33 = Fig.add_axes([0.75, 0.05, 0.25, 0.21])
-    FS = 12
-    N = x.shape[0]
-
     # PLOT MODEL
-    plotMap = Maps.InjectActiveCells(mesh, topoCells, 0.)  # Maps to mesh
+    if plotIt:
 
-    Cplot11 = mesh.plotSlice(plotMap*xi_true, normal='X', ind=int((ncx+2*npad)/2-6), ax=Ax11, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
-    Cplot11[0].set_clim((0., np.max(xi_true)))
-    Ax11.set_xlabel('Y [m]', fontsize=FS)
-    Ax11.set_ylabel('Z [m]', fontsize=FS, labelpad=-5)
-    Ax11.tick_params(labelsize=FS-2)
-    titlestr11 = "True Model (x = -12 m)"
-    Ax11.set_title(titlestr11, fontsize=FS+2)
+        Fig = plt.figure(figsize=(10, 10))
+        Ax11 = Fig.add_axes([0.07, 0.7, 0.23, 0.23])
+        Ax12 = Fig.add_axes([0.36, 0.7, 0.23, 0.23])
+        Ax13 = Fig.add_axes([0.65, 0.7, 0.23, 0.23])
+        Ax14 = Fig.add_axes([0.89, 0.7, 0.01, 0.24])
 
-    Cplot12 = mesh.plotSlice(plotMap*xi_true, normal='Y', ind=int((ncy+2*npad)/2), ax=Ax12, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
-    Cplot12[0].set_clim((0., np.max(xi_true)))
-    Ax12.set_xlabel('X [m]', fontsize=FS)
-    Ax12.set_ylabel('Z [m]', fontsize=FS, labelpad=-5)
-    Ax12.tick_params(labelsize=FS-2)
-    titlestr12 = "True Model (y = 0 m)"
-    Ax12.set_title(titlestr12, fontsize=FS+2)
+        Ax21 = Fig.add_axes([0.1, 0.335, 0.38, 0.29])
+        Ax22 = Fig.add_axes([0.6, 0.335, 0.38, 0.29])
 
-    Cplot13 = mesh.plotSlice(plotMap*xi_true, normal='Z', ind=int((ncz+2*npad)/2-1), ax=Ax13, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
-    Cplot13[0].set_clim((0., np.max(xi_true)))
-    Ax13.set_xlabel('X [m]', fontsize=FS)
-    Ax13.set_ylabel('Y [m]', fontsize=FS, labelpad=-5)
-    Ax13.tick_params(labelsize=FS-2)
-    titlestr13 = "True Model (z = 0 m)"
-    Ax13.set_title(titlestr13, fontsize=FS+2)
+        Ax31 = Fig.add_axes([0.07, 0.05, 0.24, 0.21])
+        Ax32 = Fig.add_axes([0.38, 0.05, 0.24, 0.21])
+        Ax33 = Fig.add_axes([0.69, 0.05, 0.24, 0.21])
+        FS = 12
+        N = x.shape[0]
 
-    norm = mpl.colors.Normalize(vmin=0., vmax=np.max(xi_true))
-    cbar14 = mpl.colorbar.ColorbarBase(Ax14, cmap='gist_heat_r', norm=norm, orientation = 'vertical')
-    cbar14.set_label('$\Delta \chi /$ln$(\lambda_2 / \lambda_1 )$ [SI]', rotation=270, labelpad=15, size=FS)
+        plotMap = Maps.InjectActiveCells(mesh, topoCells, 0.)  # Maps to mesh
 
-    # PLOT DECAY
-    j1 = int((N**2-1)/2 - 3*N)
-    di_vrm = mkvc(np.abs(FieldsVRM[j1, :]))
-    di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
-    Ax21.loglog(times, di_tem, 'r.-')
-    Ax21.loglog(times, di_vrm, 'b.-')
-    Ax21.loglog(times, di_tem+di_vrm, 'k.-')
-    Ax21.set_xlabel('t [s]', fontsize=FS)
-    Ax21.set_ylabel('|dBz/dt| [T/s]', fontsize=FS)
-    Ax21.tick_params(labelsize=FS-2)
-    Ax21.set_xbound(np.min(times), np.max(times))
-    Ax21.set_ybound(1.2*np.max(di_tem+di_vrm),1e-5*np.max(di_tem+di_vrm))
-    titlestr21 = "Decay at X = " + '{:.2f}'.format(loc[j1, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
-    Ax21.set_title(titlestr21, fontsize=FS+2)
-    Ax21.text(1.2e-5, 18*np.max(di_tem)/1e5, "TEM", fontsize=FS, color='r')
-    Ax21.text(1.2e-5, 6*np.max(di_tem)/1e5, "VRM", fontsize=FS, color='b')
-    Ax21.text(1.2e-5, 2*np.max(di_tem)/1e5, "TEM + VRM", fontsize=FS, color='k')
+        Cplot11 = mesh.plotSlice(plotMap*xi_true, normal='X', ind=int((ncx+2*npad)/2-6), ax=Ax11, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
+        Cplot11[0].set_clim((0., np.max(xi_true)))
+        Ax11.set_xlabel('Y [m]', fontsize=FS)
+        Ax11.set_ylabel('Z [m]', fontsize=FS, labelpad=-10)
+        Ax11.tick_params(labelsize=FS-2)
+        titlestr11 = "True Model (x = -12 m)"
+        Ax11.set_title(titlestr11, fontsize=FS+2)
 
-    j2 = int((N**2-1)/2 + 3*N)
-    di_vrm = mkvc(np.abs(FieldsVRM[j2, :]))
-    di_tem = mkvc(np.abs(FieldsTEM[j2, :]))
-    Ax22.loglog(times, di_tem, 'r.-')
-    Ax22.loglog(times, di_vrm, 'b.-')
-    Ax22.loglog(times, di_tem+di_vrm, 'k.-')
-    Ax22.set_xlabel('t [s]', fontsize=FS)
-    Ax22.set_ylabel('', fontsize=FS)
-    Ax22.tick_params(labelsize=FS-2)
-    Ax22.set_xbound(np.min(times), np.max(times))
-    Ax22.set_ybound(1.2*np.max(di_tem+di_vrm), 1e-5*np.max(di_tem+di_vrm))
-    titlestr22 = "Decay at X = " + '{:.2f}'.format(loc[j2, 0]) + " m and Y = " + '{:.2f}'.format(loc[j2, 1]) + " m"
-    Ax22.set_title(titlestr22, fontsize=FS+2)
+        Cplot12 = mesh.plotSlice(plotMap*xi_true, normal='Y', ind=int((ncy+2*npad)/2), ax=Ax12, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
+        Cplot12[0].set_clim((0., np.max(xi_true)))
+        Ax12.set_xlabel('X [m]', fontsize=FS)
+        Ax12.set_ylabel('Z [m]', fontsize=FS, labelpad=-10)
+        Ax12.tick_params(labelsize=FS-2)
+        titlestr12 = "True Model (y = 0 m)"
+        Ax12.set_title(titlestr12, fontsize=FS+2)
 
-    # PLOT ANOMALIES
-    d2 = np.reshape(np.abs(FieldsTEM[:, 0]+FieldsVRM[:, 0]), (N, N))
-    d3 = np.reshape(np.abs(FieldsTEM[:, 10]+FieldsVRM[:, 10]), (N, N))
-    d4 = np.reshape(np.abs(FieldsTEM[:, 20]+FieldsVRM[:, 20]), (N, N))
+        Cplot13 = mesh.plotSlice(plotMap*xi_true, normal='Z', ind=int((ncz+2*npad)/2-1), ax=Ax13, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
+        Cplot13[0].set_clim((0., np.max(xi_true)))
+        Ax13.set_xlabel('X [m]', fontsize=FS)
+        Ax13.set_ylabel('Y [m]', fontsize=FS, labelpad=-10)
+        Ax13.tick_params(labelsize=FS-2)
+        titlestr13 = "True Model (z = 0 m)"
+        Ax13.set_title(titlestr13, fontsize=FS+2)
 
-    Cplot31 = Ax31.contourf(x, y, d2.T, 40, cmap='magma_r')
-    cbar31 = plt.colorbar(Cplot31, ax=Ax31, pad=0.02, format='%.2e')
-    cbar31.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
-    cbar31.ax.tick_params(labelsize=FS-2)
-    Ax31.set_xlabel('X [m]', fontsize=FS)
-    Ax31.set_ylabel('Y [m]', fontsize=FS, labelpad=-12)
-    Ax31.tick_params(labelsize=FS-2)
-    Ax31.scatter(x, y, color=(0, 0, 0), s=4)
-    Ax31.set_xbound(np.min(x), np.max(x))
-    Ax31.set_ybound(np.min(y), np.max(y))
-    titlestr31 = "dBz/dt at t=" + '{:.1e}'.format(times[0]) + " s"
-    Ax31.set_title(titlestr31, fontsize=FS+2)
+        norm = mpl.colors.Normalize(vmin=0., vmax=np.max(xi_true))
+        cbar14 = mpl.colorbar.ColorbarBase(Ax14, cmap='gist_heat_r', norm=norm, orientation = 'vertical')
+        cbar14.set_label('$\Delta \chi /$ln$(\lambda_2 / \lambda_1 )$ [SI]', rotation=270, labelpad=15, size=FS)
 
-    Cplot32 = Ax32.contourf(x, y, d3.T, 40, cmap='magma_r')
-    cbar32 = plt.colorbar(Cplot32, ax=Ax32, pad=0.02, format='%.2e')
-    cbar32.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
-    cbar32.ax.tick_params(labelsize=FS-2)
-    Ax32.set_xlabel('X [m]', fontsize=FS)
-    Ax32.set_ylabel('Y [m]', fontsize=FS, labelpad=-12)
-    Ax32.tick_params(labelsize=FS-2)
-    Ax32.set_xbound(np.min(x), np.max(x))
-    Ax32.set_ybound(np.min(y), np.max(y))
-    titlestr32 = "dBz/dt at t=" + '{:.1e}'.format(times[10]) + " s"
-    Ax32.set_title(titlestr32, fontsize=FS+2)
+        # PLOT DECAY
+        j1 = int((N**2-1)/2 - 3*N)
+        di_vrm = mkvc(np.abs(FieldsVRM[j1, :]))
+        di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
+        Ax21.loglog(times, di_tem, 'r.-')
+        Ax21.loglog(times, di_vrm, 'b.-')
+        Ax21.loglog(times, di_tem+di_vrm, 'k.-')
+        Ax21.set_xlabel('t [s]', fontsize=FS)
+        Ax21.set_ylabel('|dBz/dt| [T/s]', fontsize=FS)
+        Ax21.tick_params(labelsize=FS-2)
+        Ax21.set_xbound(np.min(times), np.max(times))
+        Ax21.set_ybound(1.2*np.max(di_tem+di_vrm),1e-5*np.max(di_tem+di_vrm))
+        titlestr21 = "Decay at X = " + '{:.2f}'.format(loc[j1, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
+        Ax21.set_title(titlestr21, fontsize=FS+2)
+        Ax21.text(1.2e-5, 18*np.max(di_tem)/1e5, "TEM", fontsize=FS, color='r')
+        Ax21.text(1.2e-5, 6*np.max(di_tem)/1e5, "VRM", fontsize=FS, color='b')
+        Ax21.text(1.2e-5, 2*np.max(di_tem)/1e5, "TEM + VRM", fontsize=FS, color='k')
 
-    Cplot33 = Ax33.contourf(x, y, d4.T, 40, cmap='magma_r')
-    cbar33 = plt.colorbar(Cplot33, ax=Ax33, pad=0.02, format='%.2e')
-    cbar33.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
-    cbar33.ax.tick_params(labelsize=FS-2)
-    Ax33.set_xlabel('X [m]', fontsize=FS)
-    Ax33.set_ylabel('Y [m]', fontsize=FS, labelpad=-12)
-    Ax33.tick_params(labelsize=FS-2)
-    Ax33.set_xbound(np.min(x), np.max(x))
-    Ax33.set_ybound(np.min(y), np.max(y))
-    titlestr33 = "dBz/dt at t=" + '{:.1e}'.format(times[20]) + " s"
-    Ax33.set_title(titlestr33, fontsize=FS+2)
+        j2 = int((N**2-1)/2 + 3*N)
+        di_vrm = mkvc(np.abs(FieldsVRM[j2, :]))
+        di_tem = mkvc(np.abs(FieldsTEM[j2, :]))
+        Ax22.loglog(times, di_tem, 'r.-')
+        Ax22.loglog(times, di_vrm, 'b.-')
+        Ax22.loglog(times, di_tem+di_vrm, 'k.-')
+        Ax22.set_xlabel('t [s]', fontsize=FS)
+        Ax22.axes.get_yaxis().set_visible(False)
+        Ax22.tick_params(labelsize=FS-2)
+        Ax22.set_xbound(np.min(times), np.max(times))
+        Ax22.set_ybound(1.2*np.max(di_tem+di_vrm), 1e-5*np.max(di_tem+di_vrm))
+        titlestr22 = "Decay at X = " + '{:.2f}'.format(loc[j2, 0]) + " m and Y = " + '{:.2f}'.format(loc[j2, 1]) + " m"
+        Ax22.set_title(titlestr22, fontsize=FS+2)
+
+        # PLOT ANOMALIES
+        d2 = np.reshape(np.abs(FieldsTEM[:, 0]+FieldsVRM[:, 0]), (N, N))
+        d3 = np.reshape(np.abs(FieldsTEM[:, 10]+FieldsVRM[:, 10]), (N, N))
+        d4 = np.reshape(np.abs(FieldsTEM[:, 20]+FieldsVRM[:, 20]), (N, N))
+
+        Cplot31 = Ax31.contourf(x, y, d2.T, 40, cmap='magma_r')
+        cbar31 = plt.colorbar(Cplot31, ax=Ax31, pad=0.02, format='%.2e')
+        cbar31.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
+        cbar31.ax.tick_params(labelsize=FS-2)
+        Ax31.set_xlabel('X [m]', fontsize=FS)
+        Ax31.set_ylabel('Y [m]', fontsize=FS, labelpad=-8)
+        Ax31.tick_params(labelsize=FS-2)
+        Ax31.scatter(x, y, color=(0, 0, 0), s=4)
+        Ax31.set_xbound(np.min(x), np.max(x))
+        Ax31.set_ybound(np.min(y), np.max(y))
+        titlestr31 = "dBz/dt at t=" + '{:.1e}'.format(times[0]) + " s"
+        Ax31.set_title(titlestr31, fontsize=FS+2)
+
+        Cplot32 = Ax32.contourf(x, y, d3.T, 40, cmap='magma_r')
+        cbar32 = plt.colorbar(Cplot32, ax=Ax32, pad=0.02, format='%.2e')
+        cbar32.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
+        cbar32.ax.tick_params(labelsize=FS-2)
+        Ax32.set_xlabel('X [m]', fontsize=FS)
+        Ax32.axes.get_yaxis().set_visible(False)
+        Ax32.tick_params(labelsize=FS-2)
+        Ax32.set_xbound(np.min(x), np.max(x))
+        Ax32.set_ybound(np.min(y), np.max(y))
+        titlestr32 = "dBz/dt at t=" + '{:.1e}'.format(times[10]) + " s"
+        Ax32.set_title(titlestr32, fontsize=FS+2)
+
+        Cplot33 = Ax33.contourf(x, y, d4.T, 40, cmap='magma_r')
+        cbar33 = plt.colorbar(Cplot33, ax=Ax33, pad=0.02, format='%.2e')
+        cbar33.set_label('[T/s]', rotation=270, labelpad=12, size=FS)
+        cbar33.ax.tick_params(labelsize=FS-2)
+        Ax33.set_xlabel('X [m]', fontsize=FS)
+        Ax33.axes.get_yaxis().set_visible(False)
+        Ax33.tick_params(labelsize=FS-2)
+        Ax33.set_xbound(np.min(x), np.max(x))
+        Ax33.set_ybound(np.min(y), np.max(y))
+        titlestr33 = "dBz/dt at t=" + '{:.1e}'.format(times[20]) + " s"
+        Ax33.set_title(titlestr33, fontsize=FS+2)
 
 if __name__ == '__main__':
     run()
--- examples/13-vrm/plot_vrm_inv.py
+++ examples/13-vrm/plot_vrm_inv.py
@@ -6,10 +6,12 @@ Here, we use an equivalent source inversion to remove the VRM response from TEM
 data collected by a small coincident loop system. The data being inverted are
 the same as in the forward modeling example. To remove the VRM signal we:
 
-    1) invert the late time data to recover an equivalent source surface layer
-    of cells.
-    2) use the recovered model to predict the VRM response at all times
-    3) subtract the predicted VRM response from the observed data
+1) invert the late time data to recover an equivalent source surface layer
+of cells.
+
+2) use the recovered model to predict the VRM response at all times
+
+3) subtract the predicted VRM response from the observed data
 """
 import SimPEG.VRM as VRM
 import numpy as np
@@ -140,125 +142,129 @@ def run(plotIt=True):
     FieldsTEM = np.reshape(FieldsTEM, (n_loc, n_times))
     FieldsPRE = np.reshape(FieldsPRE, (n_loc, n_times))
 
-    Fig = plt.figure(figsize=(10, 10))
-    Ax11 = Fig.add_axes([0.2, 0.7, 0.25, 0.25])
-    Ax12 = Fig.add_axes([0.55, 0.7, 0.25, 0.25])
-    Ax13 = Fig.add_axes([0.83, 0.7, 0.01, 0.25])
-
-    Ax21 = Fig.add_axes([0.15, 0.35, 0.35, 0.27])
-    Ax22 = Fig.add_axes([0.6, 0.35, 0.35, 0.27])
-
-    Ax31 = Fig.add_axes([0.1, 0.03, 0.25, 0.25])
-    Ax32 = Fig.add_axes([0.38, 0.03, 0.25, 0.25])
-    Ax33 = Fig.add_axes([0.66, 0.03, 0.25, 0.25])
-    Ax34 = Fig.add_axes([0.93, 0.03, 0.01, 0.25])
-    FS = 12
-    N = x.shape[0]
-
     # PLOT MODEL
-    invMap = Maps.InjectActiveCells(mesh, actCells, 0.)  # Maps to mesh
-    topoMap = Maps.InjectActiveCells(mesh, topoCells, 0.)
+    if plotIt:
 
-    MAX = np.max(np.r_[xi_true, xi_rec])
-    Cplot11 = mesh.plotSlice(topoMap*xi_true, ind=int((ncz+2*npad)/2-1), ax=Ax11, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
-    Cplot11[0].set_clim((0., MAX))
-    Ax11.set_xlabel('X [m]', fontsize=FS)
-    Ax11.set_ylabel('Y [m]', fontsize=FS, labelpad=-5)
-    Ax11.tick_params(labelsize=FS-2)
-    titlestr11 = "True Model (z = 0 m)"
-    Ax11.set_title(titlestr11, fontsize=FS+2)
+        Fig = plt.figure(figsize=(10, 10))
 
-    Cplot12 = mesh.plotSlice(invMap*xi_rec, ind=int((ncz+2*npad)/2-1), ax=Ax12, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
-    Cplot12[0].set_clim((0., MAX))
-    Ax12.set_xlabel('X [m]', fontsize=FS)
-    Ax12.axes.get_yaxis().set_visible(False)
-    Ax12.tick_params(labelsize=FS-2)
-    titlestr12 = "Equivalent Source Model"
-    Ax12.set_title(titlestr12, fontsize=FS+2)
+        Ax11 = Fig.add_axes([0.2, 0.7, 0.25, 0.25])
+        Ax12 = Fig.add_axes([0.55, 0.7, 0.25, 0.25])
+        Ax13 = Fig.add_axes([0.83, 0.7, 0.01, 0.25])
 
-    norm = mpl.colors.Normalize(vmin=0., vmax=MAX)
-    cbar14 = mpl.colorbar.ColorbarBase(Ax13, cmap='gist_heat_r', norm=norm, orientation='vertical')
-    cbar14.set_label('$\Delta \chi /$ln$(\lambda_2 / \lambda_1 )$ [SI]', rotation=270, labelpad=15, size=FS)
+        Ax21 = Fig.add_axes([0.1, 0.36, 0.35, 0.26])
+        Ax22 = Fig.add_axes([0.55, 0.36, 0.35, 0.26])
 
-    # PLOT DECAY
-    j1 = int((N**2-1)/2 - 3*N)
-    di_tot = mkvc(np.abs(FieldsTOT[j1, :]))
-    di_pre = mkvc(np.abs(FieldsVRM[j1, :]))
-    di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
-    Ax21.loglog(times, di_tot, 'k.-')
-    Ax21.loglog(times, di_tem, 'r.-')
-    Ax21.loglog(times, di_pre, 'b.-')
-    Ax21.loglog(times, np.abs(di_tot-di_pre), 'g.-')
-    Ax21.set_xlabel('t [s]', fontsize=FS, labelpad=-10)
-    Ax21.set_ylabel('|dBz/dt| [T/s]', fontsize=FS)
-    Ax21.tick_params(labelsize=FS-2)
-    Ax21.set_xbound(np.min(times), np.max(times))
-    Ax21.set_ybound(1.2*np.max(di_tot), 1e-5*np.max(di_tot))
-    titlestr21 = "Decay at X = " + '{:.2f}'.format(loc[j1, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
-    Ax21.set_title(titlestr21, fontsize=FS+2)
-    Ax21.text(1.2e-5, 54*np.max(di_tot)/1e5, "Observed", fontsize=FS, color='k')
-    Ax21.text(1.2e-5, 18*np.max(di_tot)/1e5, "True TEM", fontsize=FS, color='r')
-    Ax21.text(1.2e-5, 6*np.max(di_tot)/1e5, "Predicted VRM", fontsize=FS, color='b')
-    Ax21.text(1.2e-5, 2*np.max(di_tot)/1e5, "Recovered TEM", fontsize=FS, color='g')
+        Ax31 = Fig.add_axes([0.07, 0.05, 0.24, 0.24])
+        Ax32 = Fig.add_axes([0.35, 0.05, 0.24, 0.24])
+        Ax33 = Fig.add_axes([0.63, 0.05, 0.24, 0.24])
+        Ax34 = Fig.add_axes([0.88, 0.05, 0.01, 0.24])
 
-    j2 = int((N**2-1)/2 + 3*N)
-    di_tot = mkvc(np.abs(FieldsTOT[j1, :]))
-    di_pre = mkvc(np.abs(FieldsVRM[j1, :]))
-    di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
-    Ax22.loglog(times, di_tot, 'k.-')
-    Ax22.loglog(times, di_tem, 'r.-')
-    Ax22.loglog(times, di_pre, 'b.-')
-    Ax22.loglog(times, np.abs(di_tot-di_pre), 'g.-')
-    Ax22.set_xlabel('t [s]', fontsize=FS, labelpad=-10)
-    Ax22.set_ylabel('|dBz/dt| [T/s]', fontsize=FS)
-    Ax22.tick_params(labelsize=FS-2)
-    Ax22.set_xbound(np.min(times), np.max(times))
-    Ax22.set_ybound(1.2*np.max(di_tot),1e-5*np.max(di_tot))
-    titlestr22 = "Decay at X = " + '{:.2f}'.format(loc[j2, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
-    Ax22.set_title(titlestr22, fontsize=FS+2)
+        FS = 12
+        N = x.shape[0]
 
-    # PLOT ANOMALIES
-    d2 = np.reshape(np.abs(FieldsTOT[:, 10]), (N, N))
-    d3 = np.reshape(np.abs(FieldsTEM[:, 10]), (N, N))
-    d4 = np.reshape(np.abs(FieldsTOT[:, 10]-FieldsPRE[:, 10]), (N, N))
+        invMap = Maps.InjectActiveCells(mesh, actCells, 0.)  # Maps to mesh
+        topoMap = Maps.InjectActiveCells(mesh, topoCells, 0.)
 
-    MIN = np.min(np.r_[d2, d3, d4])
-    MAX = np.max(np.r_[d2, d3, d4])
+        MAX = np.max(np.r_[xi_true, xi_rec])
+        Cplot11 = mesh.plotSlice(topoMap*xi_true, ind=int((ncz+2*npad)/2-1), ax=Ax11, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
+        Cplot11[0].set_clim((0., MAX))
+        Ax11.set_xlabel('X [m]', fontsize=FS)
+        Ax11.set_ylabel('Y [m]', fontsize=FS, labelpad=-5)
+        Ax11.tick_params(labelsize=FS-2)
+        titlestr11 = "True Model (z = 0 m)"
+        Ax11.set_title(titlestr11, fontsize=FS+2)
 
-    Cplot31 = Ax31.contourf(x, y, d2.T, 40, cmap='magma_r')
-    Ax31.set_xticks(np.linspace(-30, 30, 7))
-    Ax31.set_xlabel('X [m]', fontsize=FS)
-    Ax31.set_ylabel('Y [m]', fontsize=FS, labelpad=-12)
-    Ax31.tick_params(labelsize=FS-2)
-    Ax31.scatter(x, y, color=(0, 0, 0), s=4)
-    Ax31.set_xbound(np.min(x), np.max(x))
-    Ax31.set_ybound(np.min(y), np.max(y))
-    titlestr31 = "Observed at t=" + '{:.1e}'.format(times[10]) + " s"
-    Ax31.set_title(titlestr31, fontsize=FS+2)
+        Cplot12 = mesh.plotSlice(invMap*xi_rec, ind=int((ncz+2*npad)/2-1), ax=Ax12, grid=True, pcolorOpts={'cmap': 'gist_heat_r'})
+        Cplot12[0].set_clim((0., MAX))
+        Ax12.set_xlabel('X [m]', fontsize=FS)
+        Ax12.axes.get_yaxis().set_visible(False)
+        Ax12.tick_params(labelsize=FS-2)
+        titlestr12 = "Equivalent Source Model"
+        Ax12.set_title(titlestr12, fontsize=FS+2)
 
-    Cplot32 = Ax32.contourf(x, y, d3.T, 40, cmap='magma_r')
-    Ax32.set_xticks(np.linspace(-30, 30, 7))
-    Ax32.set_xlabel('X [m]', fontsize=FS)
-    Ax32.axes.get_yaxis().set_visible(False)
-    Ax32.tick_params(labelsize=FS-2)
-    Ax32.set_xbound(np.min(x), np.max(x))
-    Ax32.set_ybound(np.min(y), np.max(y))
-    titlestr32 = "True TEM at t=" + '{:.1e}'.format(times[10]) + " s"
-    Ax32.set_title(titlestr32, fontsize=FS+2)
+        norm = mpl.colors.Normalize(vmin=0., vmax=MAX)
+        cbar14 = mpl.colorbar.ColorbarBase(Ax13, cmap='gist_heat_r', norm=norm, orientation='vertical')
+        cbar14.set_label('$\Delta \chi /$ln$(\lambda_2 / \lambda_1 )$ [SI]', rotation=270, labelpad=15, size=FS)
 
-    Cplot33 = Ax33.contourf(x, y, d4.T, 40, cmap='magma_r')
-    Ax33.set_xticks(np.linspace(-30, 30, 7))
-    Ax33.set_xlabel('X [m]', fontsize=FS)
-    Ax33.axes.get_yaxis().set_visible(False)
-    Ax33.tick_params(labelsize=FS-2)
-    Ax33.set_xbound(np.min(x), np.max(x))
-    Ax33.set_ybound(np.min(y), np.max(y))
-    titlestr33 = "Recov. TEM at t=" + '{:.1e}'.format(times[10]) + " s"
-    Ax33.set_title(titlestr33, fontsize=FS+2)
+        # PLOT DECAY
+        j1 = int((N**2-1)/2 - 3*N)
+        di_tot = mkvc(np.abs(FieldsTOT[j1, :]))
+        di_pre = mkvc(np.abs(FieldsVRM[j1, :]))
+        di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
+        Ax21.loglog(times, di_tot, 'k.-')
+        Ax21.loglog(times, di_tem, 'r.-')
+        Ax21.loglog(times, di_pre, 'b.-')
+        Ax21.loglog(times, np.abs(di_tot-di_pre), 'g.-')
+        Ax21.set_xlabel('t [s]', fontsize=FS, labelpad=-10)
+        Ax21.set_ylabel('|dBz/dt| [T/s]', fontsize=FS)
+        Ax21.tick_params(labelsize=FS-2)
+        Ax21.set_xbound(np.min(times), np.max(times))
+        Ax21.set_ybound(1.2*np.max(di_tot), 1e-5*np.max(di_tot))
+        titlestr21 = "Decay at X = " + '{:.2f}'.format(loc[j1, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
+        Ax21.set_title(titlestr21, fontsize=FS+2)
+        Ax21.text(1.2e-5, 54*np.max(di_tot)/1e5, "Observed", fontsize=FS, color='k')
+        Ax21.text(1.2e-5, 18*np.max(di_tot)/1e5, "True TEM", fontsize=FS, color='r')
+        Ax21.text(1.2e-5, 6*np.max(di_tot)/1e5, "Predicted VRM", fontsize=FS, color='b')
+        Ax21.text(1.2e-5, 2*np.max(di_tot)/1e5, "Recovered TEM", fontsize=FS, color='g')
 
-    norm = mpl.colors.Normalize(vmin=MIN, vmax=MAX)
-    cbar34 = mpl.colorbar.ColorbarBase(Ax34, cmap='magma_r', norm=norm, orientation='vertical', format='%.1e')
-    cbar34.set_label('dBz/dt [T/s]', rotation=270, size=FS)
+        j2 = int((N**2-1)/2 + 3*N)
+        di_tot = mkvc(np.abs(FieldsTOT[j1, :]))
+        di_pre = mkvc(np.abs(FieldsVRM[j1, :]))
+        di_tem = mkvc(np.abs(FieldsTEM[j1, :]))
+        Ax22.loglog(times, di_tot, 'k.-')
+        Ax22.loglog(times, di_tem, 'r.-')
+        Ax22.loglog(times, di_pre, 'b.-')
+        Ax22.loglog(times, np.abs(di_tot-di_pre), 'g.-')
+        Ax22.set_xlabel('t [s]', fontsize=FS, labelpad=-10)
+        Ax22.axes.get_yaxis().set_visible(False)
+        Ax22.tick_params(labelsize=FS-2)
+        Ax22.set_xbound(np.min(times), np.max(times))
+        Ax22.set_ybound(1.2*np.max(di_tot),1e-5*np.max(di_tot))
+        titlestr22 = "Decay at X = " + '{:.2f}'.format(loc[j2, 0]) + " m and Y = " + '{:.2f}'.format(loc[j1, 1]) + " m"
+        Ax22.set_title(titlestr22, fontsize=FS+2)
+
+        # PLOT ANOMALIES
+        d2 = np.reshape(np.abs(FieldsTOT[:, 10]), (N, N))
+        d3 = np.reshape(np.abs(FieldsTEM[:, 10]), (N, N))
+        d4 = np.reshape(np.abs(FieldsTOT[:, 10]-FieldsPRE[:, 10]), (N, N))
+
+        MIN = np.min(np.r_[d2, d3, d4])
+        MAX = np.max(np.r_[d2, d3, d4])
+
+        Cplot31 = Ax31.contourf(x, y, d2.T, 40, cmap='magma_r')
+        Ax31.set_xticks(np.linspace(-30, 30, 7))
+        Ax31.set_xlabel('X [m]', fontsize=FS)
+        Ax31.set_ylabel('Y [m]', fontsize=FS, labelpad=-12)
+        Ax31.tick_params(labelsize=FS-2)
+        Ax31.scatter(x, y, color=(0, 0, 0), s=4)
+        Ax31.set_xbound(np.min(x), np.max(x))
+        Ax31.set_ybound(np.min(y), np.max(y))
+        titlestr31 = "Observed at t=" + '{:.1e}'.format(times[10]) + " s"
+        Ax31.set_title(titlestr31, fontsize=FS+2)
+
+        Cplot32 = Ax32.contourf(x, y, d3.T, 40, cmap='magma_r')
+        Ax32.set_xticks(np.linspace(-30, 30, 7))
+        Ax32.set_xlabel('X [m]', fontsize=FS)
+        Ax32.axes.get_yaxis().set_visible(False)
+        Ax32.tick_params(labelsize=FS-2)
+        Ax32.set_xbound(np.min(x), np.max(x))
+        Ax32.set_ybound(np.min(y), np.max(y))
+        titlestr32 = "True TEM at t=" + '{:.1e}'.format(times[10]) + " s"
+        Ax32.set_title(titlestr32, fontsize=FS+2)
+
+        Cplot33 = Ax33.contourf(x, y, d4.T, 40, cmap='magma_r')
+        Ax33.set_xticks(np.linspace(-30, 30, 7))
+        Ax33.set_xlabel('X [m]', fontsize=FS)
+        Ax33.axes.get_yaxis().set_visible(False)
+        Ax33.tick_params(labelsize=FS-2)
+        Ax33.set_xbound(np.min(x), np.max(x))
+        Ax33.set_ybound(np.min(y), np.max(y))
+        titlestr33 = "Recov. TEM at t=" + '{:.1e}'.format(times[10]) + " s"
+        Ax33.set_title(titlestr33, fontsize=FS+2)
+
+        norm = mpl.colors.Normalize(vmin=MIN, vmax=MAX)
+        cbar34 = mpl.colorbar.ColorbarBase(Ax34, cmap='magma_r', norm=norm, orientation='vertical', format='%.1e')
+        cbar34.set_label('dBz/dt [T/s]', rotation=270, size=FS, labelpad=15)
 
 if __name__ == '__main__':
     run()
