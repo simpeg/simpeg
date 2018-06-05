4397a4512f1d1de57cda9be8da1dd552c410e1ff
Thibaut Astic <thast@eos.ubc.ca>
2018-06-04 18:54:45 -0700

    fix for readGravObs

--- SimPEG/PF/Gravity.py
+++ SimPEG/PF/Gravity.py
@@ -461,14 +461,16 @@ def readGravityObservations(obs_file):
     wd = np.zeros(ndat, dtype=float)
     locXYZ = np.zeros((ndat, 3), dtype=float)
 
-    for ii in range(ndat):
-
+    ii=0
+    while ii<ndat:
         temp = np.array(line.split(), dtype=float)
         if len(temp) > 0:
             locXYZ[ii, :] = temp[:3]
             d[ii] = temp[3]
             wd[ii] = temp[4]
+            ii+=1
         line = fid.readline()
+
 
     rxLoc = GRAV.RxObs(locXYZ)
     srcField = GRAV.SrcField([rxLoc])
