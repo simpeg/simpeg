4c11775f04b19450702324576ebeae8a139c9958
fourndo <fourndo@gmail.com>
2018-06-07 12:48:12 -0700

    Add plotDataHillside to Utils

--- SimPEG/Utils/PlotUtils.py
+++ SimPEG/Utils/PlotUtils.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy.interpolate import LinearNDInterpolator
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+from scipy.spatial import cKDTree
+from scipy.interpolate.interpnd import _ndim_coords_from_arrays
+from matplotlib.colors import LightSource, Normalize
 
 
 def plot2Ddata(xyz, data, vec=False, nx=100, ny=100,
@@ -126,3 +130,123 @@ def plotLayer(sig, LocSigZ, xscale='log', ax=None,
                     np.ones(100)*locz, 'b--', lw=0.5
                 )
         return ax.plot(sigma, z, 'k-', **kwargs)
+
+
+def plotDataHillside(x, y, z, axs=None, fill=True, contour=0,
+                     vmin=None, vmax=None,
+                     clabel=True, cmap='RdBu_r', ve=1., alpha=1., alphaHS=1.,
+                     distMax=1000, midpoint=None, azdeg=315, altdeg=45):
+
+    ls = LightSource(azdeg=azdeg, altdeg=altdeg)
+
+    if x.ndim == 1:
+        # Create grid of points
+        vectorX = np.linspace(x.min(), x.max(), 1000)
+        vectorY = np.linspace(y.min(), y.max(), 1000)
+
+        X, Y = np.meshgrid(vectorX, vectorY)
+
+        # Interpolate
+        d_grid = griddata(np.c_[x, y], z, (X, Y), method='cubic')
+
+        # Remove points beyond treshold
+        tree = cKDTree(np.c_[x, y])
+        xi = _ndim_coords_from_arrays((X, Y), ndim=2)
+        dists, indexes = tree.query(xi)
+
+        # Copy original result but mask missing values with NaNs
+        d_grid[dists > distMax] = np.nan
+
+    else:
+
+        X, Y, d_grid = x, y, z
+
+    class MidPointNorm(Normalize):
+        def __init__(self, midpoint=None, vmin=None, vmax=None, clip=False):
+            Normalize.__init__(self, vmin, vmax, clip)
+            self.midpoint = midpoint
+
+        def __call__(self, value, clip=None):
+            if clip is None:
+                clip = self.clip
+
+            result, is_scalar = self.process_value(value)
+
+            self.autoscale_None(result)
+
+            if self.midpoint is None:
+                self.midpoint = np.mean(value)
+            vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+            if not (vmin < midpoint < vmax):
+                raise ValueError("midpoint must be between maxvalue and minvalue.")
+            elif vmin == vmax:
+                result.fill(0) # Or should it be all masked? Or 0.5?
+            elif vmin > vmax:
+                raise ValueError("maxvalue must be bigger than minvalue")
+            else:
+                vmin = float(vmin)
+                vmax = float(vmax)
+                if clip:
+                    mask = np.ma.getmask(result)
+                    result = np.ma.array(np.clip(result.filled(vmax), vmin, vmax),
+                                      mask=mask)
+
+                # ma division is very slow; we can take a shortcut
+                resdat = result.data
+
+                # First scale to -1 to 1 range, than to from 0 to 1.
+                resdat -= midpoint
+                resdat[resdat > 0] /= abs(vmax - midpoint)
+                resdat[resdat < 0] /= abs(vmin - midpoint)
+
+                resdat /= 2.
+                resdat += 0.5
+                result = np.ma.array(resdat, mask=result.mask, copy=False)
+
+            if is_scalar:
+                result = result[0]
+            return result
+
+        def inverse(self, value):
+            if not self.scaled():
+                raise ValueError("Not invertible until scaled")
+            vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+            if cbook.iterable(value):
+                val = ma.asarray(value)
+                val = 2 * (val-0.5)
+                val[val > 0] *= abs(vmax - midpoint)
+                val[val < 0] *= abs(vmin - midpoint)
+                val += midpoint
+                return val
+            else:
+                val = 2 * (val - 0.5)
+                if val < 0:
+                    return val*abs(vmin-midpoint) + midpoint
+                else:
+                    return val*abs(vmax-midpoint) + midpoint
+
+    im, CS = [], []
+    if axs is None:
+        axs = plt.subplot()
+
+    if fill:
+        extent = x.min(), x.max(), y.min(), y.max()
+        im = axs.contourf(
+            X, Y, d_grid, 50, vmin=vmin, vmax=vmax,
+            cmap=cmap, norm=MidPointNorm(midpoint=midpoint), alpha=alpha
+        )
+
+        axs.imshow(ls.hillshade(d_grid, vert_exag=ve, dx=1., dy=1.),
+                   cmap='gray', alpha=alphaHS,
+                   extent=extent, origin='lower')
+
+    if contour > 0:
+        CS = axs.contour(
+            X, Y, d_grid, int(contour), colors='k', linewidths=0.5
+        )
+
+        if clabel:
+            plt.clabel(CS, inline=1, fontsize=10, fmt='%i')
+    return im, CS
