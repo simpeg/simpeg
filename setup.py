4e38f2e4d04819419d8e7360ebcd022a7c1f2d98
Lindsey Heagy <lindseyheagy@gmail.com>
2018-07-11 11:37:57 -0700

    Bump version: 0.8.6 → 0.9.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.rst") as f:
 
 setup(
     name="SimPEG",
-    version="0.8.6",
+    version="0.9.0",
     packages=find_packages(),
     install_requires=[
         'numpy>=1.7',
