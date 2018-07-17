73d9e63fb717c7a18f2df7113579794b3ed94094
fourndo <fourndo@gmail.com>
2018-07-17 16:44:54 -0700

    First stab at example for web

--- examples/05-mag/plot_inversion_TreeMesh.py
+++ examples/05-mag/plot_inversion_TreeMesh.py
@@ -1,254 +1,215 @@
-"""
-PF: Magnetic: Inversion Linear
-===============================
+from SimPEG import (Mesh, Directives, Maps,
+                    InvProblem, Optimization, DataMisfit,
+                    Inversion, Utils, Regularization)
 
-Create a synthetic block model and invert
-with a compact norm
-
-"""
-import matplotlib.pyplot as plt
+import SimPEG.PF as PF
+import scipy as sp
 import numpy as np
+import matplotlib.pyplot as plt
 
-from SimPEG import Mesh
-from SimPEG import Utils
-from SimPEG import Maps
-from SimPEG import Regularization
-from SimPEG import DataMisfit
-from SimPEG import Optimization
-from SimPEG import InvProblem
-from SimPEG import Directives
-from SimPEG import Inversion
-from SimPEG import PF
+###############################################################################
+# Setup
+# -----
+#
+# Define the survey and model parameters
+#
+# First we need to define the direction of the inducing field
+# As a simple case, we pick a vertical inducing field of magnitude 50,000 nT.
+# From old convention, field orientation is given as an azimuth from North
+# (positive clockwise) and dip from the horizontal (positive downward).
+
+H0 = (50000., 90., 0.)
+
+# Assume all induced so the magnetization M is also in the same direction
+M = np.array([90, 0])
+
+# Create grid of points for topography
+# Lets create a simple Gaussian topo and set the active cells
+[xx, yy] = np.meshgrid(np.linspace(-200, 200, 50), np.linspace(-200, 200, 50))
+b = 100
+A = 50
+zz = A*np.exp(-0.5*((xx/b)**2. + (yy/b)**2.))
+
+# We would usually load a topofile
+topo = np.c_[Utils.mkvc(xx), Utils.mkvc(yy), Utils.mkvc(zz)]
+
+# Create and array of observation points
+xr = np.linspace(-100., 100., 20)
+yr = np.linspace(-100., 100., 20)
+X, Y = np.meshgrid(xr, yr)
+Z = A*np.exp(-0.5*((X/b)**2. + (Y/b)**2.)) + 5
+
+# Create a MAGsurvey
+xyzLoc = np.c_[Utils.mkvc(X.T), Utils.mkvc(Y.T), Utils.mkvc(Z.T)]
+rxLoc = PF.BaseMag.RxObs(xyzLoc)
+srcField = PF.BaseMag.SrcField([rxLoc], param=H0)
+survey = PF.BaseMag.LinearSurvey(srcField)
+
+# Here how the topography looks with a quick interpolation, just a Gaussian...
+tri = sp.spatial.Delaunay(topo)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(
+    topo[:, 0], topo[:, 1], topo[:, 2],
+    triangles=tri.simplices, cmap=plt.cm.Spectral
+)
+ax.scatter3D(xyzLoc[:, 0], xyzLoc[:, 1], xyzLoc[:, 2], c='k')
+plt.show()
+
+###############################################################################
+# Inversion Mesh
+# --------------
+#
+# Here, we create a TreeMesh with base cell size of 5 m. We reated a small
+# utility function to center the mesh around points and to figure out the
+# outer most dimension for adequate padding distance.
+# The second stage allows to refine the mesh around points or surfaces
+# (point assumed to follow some horiontal trend)
+# The refinement process is repeated twice to allow for a finer level around
+# the survey locations.
+#
+
+# Create a mesh
+h = [5, 5, 5]
+padDist = np.ones((3, 2)) * 100
+
+# Create a topography
+mesh = Utils.modelutils.meshBuilder(topo, h, padDist,
+                                    meshType='TREE',
+                                    verticalAlignment='center')
+
+# Discretize around topography, only 2th and 3th level for now
+mesh = Utils.modelutils.refineTree(mesh, topo, dtype='surface',
+                                   nCpad=[0, 3, 1], finalize=False)
+
+# Dsicretize finer around the observation locations...finalize here
+mesh = Utils.modelutils.refineTree(mesh, xyzLoc, dtype='surface',
+                                   nCpad=[4, 0, 0], finalize=True)
+
+# Define an active cells from topo
+actv = Utils.surface2ind_topo(mesh, topo)
+nC = int(actv.sum())
+
+###########################################################################
+# Forward modeling data
+# ---------------------
+#
+# We can now create a susceptibility model and generate data
+# Lets start with a simple block below topography
+#
+
+model = Utils.ModelBuilder.addBlock(
+    mesh.gridCC, np.zeros(mesh.nC),
+    np.r_[-40, -40, -50], np.r_[40, 40, 0], 0.05
+)[actv]
+
+# Create active map to go from reduce set to full
+actvMap = Maps.InjectActiveCells(mesh, actv, np.nan)
+
+# Creat reduced identity map
+idenMap = Maps.IdentityMap(nP=nC)
+
+# Create the forward model operator
+prob = PF.Magnetics.MagneticIntegral(mesh, chiMap=idenMap, actInd=actv)
+
+# Pair the survey and problem
+survey.pair(prob)
+
+# Compute linear forward operator and compute some data
+data = prob.fields(model)
+
+data += np.random.randn(len(data))
+wd = np.ones(len(data))*1.
+
+survey.dobs = data
+survey.std = wd
 
 
-def run(plotIt=True):
+actvPlot = Maps.InjectActiveCells(mesh, actv, np.nan)
+# Create a few models
+plt.figure()
+ax = plt.subplot()
+mesh.plotSlice(actvPlot*model, ax=ax, normal='Y', ind=65)
+ax.set_xlim([-300, 300])
+ax.set_ylim([-300, 75])
+ax.set_title('A simple block model.')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
 
-    # Define the inducing field parameter
-    H0 = (50000, 90, 0)
 
-    # Create a mesh
-    dx = 5.
+Utils.PlotUtils.plot2Ddata(xyzLoc, data)
+ax.set_title('Predicted data.')
+plt.gca().set_aspect('equal', adjustable='box')
 
-    hxind = [(dx, 5, -1.3), (dx, 10), (dx, 5, 1.3)]
-    hyind = [(dx, 5, -1.3), (dx, 10), (dx, 5, 1.3)]
-    hzind = [(dx, 5, -1.3), (dx, 10)]
+######################################################################
+# Inversion
+# ---------
+#
+# We can now attempt the inverse calculations. We put some great care
+# in design an inversion methology that would yield geologically
+# reasonable solution, even when non-uniform discretization
+# is used, such as TreeMesh.
+#
+# We will run the sparse inversion.
+#
 
-    mesh = Mesh.TensorMesh([hxind, hyind, hzind], 'CCC')
+# Create sensitivity weights from our linear forward operator
+rxLoc = survey.srcField.rxList[0].locs
+wr = np.zeros(prob.G.shape[1])
+for ii in range(survey.nD):
+    wr += (prob.G[ii, :]/survey.std[ii])**2.
 
-    # Get index of the center
-    midx = int(mesh.nCx/2)
-    midy = int(mesh.nCy/2)
+# wr = (wr/np.max(wr))
+wr = wr**0.5
 
-    # Lets create a simple Gaussian topo and set the active cells
-    [xx, yy] = np.meshgrid(mesh.vectorNx, mesh.vectorNy)
-    zz = -np.exp((xx**2 + yy**2) / 75**2) + mesh.vectorNz[-1]
+# Create a regularization with sparse norms on the model and its
+# gradients.
+reg = Regularization.Sparse(mesh, indActive=actv, mapping=idenMap)
+reg.norms = np.c_[0, 0, 0, 0]
+reg.cell_weights = wr  # Apply sensitivity weights
 
-    # We would usually load a topofile
-    topo = np.c_[Utils.mkvc(xx), Utils.mkvc(yy), Utils.mkvc(zz)]
+reg.mref = np.zeros(nC)
 
-    # Go from topo to actv cells
-    actv = Utils.surface2ind_topo(mesh, topo, 'N')
-    actv = np.asarray([inds for inds, elem in enumerate(actv, 1) if elem],
-                      dtype=int) - 1
+# Data misfit function
+dmis = DataMisfit.l2_DataMisfit(survey)
+dmis.W = 1./survey.std
 
-    # Create active map to go from reduce space to full
-    actvMap = Maps.InjectActiveCells(mesh, actv, -100)
-    nC = len(actv)
+# Add directives to the inversion
+opt = Optimization.ProjectedGNCG(maxIter=30, lower=0., upper=10.,
+                                 maxIterLS=20, maxIterCG=20, tolCG=1e-4)
+invProb = InvProblem.BaseInvProblem(dmis, reg, opt)
+betaest = Directives.BetaEstimate_ByEig()
 
-    # Create and array of observation points
-    xr = np.linspace(-20., 20., 20)
-    yr = np.linspace(-20., 20., 20)
-    X, Y = np.meshgrid(xr, yr)
+# Here is where the norms are applied
+# Use pick a treshold parameter empirically based on the distribution of
+#  model parameters
+IRLS = Directives.Update_IRLS(f_min_change=1e-3, maxIRLSiter=30, beta_tol=5e-1)
+update_Jacobi = Directives.UpdatePreconditioner()
 
-    # Move the observation points 5m above the topo
-    Z = -np.exp((X**2 + Y**2) / 75**2) + mesh.vectorNz[-1] + 5.
+saveOuput = Directives.SaveOutputEveryIteration()
+# saveModel.fileName = work_dir + out_dir + 'ModelSus'
 
-    # Create a MAGsurvey
-    rxLoc = np.c_[Utils.mkvc(X.T), Utils.mkvc(Y.T), Utils.mkvc(Z.T)]
-    rxLoc = PF.BaseMag.RxObs(rxLoc)
-    srcField = PF.BaseMag.SrcField([rxLoc], param=H0)
-    survey = PF.BaseMag.LinearSurvey(srcField)
+inv = Inversion.BaseInversion(
+    invProb, directiveList=[betaest, saveOuput, IRLS, update_Jacobi]
+)
 
-    # We can now create a susceptibility model and generate data
-    # Here a simple block in half-space
-    model = np.zeros((mesh.nCx, mesh.nCy, mesh.nCz))
-    model[(midx-2):(midx+2), (midy-2):(midy+2), -6:-2] = 0.02
-    model = Utils.mkvc(model)
-    model = model[actv]
+# Run the inversion
+m0 = np.ones(nC) * 1e-4  # Starting model
+prob.model = m0
+mrec = inv.run(m0)
 
-    # Create active map to go from reduce set to full
-    actvMap = Maps.InjectActiveCells(mesh, actv, -100)
 
-    # Creat reduced identity map
-    idenMap = Maps.IdentityMap(nP=nC)
+plt.figure()
+ax = plt.subplot()
+mesh.plotSlice(actvMap*mrec, ax=ax, normal='Y', ind=65)
+ax.set_xlim([-300, 300])
+ax.set_ylim([-300, 75])
+ax.set_title('Recovered model.')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
 
-    # Create the forward model operator
-    prob = PF.Magnetics.MagneticIntegral(mesh, chiMap=idenMap, actInd=actv)
-
-    # Pair the survey and problem
-    survey.pair(prob)
-
-    # Compute linear forward operator and compute some data
-    d = prob.fields(model)
-
-    # Add noise and uncertainties
-    # We add some random Gaussian noise (1nT)
-    data = d + np.random.randn(len(d))
-    wd = np.ones(len(data))*1.  # Assign flat uncertainties
-
-    survey.dobs = data
-    survey.std = wd
-    survey.mtrue = model
-
-    # Create sensitivity weights from our linear forward operator
-    rxLoc = survey.srcField.rxList[0].locs
-    wr = np.sum(prob.G**2., axis=0)**0.5
-    wr = (wr/np.max(wr))
-
-    # Create a regularization
-    reg = Regularization.Sparse(mesh, indActive=actv, mapping=idenMap)
-    reg.cell_weights = wr
-    reg.mref = np.zeros(nC)
-    reg.norms = np.c_[0, 1, 1, 1]
-    # reg.eps_p, reg.eps_q = 1e-0, 1e-0
-
-    # Data misfit function
-    dmis = DataMisfit.l2_DataMisfit(survey)
-    dmis.W = 1/wd
-
-    # Add directives to the inversion
-    opt = Optimization.ProjectedGNCG(maxIter=100, lower=0., upper=1.,
-                                     maxIterLS=20, maxIterCG=10, tolCG=1e-3)
-    invProb = InvProblem.BaseInvProblem(dmis, reg, opt)
-    betaest = Directives.BetaEstimate_ByEig(beta0_ratio=1e-1)
-
-    # Here is where the norms are applied
-    # Use pick a treshold parameter empirically based on the distribution of
-    #  model parameters
-    IRLS = Directives.Update_IRLS(
-        f_min_change=1e-4, maxIRLSiter=40
-    )
-    saveDict = Directives.SaveOutputEveryIteration(save_txt=False)
-    update_Jacobi = Directives.UpdatePreconditioner()
-    inv = Inversion.BaseInversion(
-        invProb, directiveList=[IRLS, betaest, update_Jacobi, saveDict]
-    )
-
-    # Run the inversion
-    m0 = np.ones(nC)*1e-4  # Starting model
-    mrec = inv.run(m0)
-
-    if plotIt:
-        # Here is the recovered susceptibility model
-        ypanel = midx
-        zpanel = -5
-        m_l2 = actvMap * invProb.l2model
-        m_l2[m_l2 == -100] = np.nan
-
-        m_lp = actvMap * mrec
-        m_lp[m_lp == -100] = np.nan
-
-        m_true = actvMap * model
-        m_true[m_true == -100] = np.nan
-
-        # Plot the data
-        PF.Magnetics.plot_obs_2D(rxLoc, d=d)
-
-        plt.figure()
-
-        # Plot L2 model
-        ax = plt.subplot(321)
-        mesh.plotSlice(m_l2, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan l2-model.')
-        plt.gca().set_aspect('equal')
-        plt.ylabel('y')
-        ax.xaxis.set_visible(False)
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertica section
-        ax = plt.subplot(322)
-        mesh.plotSlice(m_l2, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W l2-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Plot Lp model
-        ax = plt.subplot(323)
-        mesh.plotSlice(m_lp, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan lp-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('y')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertical section
-        ax = plt.subplot(324)
-        mesh.plotSlice(m_lp, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W lp-model.')
-        plt.gca().set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Plot True model
-        ax = plt.subplot(325)
-        mesh.plotSlice(m_true, ax=ax, normal='Z', ind=zpanel,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCy[ypanel], mesh.vectorCCy[ypanel]]), color='w')
-        plt.title('Plan true model.')
-        plt.gca().set_aspect('equal')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Vertical section
-        ax = plt.subplot(326)
-        mesh.plotSlice(m_true, ax=ax, normal='Y', ind=midx,
-                       grid=True, clim=(model.min(), model.max()))
-        plt.plot(([mesh.vectorCCx[0], mesh.vectorCCx[-1]]),
-                 ([mesh.vectorCCz[zpanel], mesh.vectorCCz[zpanel]]), color='w')
-        plt.title('E-W true model.')
-        plt.gca().set_aspect('equal')
-        plt.xlabel('x')
-        plt.ylabel('z')
-        plt.gca().set_aspect('equal', adjustable='box')
-
-        # Plot convergence curves
-        fig, axs = plt.figure(), plt.subplot()
-        axs.plot(saveDict.phi_d, 'k', lw=2)
-        axs.plot(
-            np.r_[IRLS.iterStart, IRLS.iterStart],
-            np.r_[0, np.max(saveDict.phi_d)], 'k:'
-        )
-
-        twin = axs.twinx()
-        twin.plot(saveDict.phi_m, 'k--', lw=2)
-        axs.text(
-            IRLS.iterStart, 0,
-            'IRLS Steps', va='bottom', ha='center',
-            rotation='vertical', size=12,
-            bbox={'facecolor': 'white'}
-        )
-
-        axs.set_ylabel('$\phi_d$', size=16, rotation=0)
-        axs.set_xlabel('Iterations', size=14)
-        twin.set_ylabel('$\phi_m$', size=16, rotation=0)
-
-if __name__ == '__main__':
-    run()
-    plt.show()
+saveOuput.plot_misfit_curves()
