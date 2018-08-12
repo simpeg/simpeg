e4739dab2e74bd22bfe6c12b1b8bf7831879ba8e
fourndo <fourndo@gmail.com>
2018-08-12 09:01:47 -0700

    Add directives for spherical domain inversion

--- SimPEG/Directives.py
+++ SimPEG/Directives.py
@@ -1081,3 +1081,44 @@ class UpdateSensitivityWeights(InversionDirective):
             JtJdiag += JtJ
 
         self.opt.JtJdiag = JtJdiag
+
+
+class ProjSpherical(InversionDirective):
+    """
+        Trick for spherical coordinate system.
+        Project \theta and \phi angles back to [-\pi,\pi] using
+        back and forth conversion.
+        spherical->cartesian->spherical
+    """
+    def initialize(self):
+
+        x = self.invProb.model
+        # Convert to cartesian than back to avoid over rotation
+        nC = int(len(x)/3)
+
+        xyz = Utils.matutils.atp2xyz(x.reshape((nC, 3), order='F'))
+        m = Utils.matutils.xyz2atp(xyz.reshape((nC, 3), order='F'))
+
+        self.invProb.model = m
+
+        for prob in self.prob:
+            prob.model = m
+
+        self.opt.xc = m
+
+    def endIter(self):
+
+        x = self.invProb.model
+        nC = int(len(x)/3)
+
+        # Convert to cartesian than back to avoid over rotation
+        xyz = Utils.matutils.atp2xyz(x.reshape((nC, 3), order='F'))
+        m = Utils.matutils.xyz2atp(xyz.reshape((nC, 3), order='F'))
+
+        self.invProb.model = m
+        self.invProb.phi_m_last = self.reg(m)
+
+        for prob in self.prob:
+            prob.model = m
+
+        self.opt.xc = m
