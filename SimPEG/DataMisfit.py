3a14a4cdef88f80d7293d4c56a5c05bf0c7f3214
Lindsey Heagy <lindseyheagy@gmail.com>
2018-08-17 14:55:46 -0700

    whitespace cleanup in DataMisfit

--- SimPEG/DataMisfit.py
+++ SimPEG/DataMisfit.py
@@ -100,7 +100,6 @@ class l2_DataMisfit(BaseDataMisfit):
         """
 
         if getattr(self, '_W', None) is None:
-
             survey = self.survey
             self._W = Utils.sdiag(1/(abs(survey.dobs)*self.std+self.eps))
 
