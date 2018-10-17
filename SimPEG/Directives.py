Fix issue with mesh refinement by point. Add scaling of J for tiled objective

SimPEG/Directives.py
@@ -820,10 +820,10 @@ class Update_IRLS(InversionDirective):
             ratio = (self.target / self.invProb.phi_d)
 
             if ratio > 1:
-                ratio = np.mean([2.0, ratio])
+                ratio = np.min([2.0, ratio])
 
             else:
-                ratio = np.mean([0.75, ratio])
+                ratio = np.max([0.75, ratio])
 
             self.invProb.beta = self.invProb.beta * ratio
 

SimPEG/Maps.py
@@ -722,11 +722,11 @@ class Tile(IdentityMap):
 
             sumRow = Utils.mkvc(np.sum(P, axis=1) + self.tol)
 
-            self.AveV = Utils.sdiag(1./sumRow)
+            self.totVol = Utils.sdiag(sumRow)
 
-            self.invVol = Utils.sdiag(1./self.meshGlobal.vol[self.actvGlobal])
+            self.scaleJ = Utils.sdiag(sumRow/self.meshLocal.vol[self.actvLocal])
 
-            self._P = self.AveV * sp.block_diag([P for ii in range(self.nBlock)])
+            self._P = Utils.sdiag(1./sumRow) * sp.block_diag([P for ii in range(self.nBlock)])
 
             self._shape = self.actvLocal.sum(), self.actvGlobal.sum()
 
@@ -778,9 +778,10 @@ class Tile(IdentityMap):
             :return: derivative of transformed model
         """
 
+        self.P
         if v is not None:
-            return self.P * v
-        return self.P
+            return self.scaleJ * self.P * v
+        return self.scaleJ * self.P
 
 
 class SelfConsistentEffectiveMedium(IdentityMap, properties.HasProperties):

SimPEG/Utils/modelutils.py
@@ -350,17 +350,17 @@ def refineTree(mesh, xyz, finalize=False, dtype="point", nCpad=[1, 1, 1]):
         # Reflect in the opposite direction
         vec = np.r_[stencil[::-1], 1, stencil]
         vecX, vecY, vecZ = np.meshgrid(vec, vec, vec)
-        gridLevel = np.maximum(np.maximum(vecX,
-                               vecY), vecZ)
+        gridLevel = np.maximum(np.maximum(np.abs(vecX),
+                               np.abs(vecY)), np.abs(vecZ))
         gridLevel = np.kron(np.ones((xyz.shape[0], 1)), gridLevel)
 
         # Grid the coordinates
-        vec = np.r_[-stencil[::-1], 0, stencil]
+        vec = np.r_[-np.cumsum(stencil)[::-1], 0, np.cumsum(stencil)]
         vecX, vecY, vecZ = np.meshgrid(vec, vec, vec)
         offset = np.c_[
-            mkvc(np.sign(vecX)*2**np.abs(vecX) * mesh.hx.min()),
-            mkvc(np.sign(vecY)*2**np.abs(vecY) * mesh.hx.min()),
-            mkvc(np.sign(vecZ)*2**np.abs(vecZ) * mesh.hx.min())
+            mkvc(np.sign(vecX)*np.abs(vecX) * mesh.hx.min()),
+            mkvc(np.sign(vecY)*np.abs(vecY) * mesh.hy.min()),
+            mkvc(np.sign(vecZ)*np.abs(vecZ) * mesh.hz.min())
         ]
 
         # Replicate the point locations in each offseted grid points