Improve interpolation matrix for Tile with Tree mesh. Fix bug with derivative calc

SimPEG/Maps.py
@@ -643,14 +643,20 @@ class Tile(IdentityMap):
         """
         if getattr(self, '_P', None) is None:
 
-            indx = self.getTreeIndex(self.tree, self.meshLocal, self.actvLocal)
-            local2Global = np.c_[np.kron(np.ones(self.nCell), np.asarray(range(self.actvLocal.sum()))).astype('int'), mkvc(indx)]
+            if self.meshLocal._meshType == "TREE":
+                print("In treeMesh")
+                indx = self.meshLocal._get_containing_cell_indexes(self.meshGlobal.gridCC[self.actvGlobal])
+                print(indx.shape, np.where(self.actvGlobal)[0].shape)
+                full = np.c_[indx, np.where(self.actvGlobal)[0]]
+            else:
+                indx = self.getTreeIndex(self.tree, self.meshLocal, self.actvLocal)
+                local2Global = np.c_[np.kron(np.ones(self.nCell), np.asarray(range(self.actvLocal.sum()))).astype('int'), mkvc(indx)]
 
-            tree = cKDTree(self.meshLocal.gridCC[self.actvLocal, :])
-            r, ind = tree.query(self.meshGlobal.gridCC[self.actvGlobal], k=self.nCell)
-            global2Local = np.c_[np.kron(np.ones(self.nCell), np.asarray(range(self.actvGlobal.sum()))).astype('int'), mkvc(ind)]
+                tree = cKDTree(self.meshLocal.gridCC[self.actvLocal, :])
+                r, ind = tree.query(self.meshGlobal.gridCC[self.actvGlobal], k=self.nCell)
+                global2Local = np.c_[np.kron(np.ones(self.nCell), np.asarray(range(self.actvGlobal.sum()))).astype('int'), mkvc(ind)]
 
-            full = np.unique(np.vstack([local2Global, global2Local[:, [1, 0]]]), axis=0)
+                full = np.unique(np.vstack([local2Global, global2Local[:, [1, 0]]]), axis=0)
 
             # Free up memory
             self._tree = None

SimPEG/PF/Magnetics.py
@@ -74,11 +74,13 @@ class MagneticIntegral(Problem.LinearProblem):
                 fields = da.dot(self.G, (self.Mxyz*m).astype(np.float32))
 
             else:
-                fields = da.dot(self.G, m.astype(np.float32))
+
+                fields = da.dot(self.G, m)
 
             if self.modelType == 'amplitude':
 
                 fields = self.calcAmpData(fields.astype(np.float64))
+
 
         return np.array(fields, dtype='float')
 
@@ -157,26 +159,26 @@ class MagneticIntegral(Problem.LinearProblem):
             else:
                 w = W.diagonal()
 
-            # self.gtgdiag = np.zeros(dmudm.shape[1])
+            self.gtgdiag = np.zeros(dmudm.shape[1])
 
-            # for ii in range(self.G.shape[0]):
+            for ii in range(self.G.shape[0]):
+                # print(self.G[ii, :])
+                self.gtgdiag += (w[ii]*(dmudm.T*self.G[ii, :]).T)**2.
 
-            #     self.gtgdiag += (w[ii]*self.G[ii, :]*dmudm)**2.
-
-            self.gtgdiag = np.array(da.sum(da.power(self.G, 2), axis=0))
+            print(self.gtgdiag.min(), self.gtgdiag.max())
+            # self.gtgdiag = np.array(da.sum(da.power(self.G, 2), axis=0))
 
         if self.coordinate_system == 'cartesian':
             if self.modelType == 'amplitude':
                 return np.sum((W * self.dfdm * sdiag(mkvc(self.gtgdiag)**0.5) * dmudm)**2., axis=0)
             else:
-                Japprox = (sdiag(mkvc(self.gtgdiag)**0.5)*dmudm).T * dmudm
-                return mkvc(np.sum(Japprox.power(2), axis=0))
+                return self.gtgdiag
 
         else:  # spherical
             if self.modelType == 'amplitude':
                 return np.sum(((W * self.dfdm) * da.dot(self.G, (self.dSdm * dmudm)))**2., axis=0)
             else:
-                Japprox = (sdiag(mkvc(self.gtgdiag)**0.5)*dmudm).T * (self.dSdm * dmudm)
+                Japprox = sdiag(mkvc(self.gtgdiag)**0.5*dmudm.T) * (self.dSdm * dmudm)
                 return mkvc(np.sum(Japprox.power(2), axis=0))
 
     def getJ(self, m, f=None):