311627b2a17daf96eb57aa7a3f641347dead49e0
cgohlke <cjgohlke@gmail.com>
2018-12-05 14:04:13 -0800

    Do not install tests in site-packages

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open("README.rst") as f:
 setup(
     name="SimPEG",
     version="0.9.3",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     install_requires=[
         'numpy>=1.7',
         'scipy>=0.13',
