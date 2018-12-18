160334f54f3f8b5d0bb5b3775d00e7fba5ccb8a7
Thibaut Astic <thast@eos.ubc.ca>
2018-12-17 18:01:42 -0800

    add test for DataMisfit std and eps

--- tests/base/test_DataMisfit.py
+++ tests/base/test_DataMisfit.py
@@ -31,8 +31,11 @@ class DataMisfitTest(unittest.TestCase):
 
         prob.pair(survey)
 
+        self.std = 0.01
+        survey.std = self.std
         dobs = survey.makeSyntheticData(model)
-
+        self.eps = 1e-8 * np.min(np.abs(dobs))
+        survey.eps = self.eps
         dmis = DataMisfit.l2_DataMisfit(survey)
 
         self.model = model
@@ -41,6 +44,8 @@ class DataMisfitTest(unittest.TestCase):
         self.prob = prob
         self.dobs = dobs
         self.dmis = dmis
+        import SimPEG
+        print(SimPEG.__path__)
 
     def test_Wd_depreciation(self):
         with self.assertRaises(Exception):
@@ -68,6 +73,18 @@ class DataMisfitTest(unittest.TestCase):
     def test_DataMisfitOrder(self):
         self.dmis.test(x=self.model)
 
+    def test_std_eps(self):
+        stdtest = np.all(self.survey.std == self.dmis.std)
+        epstest = (self.survey.eps == self.dmis.eps)
+        Wtest = np.allclose(
+            np.abs(np.dot(self.dmis.W.todense(), self.dobs)),
+            1./self.std,
+            atol=self.eps
+        )
+
+        self.assertTrue(stdtest)
+        self.assertTrue(epstest)
+        self.assertTrue(Wtest)
 
 if __name__ == '__main__':
     unittest.main()
