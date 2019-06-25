6b2d1999e4c32b4e83d16dec9788a5e688913ddb
Lindsey Heagy <lindseyheagy@gmail.com>
2019-06-25 11:20:13 -0700

    Bump version: 0.11.4 → 0.11.5

--- SimPEG/__init__.py
+++ SimPEG/__init__.py
@@ -23,7 +23,7 @@ from .Utils.SolverUtils import (
     _checkAccuracy, SolverWrapD, SolverWrapI,
     Solver, SolverCG, SolverDiag, SolverLU, SolverBiCG,
 )
-__version__   = '0.11.4'
+__version__   = '0.11.5'
 __author__    = 'SimPEG Team'
 __license__   = 'MIT'
 __copyright__ = '2013 - 2019, SimPEG Team, http://simpeg.xyz'
