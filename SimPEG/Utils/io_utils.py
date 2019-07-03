94b96436b2a12b05e359d6b6a9e1872225789c5e
fourndo <fourndo@gmail.com>
2019-07-03 12:10:35 -0700

    Add FTMG data reader

--- SimPEG/Utils/io_utils.py
+++ SimPEG/Utils/io_utils.py
@@ -5,6 +5,8 @@ import re
 import warnings
 from SimPEG import Utils
 
+
+
 def read_GOCAD_ts(tsfile):
     """
 
@@ -509,3 +511,342 @@ def readVectorUBC(mesh, fileName):
     vModel[:, 2] *= -1
     return vModel
 
+
+class readFTMGdataFile:
+    """
+    Patch object for holding FTMG data
+
+    """
+    #
+
+    def __init__(self, file_path=None):
+        if file_path is not None:
+            # initiate variables
+            self.lines = []
+            self.project_name = ''
+            self.date = ''
+            self.id = ''
+            # read the data file
+            input_file = open(file_path)
+            # extract lines
+            cnt = 0
+            cnt_line = 0
+            header_ids = []
+            lines = []
+            for dline in input_file:
+                # check for header line
+                if '/' in dline:
+                    if cnt == 1:
+                        splt = dline.split('/')
+                        self.project_name = splt[1]
+                    if cnt == 2:
+                        splt = dline.split('/')
+                        self.date = splt[1]
+                    if cnt == 3:
+                        splt = dline.split('/')
+                        self.id = splt[1]
+                    if cnt == 17:
+                        splt = dline.split('/')
+                        header_ids = splt[1].split()
+                    cnt += 1
+                else:
+                    if 'Line' in dline:
+                        if cnt_line == 0:
+                            splt = dline.split()
+                            new_line = ftmgLine(splt[1])
+                            lines.append(new_line)
+                            cnt_line += 1
+                        else:
+                            # print('number of stations: {0} in line: {1}'.format(len(lines[0].stations), lines[0].line_id))
+                            self.lines.append(lines[0])
+                            splt = dline.split()
+                            lines[0] = ftmgLine(splt[1])
+                    else:
+                        text_line = dline.split()
+                        data_dict = {}
+                        # check that we have the same amount of header ids
+                        # as we do data columns
+                        # print("hdr length: {0} & data length {1}".format(len(header_ids), len(text_line)))
+                        if len(header_ids) == len(text_line):
+                            for idx, ids in enumerate(header_ids):
+                                data_dict[ids] = float(text_line[idx])
+                                # print(ids, data_dict[ids])
+                            # create a stations class
+                            lines[0].addStation(ftmgStation(data_obj=data_dict))
+                        else:
+                            print('[ERROR] column id and data column mismatch or blank line found!!!')
+            # print('number of stations: {0} in line: {1}'.format(len(lines[0].stations), lines[0].line_id))
+            self.lines.append(lines[0])
+        else:
+            print("Please define an input file!!!!! If you ain't first your last!!!")
+
+    def getUtmLocations(self, limits=None, ground=False):
+        """
+        Return a numpy array of the easting and northings
+
+        """
+
+        easting = []
+        northing = []
+        elevation = []
+        if limits is None:
+            for line in range(len(self.lines)):
+                for stn in range(len(self.lines[line].stations)):
+                    easting.append(self.lines[line].stations[stn].easting)
+                    northing.append(self.lines[line].stations[stn].northing)
+                    # see if ground elevation is requested
+                    if ground:
+                        z_ground = (self.lines[line].stations[stn].alt_gps -
+                                    self.lines[line].stations[stn].radar)
+                        elevation.append(z_ground)
+                    else:
+                        elevation.append(self.lines[line].stations[stn].alt_gps)
+        else:
+            for line in range(len(self.lines)):
+                for stn in range(len(self.lines[line].stations)):
+                    x_stn = self.lines[line].stations[stn].easting
+                    y_stn = self.lines[line].stations[stn].northing
+                    # check that station is within the limits
+                    if limits[0] < x_stn < limits[1] and limits[2] < y_stn < limits[3]:
+                        easting.append(self.lines[line].stations[stn].easting)
+                        northing.append(self.lines[line].stations[stn].northing)
+                        # see if ground elevation is requested
+                        if ground:
+                            z_ground = (self.lines[line].stations[stn].alt_gps -
+                                        self.lines[line].stations[stn].radar)
+                            elevation.append(z_ground)
+                        else:
+                            elevation.append(self.lines[line].stations[stn].alt_gps)
+
+        return np.asarray(easting), np.asarray(northing), np.asarray(elevation)
+
+    def getComponentData(self, component=None, limits=None, inducing_field=None):
+        output_comp = []
+
+        if component.lower() == 'tmi':
+            assert inducing_field is not None, "Inducing field parameters must be provided for 'TMI' data"
+
+        # check for data extents requested
+
+        # check thats components are supplied
+        if component is not None:
+            for line in range(len(self.lines)):
+                for stn in range(len(self.lines[line].stations)):
+
+                    if limits is not None:
+                        x_stn = self.lines[line].stations[stn].easting
+                        y_stn = self.lines[line].stations[stn].northing
+
+                        if not (limits[0] < x_stn < limits[1]) and (limits[2] < y_stn < limits[3]):
+                            continue
+
+                    # check if component was aquired
+                    if component in self.lines[line].stations[stn].comps:
+
+                        if component.lower() == 'tmi':
+                            output_comp.append(self.lines[line].stations[stn].comps[component] - inducing_field[0])
+                        else:
+                            output_comp.append(self.lines[line].stations[stn].comps[component])
+        # else:
+        #     # check thats components are supplied
+        #     if component is not None:
+        #         for line in range(len(self.lines)):
+        #             for stn in range(len(self.lines[line].stations)):
+        #                 x_stn = self.lines[line].stations[stn].easting
+        #                 y_stn = self.lines[line].stations[stn].northing
+        #                 # check that station is within the limits
+        #                 if limits[0] < x_stn < limits[1] and limits[2] < y_stn < limits[3]:
+        #                     # check if component was aquired
+        #                     if component in self.lines[line].stations[stn].comps:
+        #                         output_comp.append(self.lines[line].stations[stn].comps[component])
+
+        return output_comp
+
+    def createFtmgSurvey(self, inducing_field=None, limits=None, force_comp=None):
+        """
+        Returns a SimPEF survey Object for FTMG to be fed into inversion scheme
+
+        """
+        print("TODO")
+        # check that we have an inducing field
+        from SimPEG.PF import BaseMag
+        if inducing_field is not None:
+            # check data extents are requested
+            if limits is None:
+                # check that we have the proper amount of info in array
+                if inducing_field.shape[0] == 3:
+                    # get observation data
+                    x, y, z = self.getUtmLocations()
+                    comps = self.getListOfAvailableComponents(format_type='simpeg', force_comp=force_comp)
+                    # Create a MAGsurvey
+                    rxLoc = np.c_[Utils.mkvc(x.T), Utils.mkvc(y.T), Utils.mkvc(z.T)]
+                    Rx = BaseMag.RxObs(rxLoc)
+                    srcField = BaseMag.SrcField([Rx], param=inducing_field)
+                    survey = BaseMag.LinearSurvey(srcField, components=comps)
+                    # create dobs
+                    dobs_list = []
+                    components = self.getListOfAvailableComponents(force_comp=force_comp)
+                    for comp in components:
+                        if comp is not 'TMI':
+                            print('component added: {0}'.format(comp))
+                            dobs_list.append(self.getComponentData(component=comp, inducing_field=inducing_field))
+                    # now sort the data to SimPEG format
+                    dobs = np.vstack(dobs_list)
+                    size = dobs.shape[0] * dobs.shape[1]
+                    survey.dobs = np.reshape(dobs, size, order='F')
+
+                    return survey
+
+                else:
+                    print('[ERROR] array with Inducing field parameters must be size 3!!!')
+            else:
+                # get locations with limits
+                x, y, z = self.getUtmLocations(limits=limits)
+                comps = self.getListOfAvailableComponents(format_type='simpeg', force_comp=force_comp)
+                # Create a MAGsurvey
+                rxLoc = np.c_[Utils.mkvc(x.T), Utils.mkvc(y.T), Utils.mkvc(z.T)]
+                Rx = BaseMag.RxObs(rxLoc)
+                srcField = BaseMag.SrcField([Rx], param=inducing_field)
+                survey = BaseMag.LinearSurvey(srcField, components=comps)
+                # create dobs
+                dobs_list = []
+                components = self.getListOfAvailableComponents(force_comp=force_comp)
+                for comp in components:
+                    if comp is not 'TMI':
+                        print('component added: {0}'.format(comp))
+                        dobs_list.append(self.getComponentData(component=comp, limits=limits))
+                # now sort the data to SimPEG format
+                dobs = np.vstack(dobs_list)
+                size = dobs.shape[0] * dobs.shape[1]
+                survey.dobs = np.reshape(dobs, size, order='F')
+
+                return survey
+
+        else:
+            print('[ERROR] Please include numpy array with Inducing field parameters!!!')
+
+    def getListOfAvailableComponents(self, keep_tmi=False, format_type='std', force_comp=None):
+        """
+        Returns the components collected for given readFTMGdata
+
+        """
+        available_components = self.lines[0].stations[0].available_components
+        if force_comp is not None:
+            available_components = force_comp
+        list_ = []
+        if not keep_tmi:
+            for comp in available_components:
+                    if comp is not 'TMI':
+                        list_.append(comp)
+        else:
+            list_ = available_components
+
+        if format_type == 'simpeg':
+            list_bu = list_
+            list_ = []
+            if 'Bxx' in list_bu:
+                list_.append("dbx_dx")
+            if 'Byy' in list_bu:
+                list_.append("dby_dy")
+            if 'Bzz' in list_bu:
+                list_.append("dbz_dz")
+            if 'Bxz' in list_bu:
+                list_.append("dbx_dz")
+            if 'Bxy' in list_bu:
+                list_.append("dbx_dy")
+            if 'Byz' in list_bu:
+                list_.append("dby_dz")
+            if 'Bz' in list_bu:
+                list_.append("bz")
+            if 'Bx' in list_bu:
+                list_.append("bx")
+            if 'By' in list_bu:
+                list_.append("by")
+        return list_
+
+
+class ftmgLine:
+    """
+    Line class for storing the observations
+
+    """
+
+    def __init__(self, line_id):
+        self.stations = []
+        self.line_id = line_id
+
+    def addStation(self, station):
+        self.stations.append(station)
+
+
+class ftmgStation:
+    """
+    station class for hold station observation info
+
+    """
+
+    def __init__(self, data_obj=None):
+        self.available_components = []
+        self.comps = {}
+        self.easting = None
+        self.northing = None
+        self.lat = None
+        self.lon = None
+        self.alt_gps = None
+        self.radar = None
+        self.date = None
+        self.gps_sec = None
+        self.I1 = None
+        self.I2 = None
+        if data_obj is not None:
+            # assign all availble data
+            self.easting = data_obj['x']
+            self.northing = data_obj['y']
+            self.lat = data_obj['lat']
+            self.lon = data_obj['lon']
+            self.alt_gps = data_obj['alt_gps']
+            self.radar = data_obj['radar']
+            self.date = data_obj['date']
+            self.gps_sec = data_obj['GPS_sec']
+            if 'Bxx' in data_obj:
+                # self.Bxx = data_obj['Bxx']
+                self.comps['Bxx'] = data_obj['Bxx']
+                self.available_components.append('Bxx')
+            if 'Bxy' in data_obj:
+                self.comps['Bxy'] = data_obj['Bxy']
+                # self.Bxy = data_obj['Bxy']
+                self.available_components.append('Bxy')
+            if 'Bxz' in data_obj:
+                self.comps['Bxz'] = data_obj['Bxz']
+                # self.Bxz = data_obj['Bxz']
+                self.available_components.append('Bxz')
+            if 'Byy' in data_obj:
+                self.comps['Byy'] = data_obj['Byy']
+                # self.Byy = data_obj['Byy']
+                self.available_components.append('Byy')
+            if 'Byz' in data_obj:
+                self.comps['Byz'] = data_obj['Byz']
+                # self.Byz = data_obj['Byz']
+                self.available_components.append('Byz')
+            if 'Bzz' in data_obj:
+                self.comps['Bzz'] = data_obj['Bzz']
+                # self.Bzz = data_obj['Bzz']
+                self.available_components.append('Bzz')
+            if 'Bx' in data_obj:
+                self.comps['Bx'] = data_obj['Bx']
+                # self.Bx = data_obj['Bx']
+                self.available_components.append('Bx')
+            if 'By' in data_obj:
+                self.comps['By'] = data_obj['By']
+                # self.By = data_obj['By']
+                self.available_components.append('By')
+            if 'Bz' in data_obj:
+                self.comps['Bz'] = data_obj['Bz']
+                # self.Bz = data_obj['Bz']
+                self.available_components.append('Bz')
+            if 'TMI' in data_obj:
+                self.comps['TMI'] = data_obj['TMI']
+                # self.tmi = data_obj['TMI']
+                self.available_components.append('TMI')
+            self.I1 = data_obj['I1']
+            self.I2 = data_obj['I2']
