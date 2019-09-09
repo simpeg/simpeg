4fb417439883ac59c4a426761c40319216e93c2e
Thibaut Astic <thast@eos.ubc.ca>
2019-09-09 11:58:21 -0700

    fix mapping derivatives

--- SimPEG/ObjectiveFunction.py
+++ SimPEG/ObjectiveFunction.py
@@ -482,4 +482,4 @@ class L2ObjectiveFunction(BaseObjectiveFunction):
                 )
             )
         W = self.W * self.mapping.deriv(m)
-        return self.W.T * self.W
+        return W.T * W
