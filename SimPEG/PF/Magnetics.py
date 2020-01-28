Fix missing sign. Yikes

SimPEG/PF/Magnetics.py
@@ -1319,7 +1319,7 @@ def calcRow(
                         (np.log(arg14) - np.log(arg19)) +
                         (np.log(arg24) - np.log(arg29)) +
                         (np.log(arg34) - np.log(arg39)))
-            bx /= (4 * np.pi)
+            bx /= (-4 * np.pi)
 
             # rows += [bx]
 