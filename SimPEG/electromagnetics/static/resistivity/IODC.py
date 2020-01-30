3410aa85de8cc0591b98b17719d2106900b32052
seogi_macbook <sgkang09@gmail.com>
2020-01-30 10:47:59 -0800

    use second option of kai

--- SimPEG/electromagnetics/static/resistivity/IODC.py
+++ SimPEG/electromagnetics/static/resistivity/IODC.py
@@ -529,22 +529,28 @@ class IO(properties.HasProperties):
             else:
                 # For 2D mesh
                 if dimension == 2:
-                    # sort by x
                     mask = np.isin(self.electrode_locations[:, 0], topo[:, 0])
                     if np.any(mask):
-                        raise RuntimeError("the x coordinates of some topo and electrodes are the same.")
+                        warnings.warn(
+                            "Because the x coordinates of some topo and electrodes are the same,"
+                            " we excluded electrodes with the same coordinates.",
+                            RuntimeWarning
+                        )
                     locs_tmp = np.vstack((topo, self.electrode_locations[~mask, :]))
                     row_idx = np.lexsort((locs_tmp[:, 0],))
-                # For 3D mesh
                 else:
-                    # sort by x, then by y
                     dtype = [('x', np.float64), ('y', np.float64)]
                     mask = np.isin(self.electrode_locations[:, [0, 1]].copy().view(dtype),
                                    topo[:, [0, 1]].copy().view(dtype)).flatten()
                     if np.any(mask):
-                        raise RuntimeError("the x and y coordinates of some topo and electrodes are the same.")
+                        warnings.warn(
+                            "Because the x and y coordinates of some topo and electrodes are the same,"
+                            " we excluded electrodes with the same coordinates.",
+                            RuntimeWarning
+                        )
                     locs_tmp = np.vstack((topo, self.electrode_locations[~mask, :]))
-                    row_idx = np.lexsort((locs_tmp[:, 1], locs_tmp[:, 0]))
+                    row_idx = np.lexsort((locs_tmp[:, 1],
+                                          locs_tmp[:, 0]))
                 locs = locs_tmp[row_idx, :]
 
             if dx > dx_ideal:
