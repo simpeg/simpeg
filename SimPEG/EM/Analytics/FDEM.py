24610112e4cb097b5d4b69eab1b236033cdc3bd2
prisae <dieter@werthmuller.org>
2020-02-17 14:16:48 +0100

    Fix Ward and Hohmann example

--- SimPEG/EM/Analytics/FDEM.py
+++ SimPEG/EM/Analytics/FDEM.py
@@ -7,21 +7,36 @@ from SimPEG import Utils
 
 def hzAnalyticDipoleF(r, freq, sigma, secondary=True, mu=mu_0):
     """
-    4.56 in Ward and Hohmann
+    4.2 in Ward and Hohmann, 1988.
 
     .. plot::
 
+        import numpy as np
         import matplotlib.pyplot as plt
         from SimPEG import EM
-        freq = np.logspace(-1, 6, 61)
-        test = EM.Analytics.FDEM.hzAnalyticDipoleF(100, freq, 0.001, secondary=False)
-        plt.loglog(freq, abs(test.real))
-        plt.loglog(freq, abs(test.imag))
-        plt.title('Response at $r$=100m')
-        plt.xlabel('Frequency')
-        plt.ylabel('Response')
-        plt.legend(('real','imag'))
+        freq = np.logspace(-1, 5, 301)
+        test = EM.Analytics.FDEM.hzAnalyticDipoleF(
+                100, freq, 0.01, secondary=False)
+        plt.loglog(freq, test.real, 'C0-', label='Real')
+        plt.loglog(freq, -test.real, 'C0--')
+        plt.loglog(freq, test.imag, 'C1-', label='Imaginary')
+        plt.loglog(freq, -test.imag, 'C1--')
+        plt.title('Response at $r=100$ m')
+        plt.xlim([1e-1, 1e5])
+        plt.ylim([1e-12, 1e-6])
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('$H_z$ (A/m)')
+        plt.legend(loc=6)
         plt.show()
+
+
+    **Reference**
+
+    - Ward, S. H., and G. W. Hohmann, 1988, Electromagnetic theory for
+      geophysical applications, Chapter 4 of Electromagnetic Methods in Applied
+      Geophysics: SEG, Investigations in Geophysics No. 3, 130--311; DOI:
+      `10.1190/1.9781560802631.ch4
+      <https://doi.org/10.1190/1.9781560802631.ch4>`_.
 
     """
     r = np.abs(r)
