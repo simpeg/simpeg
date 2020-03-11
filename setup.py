eabd8bbe6aee0f1aa13ec46979ad7891d6ba9e63
Joseph Capriotti <jcapriot@mines.edu>
2020-03-11 16:33:09 -0600

    Bump version: 0.14.0b0 → 0.14.0b1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.rst") as f:
 
 setup(
     name="SimPEG",
-    version="0.14.0b0",
+    version="0.14.0b1",
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'numpy>=1.7',
