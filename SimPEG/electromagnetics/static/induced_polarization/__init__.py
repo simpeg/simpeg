9b4f9081f6ce3c297574c46e9f3236c2af4e1bf8
Lindsey Heagy <lindseyheagy@gmail.com>
2020-06-02 20:11:09 -0600

    import dc utils under ip as well

--- SimPEG/electromagnetics/static/induced_polarization/__init__.py
+++ SimPEG/electromagnetics/static/induced_polarization/__init__.py
@@ -4,6 +4,7 @@ from .survey import Survey, from_dc_to_ip_survey
 from .run import run_inversion
 from ..resistivity import receivers
 from ..resistivity import sources
+from ..resistivity import utils
 
 Simulation2DCellCentred = Simulation2DCellCentered
 Simulation3DCellCentred = Simulation2DCellCentered
