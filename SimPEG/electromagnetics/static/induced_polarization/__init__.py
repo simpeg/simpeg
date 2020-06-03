3e90112c331e1fd63f838338b40746e1fd1e1473
Lindsey Heagy <lindseyheagy@gmail.com>
2020-06-02 20:05:57 -0600

    add sources and receivers from static under the ip namespace

--- SimPEG/electromagnetics/static/induced_polarization/__init__.py
+++ SimPEG/electromagnetics/static/induced_polarization/__init__.py
@@ -2,6 +2,8 @@ from .simulation import Simulation3DCellCentered, Simulation3DNodal
 from .simulation_2d import Simulation2DCellCentered, Simulation2DNodal
 from .survey import Survey, from_dc_to_ip_survey
 from .run import run_inversion
+from ..resistivity import receivers
+from ..resistivity import sources
 
 Simulation2DCellCentred = Simulation2DCellCentered
 Simulation3DCellCentred = Simulation2DCellCentered
