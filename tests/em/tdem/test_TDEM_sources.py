cf2f2ade24aebae2156e725e5f482610eee1ab1b
I-Kang Ding <ikang.ding@koboldmetals.com>
2020-06-15 18:08:47 -0400

    Add unit tests for other sources

--- tests/em/tdem/test_TDEM_sources.py
+++ tests/em/tdem/test_TDEM_sources.py
@@ -18,16 +18,79 @@ class TestStepOffWaveform(unittest.TestCase):
     def setUpClass(cls):
         cls.times = np.linspace(start=0, stop=1e-2, num=11)
 
-    def test_waveform_with_default_eps(self):
+    def test_waveform_with_default_off_time(self):
         step_off = StepOffWaveform()
         result = [step_off.eval(t) for t in self.times]
         expected = np.array([1.0] + [0.0] * 10)
         assert_array_almost_equal(result, expected)
 
     def test_waveform_with_custom_off_time(self):
+        """For StepOffWaveform, offTime arg does not do anything."""
         step_off = StepOffWaveform(offTime=1e-3)
         result = [step_off.eval(t) for t in self.times]
-        expected = np.array([0.0, 1.0] + [0.0] * 9)
+        expected = np.array([1.0] + [0.0] * 10)
+        assert_array_almost_equal(result, expected)
+
+
+class TestRampOffWaveform(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.times = np.linspace(start=0, stop=1e-2, num=11)
+
+    def test_waveform_with_whole_offtime(self):
+        ramp_off = RampOffWaveform(offTime=1e-2)
+        result = [ramp_off.eval(t) for t in self.times]
+        expected = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0])
+        assert_array_almost_equal(result, expected)
+
+    def test_waveform_with_partial_off_time(self):
+        ramp_off = RampOffWaveform(offTime=5e-3)
+        result = [ramp_off.eval(t) for t in self.times]
+        expected = np.array([1.0, 0.8, 0.6, 0.4, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        assert_array_almost_equal(result, expected)
+
+
+class TestVTEMWaveform(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.times = np.linspace(start=0, stop=1e-2, num=11)
+
+    def test_waveform_with_default_param(self):
+        vtem = VTEMWaveform()
+        result = [vtem.eval(t) for t in self.times]
+        expected = np.array(
+            [0.0, 0.701698, 0.93553, 0.816327, 0.136054, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        )
+        assert_array_almost_equal(result, expected)
+
+    def test_waveform_with_custom_param(self):
+        vtem = VTEMWaveform(offTime=8e-3, peakTime=4e-3, a=2.0)
+        result = [vtem.eval(t) for t in self.times]
+        expected = np.array(
+            [0.0, 0.455054, 0.731059, 0.898464, 1.0, 0.75, 0.5, 0.25, 0.0, 0.0, 0.0]
+        )
+        assert_array_almost_equal(result, expected)
+
+
+class TestTrapezoidWaveform(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.times = np.linspace(start=0, stop=1e-2, num=11)
+
+    def test_waveform_with_symmetric_on_off(self):
+        trapezoid = TrapezoidWaveform(
+            ramp_on=np.r_[0.0, 2e-3], ramp_off=np.r_[6e-3, 8e-3]
+        )
+        result = [trapezoid.eval(t) for t in self.times]
+        expected = np.array([0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 0.0, 0.0])
+        assert_array_almost_equal(result, expected)
+
+    def test_waveform_with_asymmetric_on_off(self):
+        trapezoid = TrapezoidWaveform(
+            ramp_on=np.r_[0.0, 2e-3], ramp_off=np.r_[6e-3, 10e-3]
+        )
+        result = [trapezoid.eval(t) for t in self.times]
+        expected = np.array([0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 0.75, 0.5, 0.25, 0.0])
         assert_array_almost_equal(result, expected)
 
 
