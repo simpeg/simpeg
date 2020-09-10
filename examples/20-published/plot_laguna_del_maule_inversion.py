edb2a73792d75d78ef8c9514d572b749dca6826a
domfournier <dominiquef@mirageoscience.com>
2020-09-10 16:28:24 -0700

    Update plot_laguna_del_maule_inversion.py
    As requested by @craigmillernz

--- examples/20-published/plot_laguna_del_maule_inversion.py
+++ examples/20-published/plot_laguna_del_maule_inversion.py
@@ -153,7 +153,11 @@ def run(plotIt=True, cleanAfterRun=True):
     if plotIt:
         # Plot observed data
         # The sign of the data is flipped here for the change of convention 
-        # between Cartesian (SimPEG) and traditional gravity data (positive down)
+        # between Cartesian coordinate system (internal SimPEG format that 
+        # expects "positive up" gravity signal) and traditional gravity data
+        # conventions (positive down). For example a traditional negative 
+        # gravity anomaly is described as "positive up" in Cartesian coordinates
+        # and hence the sign needs to be flipped for use in SimPEG.
         plot2Ddata(rxLoc, -d)
 
         # %%
