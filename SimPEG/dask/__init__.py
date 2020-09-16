a60a4e9d71989a2cbe1a8500fa20c63f01ce5b66
Juan <john.kuttai@gmail.com>
2020-09-15 22:08:41 -0700

    Start of tiled simulations code; initial structuring with dask versions of data misfit and combo objective function. Some dask functions are dummy for now.

--- SimPEG/dask/__init__.py
+++ SimPEG/dask/__init__.py
@@ -6,6 +6,8 @@ try:
     import SimPEG.dask.potential_fields.base
     import SimPEG.dask.potential_fields.gravity.simulation
     import SimPEG.dask.potential_fields.magnetics.simulation
+    import SimPEG.dask.data_misfit
+    import SimPEG.dask.objective_function
 except ImportError as err:
     print("unable to load dask operations")
     print(err)
