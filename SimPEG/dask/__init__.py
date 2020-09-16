652a2758680fdfd03048e858d33a1b883945185b
Juan <john.kuttai@gmail.com>
2020-09-16 12:42:31 -0700

    Added code for start of tiled simulations configuration

--- SimPEG/dask/__init__.py
+++ SimPEG/dask/__init__.py
@@ -2,9 +2,12 @@ try:
     import SimPEG.dask.simulation
     import SimPEG.dask.electromagnetics.static.resistivity.simulation
     import SimPEG.dask.electromagnetics.static.induced_polarization.simulation
+    import SimPEG.dask.electromagnetics.natural_source.simulation
     import SimPEG.dask.potential_fields.base
     import SimPEG.dask.potential_fields.gravity.simulation
     import SimPEG.dask.potential_fields.magnetics.simulation
+    import SimPEG.dask.data_misfit
+    import SimPEG.dask.objective_function
 except ImportError as err:
     print("unable to load dask operations")
     print(err)
