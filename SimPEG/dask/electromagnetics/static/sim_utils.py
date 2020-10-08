77a05848c193a1ef29519b8e85a0fa0846036b5a
Juan <john.kuttai@gmail.com>
2020-10-08 09:56:28 -0700

    created tiling method for dcip

--- SimPEG/dask/electromagnetics/static/sim_utils.py
+++ SimPEG/dask/electromagnetics/static/sim_utils.py
@@ -1,6 +1,6 @@
 from .... import maps, utils
 import discretize
-from discretize.utils import mkvc, refine_tree_xyz
+from discretize.utils import mesh_builder_xyz, refine_tree_xyz, mkvc
 from ....electromagnetics import resistivity as dc
 from ....electromagnetics.static.utils import StaticUtils as dcutils
 from pymatsolver import Pardiso as Solver
@@ -8,9 +8,9 @@ import numpy as np
 
 
 def create_sub_simulations(source_list,
+                           global_electrodes,
                            dem,
                            base_mesh,
-                           global_active,
                            oct_levels=[5, 3, 2, 2],
                            remote_quadrant=0,
                            output_meshes=False,
@@ -18,106 +18,111 @@ def create_sub_simulations(source_list,
                            padLen=800,
                            remote_present=True):
 
-    sub_survey = dc.Survey(source_list)
-    electrodes = np.vstack((sub_survey.locations_a,
-                            sub_survey.locations_b,
-                            sub_survey.locations_m,
-                            sub_survey.locations_n))
-
-    flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
-    remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
-
-    # figure remote location so discretization is good there
-    if remote_quadrant == 1:
-        flag_remote = electrodes[:, 1] > electrodes[:, 1].max()
-        remote_only = electrodes[:, 1] <= electrodes[:, 1].max()
-    elif remote_quadrant == 2:
-        flag_remote = electrodes[:, 0] > electrodes[:, 0].max()
-        remote_only = electrodes[:, 0] <= electrodes[:, 0].max()
-    elif remote_quadrant == 3:
-        flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
-        remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
-    elif remote_quadrant == 4:
-        flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
-        remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
-
-    # create databases
-    no_remote_db = electrodes[flag_remote]
-    remote_db = electrodes[remote_only]
-
     # combine topo
-    topo = np.vstack((dem, electrodes))
+    topography = np.vstack((dem, global_electrodes))
+    padding_distance = np.r_[np.c_[padLen, padLen],
+                                 np.c_[padLen, padLen], np.c_[padLen, padLen]]
     # creating mesh using Discretize!
     h = [base_mesh.hx.min(), base_mesh.hy.min(), base_mesh.hy.min()]
-
-    padding_distance = np.r_[np.c_[padLen, padLen],
-                             np.c_[padLen, padLen], np.c_[padLen, padLen]]
-
-    mesh = discretize.utils.mesh_builder_xyz(
-        electrodes, h,
+    # create global mesh
+    global_mesh = mesh_builder_xyz(
+        global_electrodes, h,
         padding_distance=padding_distance,
         mesh_type='TREE', base_mesh=base_mesh,
         depth_core=depth_core
     )
-    mesh = discretize.utils.refine_tree_xyz(
-        mesh, topo,
-        method='surface', octree_levels=[1, 1, 0],
-        finalize=False
-    )
+    local_meshes = []
 
-    if remote_present:
-        mesh = discretize.utils.refine_tree_xyz(
-            mesh, remote_db,
-            method='radial', octree_levels=[2, 1, 1],
+    for source in source_list:
+        sub_survey = dc.Survey([source])
+        electrodes = np.vstack((sub_survey.locations_a,
+                                sub_survey.locations_b,
+                                sub_survey.locations_m,
+                                sub_survey.locations_n))
+
+        flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
+        remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
+
+        # figure remote location so discretization is good there
+        if remote_quadrant == 1:
+            flag_remote = electrodes[:, 1] > electrodes[:, 1].max()
+            remote_only = electrodes[:, 1] <= electrodes[:, 1].max()
+        elif remote_quadrant == 2:
+            flag_remote = electrodes[:, 0] > electrodes[:, 0].max()
+            remote_only = electrodes[:, 0] <= electrodes[:, 0].max()
+        elif remote_quadrant == 3:
+            flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
+            remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
+        elif remote_quadrant == 4:
+            flag_remote = electrodes[:, 1] > electrodes[:, 1].min()
+            remote_only = electrodes[:, 1] <= electrodes[:, 1].min()
+
+        # create databases
+        no_remote_db = electrodes[flag_remote]
+        remote_db = electrodes[remote_only]
+
+
+        padding_distance = np.r_[np.c_[padLen, padLen],
+                                 np.c_[padLen, padLen], np.c_[padLen, padLen]]
+
+        local_mesh = discretize.utils.mesh_builder_xyz(
+            global_electrodes, h,
+            padding_distance=padding_distance,
+            mesh_type='TREE', base_mesh=base_mesh,
+            depth_core=depth_core
+        )
+        local_mesh = discretize.utils.refine_tree_xyz(
+            local_mesh, topography,
+            method='surface', octree_levels=[1, 1, 0],
             finalize=False
         )
-        mesh = discretize.utils.refine_tree_xyz(
-            mesh, no_remote_db,
-            method='surface', octree_levels=oct_levels,
-            # octree_levels_padding=[4, 4, 2, 2],
-            finalize=True
+
+        if remote_present:
+            local_mesh = discretize.utils.refine_tree_xyz(
+                local_mesh, remote_db,
+                method='radial', octree_levels=[2, 1, 1],
+                finalize=False
+            )
+            local_mesh = discretize.utils.refine_tree_xyz(
+                local_mesh, no_remote_db,
+                method='surface', octree_levels=oct_levels,
+                # octree_levels_padding=[4, 4, 2, 2],
+                finalize=True
+            )
+        else:
+            local_mesh = discretize.utils.refine_tree_xyz(
+                local_mesh, electrodes,
+                method='surface', octree_levels=oct_levels,
+                # octree_levels_padding=[4, 4, 2, 2],
+                finalize=True
+            )
+        global_mesh.insert_cells(
+            local_mesh.gridCC,
+            local_mesh.cell_levels_by_index(np.arange(local_mesh.nC)),
+            finalize=False,
         )
-    else:
-        mesh = discretize.utils.refine_tree_xyz(
-            mesh, electrodes,
-            method='surface', octree_levels=oct_levels,
-            # octree_levels_padding=[4, 4, 2, 2],
-            finalize=True
-        )
 
-    if output_meshes:
-        discretize.TreeMesh.writeUBC(
-            mesh, 'OctreeMesh-pre.msh',
-            models={'SigmaOctree-pre.dat': np.ones(mesh.nC)}
-        )
+        local_meshes.append(local_mesh)
 
-    # actinds = mesh.gridCC[:, 2] < 0
-    # local_active = utils.surface2ind_topo(mesh, topo, method='linear')
-    # print(actinds.shape)
+        if output_meshes:
+            discretize.TreeMesh.writeUBC(
+                local_mesh, 'OctreeMesh-pre.msh',
+                models={'SigmaOctree-pre.dat': np.ones(local_mesh.nC)}
+            )
 
-    # actmap = maps.InjectActiveCells(
-    #     mesh, indActive=actinds, valInactive=np.log(1e-8)
-    # )
-    # mapping = maps.ExpMap(mesh) * actmap
-
-    tile_map = maps.TileMap(base_mesh, global_active, mesh)
-
-    # local active
-    local_active = tile_map.local_active
-    print(local_active.shape, local_active.sum(), mesh.nC, base_mesh.nC)
-    # drape topo
-    sub_survey.drape_electrodes_on_topography(mesh, local_active)
-
-    # Generate 3D DC problem
-    # "CC" means potential is defined at center
-    sim = dc.Simulation3DCellCentered(
-        mesh, survey=sub_survey, sigmaMap=tile_map, storeJ=False,
-        Solver=Solver
+    global_mesh.finalize()
+    global_active = utils.surface2ind_topo(
+        global_mesh, topography, method='linear'
     )
 
-    del mesh
-    sim.actinds = local_active
-    return sim
+    # Cycle back to all local meshes and create tile maps
+    local_maps = []
+    for mesh in local_meshes:
+        local_maps.append(
+            maps.TileMap(global_mesh, global_active, mesh)
+        )
+
+    return (global_mesh, global_active), (local_meshes, local_maps)
 
 
 def create_sub_simulations_old(source_list,
