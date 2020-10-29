37e559348550ac0f68b9e167f67a73b108540fb1
dccowan <devinccowan@gmail.com>
2020-10-28 20:22:09 -0700

    Add IO for UBC gravity gradiometry and fix stability of gravity gradiometry sensitivities

--- SimPEG/utils/io_utils.py
+++ SimPEG/utils/io_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import time as tm
 import re
 import warnings
+from discretize.utils import mkvc
 
 
 def read_GOCAD_ts(tsfile):
@@ -280,10 +281,10 @@ def readUBCmagneticsObservations(obs_file):
     fid.close()
 
     if np.all(wd==0.):
-    	wd = None
+        wd = None
 
     if np.all(d==0.):
-    	d = None
+        d = None
 
     rxLoc = magnetics.receivers.Point(locXYZ)
     srcField = magnetics.sources.SourceField([rxLoc], parameters=(B[2], B[0], B[1]))
@@ -318,10 +319,10 @@ def writeUBCmagneticsObservations(filename, data_object):
     data = survey.source_field.receiver_list[0].locations
     
     if data_object.dobs is not None:
-    	data = np.c_[data, data_object.dobs]
+        data = np.c_[data, data_object.dobs]
     
     if data_object.standard_deviation is not None:
-    	data = np.c_[data, data_object.standard_deviation]
+        data = np.c_[data, data_object.standard_deviation]
 
     head = (
         "%6.2f %6.2f %6.2f\n" % (B[1], B[2], B[0])
@@ -380,10 +381,10 @@ def readUBCgravityObservations(obs_file):
     fid.close()
 
     if np.all(wd == 0.):
-    	wd = None
+        wd = None
 
     if np.all(d == 0.):
-    	d = None
+        d = None
 
     rxLoc = gravity.receivers.Point(locXYZ)
     srcField = gravity.sources.SourceField([rxLoc])
@@ -407,12 +408,151 @@ def writeUBCgravityObservations(filename, data_object):
     data = survey.source_field.receiver_list[0].locations
     
     if data_object.dobs is not None:
-    	data = np.c_[data, data_object.dobs]
+        data = np.c_[data, data_object.dobs]
     
     if data_object.standard_deviation is not None:
-    	data = np.c_[data, data_object.standard_deviation]
+        data = np.c_[data, data_object.standard_deviation]
 
     head = "%i\n" % survey.nD
+    np.savetxt(
+        filename, data, fmt="%e", delimiter=" ", newline="\n", header=head, comments=""
+    )
+
+    print("Observation file saved to: " + filename)
+
+
+
+def readUBCgravitygradiometryObservations(obs_file, file_type):
+    """
+    Read UBC gravity gradiometry file format
+
+    INPUT:
+    :param fileName, path to the UBC obs gravity gradiometry file
+    :param file_type, 'dobs' 'dpred' 'survey'
+
+    OUTPUT:
+    :param survey
+
+    """
+    from SimPEG.potential_fields import gravity
+    from SimPEG import data
+
+    fid = open(obs_file, "r")
+
+    # First line has components. Extract components
+    line = fid.readline()
+    line = line.split('=')[1].split('!')[0].split('\n')[0]
+    line = line.replace(',', ' ').split(' ')  # UBC uses ',' or ' ' as deliminator
+    components = [s for s in line if len(s)>0]  # Remove empty string
+    factor = np.zeros(len(components))
+
+    # Convert component types from UBC to SimPEG
+    ubc_types = ['xx', 'xy', 'xz', 'yy', 'yz', 'zz']
+    simpeg_types = ['gyy', 'gxy', 'gyz', 'gxx', 'gxz', 'gzz']
+    factor_list = [1., 1., -1., 1., -1., 1.]
+
+    for ii in range(0, len(components)):
+        k = ubc_types.index(components[ii])
+        factor[ii] = factor_list[k]
+        components[ii] = simpeg_types[k]
+    
+    # Second Line has number of locations
+    line = fid.readline()
+    ndat = int(line.split()[0])
+
+    # Pre-allocate space for obsx, obsy, obsz, data, uncert
+    line = fid.readline()
+
+    locXYZ = np.zeros((ndat, 3), dtype=float)
+    if file_type == 'survey':
+        d = None
+        wd = None
+    elif file_type == 'dpred':
+        d = np.zeros((ndat, len(components)), dtype=float)
+        wd = None
+    else:
+        d = np.zeros((ndat, len(components)), dtype=float)
+        wd = np.zeros((ndat, len(components)), dtype=float)
+    
+
+    ii = 0
+    while ii < ndat:
+
+        temp = np.array(line.split(), dtype=float)
+        locXYZ[ii, :] = temp[:3]
+        
+        if file_type == 'dpred':
+            d[ii, :] = factor * temp[3:]
+            
+        elif file_type == 'dobs':
+            d[ii, :] = factor * temp[3::2]
+            wd[ii, :] = temp[4::2]
+        
+        ii += 1
+        line = fid.readline()
+    fid.close()
+    
+    # Turn into vector. For multiple components, SimPEG orders by rows
+    if d is not None:
+        d = mkvc(d.T)
+    if wd is not None:
+        wd = mkvc(wd.T)
+
+    rxLoc = gravity.receivers.Point(locXYZ, components=components)
+    srcField = gravity.sources.SourceField([rxLoc])
+    survey = gravity.survey.Survey(srcField)
+    data_object = data.Data(survey, dobs=d, standard_deviation=wd)
+    return data_object
+
+
+def writeUBCgravitygradiometryObservations(filename, data_object):
+    """
+        Write UBC gravity gradiometry file format
+
+        INPUT:
+        :param: fileName, path to the UBC obs grav file
+        :param: survey Gravity object
+        :param: data array
+
+    """
+    survey = data_object.survey
+
+    # Convert component types from UBC to SimPEG
+    components = list(survey.components.keys())
+    n_comp = len(components)
+    factor = np.ones(n_comp)
+
+    ubc_types = ['xx', 'xy', 'xz', 'yy', 'yz', 'zz']
+    simpeg_types = ['gyy', 'gxy', 'gyz', 'gxx', 'gxz', 'gzz']
+    factor_list = [1., 1., -1., 1., -1., 1.]
+    for ii in range(0, len(components)):
+        k = simpeg_types.index(components[ii])
+        factor[ii] = factor_list[k]
+        components[ii] = ubc_types[k]
+
+    components = ','.join(components)
+    
+    data = survey.source_field.receiver_list[0].locations
+    n_loc = np.shape(data)[0]
+    
+    if np.any(data_object.standard_deviation!=0):
+        for ii in range(0, len(components)):
+            data = np.c_[
+                data,
+                factor[ii]*data_object.dobs[ii::n_comp],
+                data_object.standard_deviation[ii::n_comp]
+            ]
+
+    elif np.any(data_object.dobs!=0):
+        for ii in range(0, n_comp):
+            data = np.c_[
+                data,
+                factor[ii]*data_object.dobs[ii::n_comp]
+            ]
+    
+    
+    head = ("datacomp=%s\n" % components) + ("%i" % n_loc)
+    
     np.savetxt(
         filename, data, fmt="%e", delimiter=" ", newline="\n", header=head, comments=""
     )
