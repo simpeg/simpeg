fix missing negative sign for electric field formulation

SimPEG/electromagnetics/natural_source/fields.py
@@ -77,7 +77,7 @@ class Fields1DElectricField(FieldsFDEM):
         b = np.zeros((self._nF, len(source_list)), dtype=complex)
         for i, src in enumerate(source_list):
             b[:, i] = (
-                1
+                -1
                 / (1j * omega(src.frequency))
                 * (
                     self._MfI @ (self._D.T @ (self._V @ eSolution[:, i]))
@@ -90,7 +90,7 @@ class Fields1DElectricField(FieldsFDEM):
         if adjoint:
             # V, MfI are symmetric
             return (
-                1
+                -1
                 / (1j * omega(src.frequency))
                 * (self._V @ (self._D @ (self._MfI @ du_dm_v)))
             )

tests/em/nsem/forward/test_1D_finite_volume.py
@@ -66,7 +66,13 @@ class FiniteVolume1DTest(unittest.TestCase):
         phase = f[:, "phase"][-self.npad - 1, :]
 
         sig_a_error = np.abs(sig_a - s) / s
-        phase_error = np.abs(phase - -45)
+
+        if formulation == "e":
+            p = 45
+        elif formulation == "b":
+            p = -45
+
+        phase_error = np.abs(phase - p)
 
         passed_sigma = np.all(sig_a_error < TOL_SIGMA)
         passed_phase = np.all(phase_error < TOL_PHASE)