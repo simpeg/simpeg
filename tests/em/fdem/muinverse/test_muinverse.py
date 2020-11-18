b3efb94b0cc3c0d117111c15fa19357d2dac45e4
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-11-18 10:35:54 -0800

    reduce the order tolerance

--- tests/em/fdem/muinverse/test_muinverse.py
+++ tests/em/fdem/muinverse/test_muinverse.py
@@ -9,6 +9,7 @@ import unittest
 MuMax = 50.0
 TOL = 1e-8
 EPS = 1e-8
+ORDERTOL = 0.8
 
 np.random.seed(105)
 
@@ -170,7 +171,9 @@ class MuTests(unittest.TestCase):
                 lambda x: self.simulation.Jvec(self.m0, x),
             )
 
-        return tests.checkDerivative(fun, self.m0, num=3, plotIt=False, eps=EPS)
+        return tests.checkDerivative(
+            fun, self.m0, num=3, plotIt=False, eps=EPS, tolerance=ORDERTOL
+        )
 
     def JtvecTest(
         self, prbtype="ElectricField", sigmaInInversion=False, invertMui=False
