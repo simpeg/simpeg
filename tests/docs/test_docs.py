90f4f788f6a9b6dd86293f317a19bcf3f15af5bc
dccowan <devinccowan@gmail.com>
2021-03-29 10:00:48 -0700

    fix indentation

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -34,7 +34,7 @@ class Doc_Test(unittest.TestCase):
             print(response)
             self.assertTrue(response == 0)
         # else:
-    except:
+        except:
             response = subprocess.run(["make", "linkcheck-noplot"])
             print(response.returncode)
             self.assertTrue(response.returncode == 0)
