e59722bd893e4b933967ab2d9c3c244fdb11f5db
dccowan <devinccowan@gmail.com>
2021-03-29 09:18:34 -0700

    update docs test and requirements

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -14,27 +14,28 @@ class Doc_Test(unittest.TestCase):
         wd = os.getcwd()
         os.chdir(os.path.sep.join(self.path_to_docs))
         
-        if platform.system() != 'Windows':
-            response = subprocess.run(["make", "html-noplot"])
-            self.assertTrue(response.returncode == 0)
-        else:
+        if platform.system() == 'Windows':
             response = subprocess.call(["make", "html"], shell=True)  # Needed for local test on Windows
             self.assertTrue(response == 0)
-
+        else:
+            response = subprocess.run(["make", "html-noplot"])
+            self.assertTrue(response.returncode == 0)
+            
         os.chdir(wd)
 
     def test_linkcheck(self):
         wd = os.getcwd()
         os.chdir(os.path.sep.join(self.path_to_docs))
         
-        if platform.system() != 'Windows':
-            response = subprocess.run(["make", "linkcheck-noplot"])
-            print(response.returncode)
-            self.assertTrue(response.returncode == 0)
-        else:
+        if platform.system() == 'Windows':
             response = subprocess.call(["make", "linkcheck"], shell=True)  # Needed for local test on Windows
             print(response)
             self.assertTrue(response == 0)
+        else:
+            response = subprocess.run(["make", "linkcheck-noplot"])
+            print(response.returncode)
+            self.assertTrue(response.returncode == 0)
+        
 
         os.chdir(wd)
 
