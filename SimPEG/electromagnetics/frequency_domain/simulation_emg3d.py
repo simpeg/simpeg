27cb8d861042e503ac84683ae2e696265bae93bc
prisae <mail@werthmuller.org>
2021-06-04 19:21:33 +0200

    SimPEG-Survey <-> emg3d-Survey

--- SimPEG/electromagnetics/frequency_domain/simulation_emg3d.py
+++ SimPEG/electromagnetics/frequency_domain/simulation_emg3d.py
@@ -1,210 +1,319 @@
 import numpy as np
-import scipy.sparse as sp
-from scipy.constants import mu_0
-import properties
-from ...utils.code_utils import deprecate_class
 
-from ... import props
-from ...data import Data
+from discretize.utils import requires
+
 from ...utils import mkvc
 from .simulation import BaseFDEMSimulation
-from ..utils import omega
-from .survey import Survey
-from .fields import (
-    Fields3DElectricField,
-)
-import emg3d
-from emg3d import models, surveys, simulations, optimize
+
+# emg3d is a soft dependency
+try:
+    import emg3d
+except ImportError:
+    emg3d = False
 
 
+@requires({"emg3d": emg3d})
 class Simulation3DEMG3D(BaseFDEMSimulation):
-    """
-    By eliminating the magnetic flux density using
+    """3D simulation of electromagnetic fields using emg3d as a solver.
 
-        .. math ::
+    .. note::
 
-            \mathbf{b} = \\frac{1}{i \omega}\\left(-\mathbf{C} \mathbf{e} +
-            \mathbf{s_m}\\right)
+        - Only isotropic model implemented so far. Needs extension to tri-axial
+          anisotropy, mu_r, and epsilon_r.
+
+        - Currently, only electric "point"-dipole sources and electric/magnetic
+          point receivers are implemented. emg3d could do more, e.g., finite
+          length electric dipoles or arbitrary electric wires (and therefore
+          loops). These are *not yet* implemented here.
 
 
-    we can write Maxwell's equations as a second order system in
-    \\\(\\\mathbf{e}\\\) only:
+    Parameters
+    ----------
+    simulation_opts : dict
+        Input parameters forward to ``emg3d.Simulation``. See the emg3d
+        documentation for all the possibilities.
 
-    .. math ::
-
-        \\left(\mathbf{C}^{\\top} \mathbf{M_{\mu^{-1}}^f} \mathbf{C} +
-        i \omega \mathbf{M^e_{\sigma}} \\right)\mathbf{e} =
-        \mathbf{C}^{\\top} \mathbf{M_{\mu^{-1}}^f}\mathbf{s_m}
-        - i\omega\mathbf{M^e}\mathbf{s_e}
-
-    which we solve for :math:`\mathbf{e}`.
-
-    :param discretize.base.BaseMesh mesh: mesh
     """
 
     _solutionType = "eSolution"
     _formulation = "EB"
-    fieldsPair = Fields3DElectricField
-    max_workers = 1
     storeJ = False
     _Jmatrix = None
 
     def __init__(self, mesh, **kwargs):
-        super(Simulation3DEMG3D, self).__init__(mesh, **kwargs)
-  
+        """Initialize Simulation using emg3d as solver."""
+
+        # Store simulation options.
+        simulation_opts = kwargs.pop('simulation_opts', {})
+
+        super().__init__(mesh, **kwargs)
+
+        # Create twin Simulation in emg3d.
+        self.emg3d_sim = emg3d.Simulation(
+            survey=self.emg3d_survey,
+            model=emg3d.Model(self.mesh),  # Dummy values of 1 for initiation.
+            **{'name': 'Simulation created by SimPEG',
+               'gridding': 'same',                  # Change this eventually!
+               'tqdm_opts': {'disable': True},      # Switch-off tqdm
+               'receiver_interpolation': 'linear',  # Should be linear
+               **simulation_opts}                   # User input
+        )
+
+        # TODO : Count to store data per iteration.
+        # - Should be replaced by a proper count form SimPEG.inversion.
+        # - Should probably be made optional to store data at each step.
+        self._it_count = 0
+
     @property
     def emg3d_survey(self):
+        """emg3d survey; obtained from SimPEG survey."""
+
         if getattr(self, "_emg3d_survey", None) is None:
-            self._emg3d_survey = self.generate_emg3d_survey_from_simpeg_survey()
-        return self._emg3d_survey
-    
-    @property
-    def emg3d_sigma(self):
-        self._emg3d_sigma = models.Model(
-            self.mesh, 
-            self.sigma.reshape(self.mesh.vnC, order='F'), 
-            mapping='Conductivity'
-        )
-        return self._emg3d_sigma
 
-    @ property
-    def emg3d_simulation_inputs(self):
-        if getattr(self, "_emg3d_simulation_inputs", None) is None:
-            solver_opts = {
-                'sslsolver': False,
-                'semicoarsening': False,
-                'linerelaxation': False,
-            #     'tol': 5e-5,  # Reduce tolerance to speed-up
-            }
-            self._emg3d_simulation_inputs = {
-                'name': 'Testing',
-                'survey': self.emg3d_survey,
-                'solver_opts': solver_opts,
-                'max_workers': self.max_workers,
-                'gridding': 'same',
-                'verb': -1,
-            }        
-        return self._emg3d_simulation_inputs
-    
-    
-    def generate_emg3d_survey_from_simpeg_survey(self):
+            # Allocate lists to create data to/from dicts.
+            src_list = []
+            freq_list = []
+            rec_list = []
+            data_dict = {}
+            indices = np.zeros((self.survey.nD, 3), dtype=int)
 
-        source_list_emg3d = []
-        src_rx_uids = []
-        source_frequency = []
-        for src in self.survey.srcList:
-            source_frequency.append(src.frequency)    
-            src_emg3d = emg3d.TxElectricDipole(
-                (src.location[0], src.location[1], src.location[2], src.dip, src.azimuth)
+            # Counter for SimPEG data object (lists the data continuously).
+            ind = 0
+
+            # Loop over sources.
+            for src in self.survey.source_list:
+
+                # Create emg3d source.
+                source = emg3d.TxElectricDipole(
+                    (*src.location, src.azimuth, src.elevation),
+                    strength=src.strength, length=src.length
+                )
+
+                # New frequency: add.
+                if src.frequency not in freq_list:
+                    f_ind = len(freq_list)
+                    freq_list.append(src.frequency)
+
+                # Existing source: get index.
+                else:
+                    f_ind = freq_list.index(src.frequency)
+
+                # New source: add.
+                if source not in src_list:
+                    s_ind = len(src_list)
+                    data_dict[s_ind] = {f_ind: {}}
+                    src_list.append(source)
+
+                # Existing source: get index.
+                else:
+                    s_ind = src_list.index(source)
+
+                    # If new frequency for existing source, add:
+                    if f_ind not in data_dict[s_ind].keys():
+                        data_dict[s_ind][f_ind] = {}
+
+                # Loop over receiver lists.
+                rec_types = [emg3d.RxElectricPoint, emg3d.RxMagneticPoint]
+                for rec in src.receiver_list:
+
+                    if rec.projField not in ['e', 'h']:
+                        raise NotImplementedError(
+                            "Only projField = {'e'; 'h'} implemented."
+                        )
+
+                    if rec.orientation not in ['x', 'y', 'z']:
+                        raise NotImplementedError(
+                            "Only orientation = {'x'; 'y'; 'z'} implemented."
+                        )
+
+                    # Get type, azimuth, elevation.
+                    rec_type = rec_types[rec.projField == 'h']
+                    azimuth = [0, 90][rec.orientation == 'y']
+                    elevation = [0, 90][rec.orientation == 'z']
+
+                    # Loop over receivers.
+                    for i in range(rec.locations[:, 0].size):
+
+                        # Create emg3d receiver.
+                        receiver = rec_type(
+                                (*rec.locations[i, :], azimuth, elevation))
+
+                        # New receiver: add.
+                        if receiver not in rec_list:
+                            r_ind = len(rec_list)
+                            data_dict[s_ind][f_ind][r_ind] = ind
+                            rec_list.append(receiver)
+
+                        # Existing receiver: get index.
+                        else:
+                            r_ind = rec_list.index(receiver)
+
+                            # If new receiver for existing src-freq, add:
+                            existing = data_dict[s_ind][f_ind].keys()
+                            if r_ind not in existing:
+                                data_dict[s_ind][f_ind][r_ind] = ind
+
+                            # Else, throw an error.
+                            else:
+                                raise ValueError(
+                                    "Duplicate source-receiver-frequency."
+                                )
+
+                        indices[ind, :] = [s_ind, r_ind, f_ind]
+                        ind += 1
+
+            # Create and store survey.
+            survey = emg3d.Survey(
+                name='Survey created by SimPEG',
+                sources=emg3d.surveys.txrx_lists_to_dict(src_list),
+                receivers=emg3d.surveys.txrx_lists_to_dict(rec_list),
+                frequencies=freq_list,
+                noise_floor=1.,       # We deal with std in SimPEG.
+                relative_error=None,  #  "   "   "
             )
-            source_list_emg3d.append(src_emg3d)
-            for rx in src.rxList:
-                src_rx_uids.append([src._uid, rx._uid])
-        src_rx_uids = np.vstack(src_rx_uids)
+            self._emg3d_survey = survey
 
-        # assume all sources have the same receiver locations now
-        if np.unique(src_rx_uids[:,1]).size == 1:
-            # also assume rx only contains a single component
-            if rx.projField == 'e':
-                emg3d_rx_object = emg3d.RxElectricPoint
-            elif rx.projField == 'h':
-                emg3d_rx_object = emg3d.RxMagneticPoint
-            
-            if rx.orientation == 'x':
-                azimuth, dip = 0, 0
-            elif rx.orientation == 'y':
-                azimuth, dip = 90, 0
-            elif rx.orientation == 'z':
-                azimuth, dip = 0, 90
-            else:
-                raise Exception()
-            receivers = emg3d.surveys.txrx_coordinates_to_dict(
-                emg3d_rx_object,
-                (rx.locations[:,0], rx.locations[:,1], rx.locations[:,2], dip, azimuth),
-            )    
-        else:
-            raise Exception()
-        sources = emg3d.surveys.txrx_lists_to_dict(source_list_emg3d)  
-        source_frequency = np.unique(np.array(source_frequency))
-        emg3d_survey = surveys.Survey(
-            name='emg3d survey',
-            sources=sources,
-            receivers=receivers,
-            frequencies=source_frequency,
-            noise_floor=1.,
-            relative_error=None,
+            # Store data-mapping SimPEG <-> emg3d
+            self._dmap_simpeg_emg3d = tuple(indices.T)
+
+            # Create emg3d data dummy; can be re-used.
+            self._emg3d_array = np.full(survey.shape, np.nan+1j*np.nan)
+
+        return self._emg3d_survey
+
+    @property
+    def emg3d_model(self):
+        """emg3d conductivity model; obtained from SimPEG conductivities."""
+        self._emg3d_model = emg3d.Model(
+            self.mesh,
+            property_x=self.sigma.reshape(self.mesh.shape_cells, order='F'),
+            # property_y=None,  Not yet implemented
+            # property_z=None,   "
+            # mu_r=None,         "
+            # epsilon_r=None,    "
+            mapping='Conductivity',
         )
-        self.n_source = len(source_list_emg3d)
-        self.n_frequency = len(source_frequency)
-        self.n_receiver = rx.locations.shape[0]
-        return emg3d_survey
+        return self._emg3d_model
+
+    @property
+    def _emg3d_simulation_update(self):
+        """Updates emg3d simulation with new model, resetting the fields."""
+
+        # We have to replace the model
+        # Warning:
+        # This will not work for automatic model extension possible in emg3d.
+        self.emg3d_sim.model = self.emg3d_model
+
+        # Re-initiate all dicts _except_ grids (automatic gridding)
+        self.emg3d_sim._dict_model = self.emg3d_sim._dict_initiate
+        self.emg3d_sim._dict_efield = self.emg3d_sim._dict_initiate
+        self.emg3d_sim._dict_hfield = self.emg3d_sim._dict_initiate
+        self.emg3d_sim._dict_efield_info = self.emg3d_sim._dict_initiate
+        self.emg3d_sim._gradient = None
+        self.emg3d_sim._misfit = None
+        self.emg3d_sim._vec = None  # TODO check back when emg3d-side finished!
 
     def Jvec(self, m, v, f=None):
+        """
+        Sensitivity times a vector.
+
+        :param numpy.ndarray m: inversion model (nP,)
+        :param numpy.ndarray v: vector which we take sensitivity product with
+            (nP,)
+        :param SimPEG.electromagnetics.frequency_domain.fields.FieldsFDEM f: fields object
+        :rtype: numpy.ndarray
+        :return: Jvec (ndata,)
+        """
         if self.verbose:
-            print ("Compute Jvec")
+            print("Compute Jvec")
 
         if self.storeJ:
             J = self.getJ(m, f=f)
             Jv = mkvc(np.dot(J, v))
             return Jv
 
-        self.model = m        
+        self.model = m
 
         if f is None:
-            f = self.fields(m=m)       
-        
+            f = self.fields(m=m)
+
         dsig_dm_v = self.sigmaDeriv @ v
-        j_vec = optimize.jvec(f, vec=dsig_dm_v)
-        return j_vec
+        j_vec = emg3d.optimize.jvec(f, vec=dsig_dm_v)
+
+        # Map emg3d-data-array to SimPEG-data-vector
+        return j_vec[self._dmap_simpeg_emg3d]
 
     def Jtvec(self, m, v, f=None):
+        """
+        Sensitivity transpose times a vector
+
+        :param numpy.ndarray m: inversion model (nP,)
+        :param numpy.ndarray v: vector which we take adjoint product with (ndata,)
+        :param SimPEG.electromagnetics.frequency_domain.fields.FieldsFDEM f: fields object
+        :rtype: numpy.ndarray
+        :return: Jtvec (nP,)
+        """
         if self.verbose:
             print("Compute Jtvec")
-        
+
         if self.storeJ:
+
+            # Put v onto emg3d data-array.
+            self._emg3d_array[self._dmap_simpeg_emg3d] = v
+
             J = self.getJ(m, f=f)
-            Jtv = mkvc(np.dot(J.T, v))
+            Jtv = mkvc(np.dot(J.T, self._emg3d_array))
+
             return Jtv
 
         self.model = m
 
         if f is None:
-            f = self.fields(m=m)        
+            f = self.fields(m=m)
 
         return self._Jtvec(m, v=v, f=f)
-    
+
     def _Jtvec(self, m, v=None, f=None):
-        """
-            Compute adjoint sensitivity matrix (J^T) and vector (v) product.
-            Full J matrix can be computed by inputing v=None
+        """Compute adjoint sensitivity matrix (J^T) and vector (v) product.
+
+        Full J matrix can be computed by setting v=None (not implemented yet).
         """
 
         if v is not None:
+            # Put v onto emg3d data-array.
+            self._emg3d_array[self._dmap_simpeg_emg3d] = v
 
-            jt_sigma_vec = optimize.gradient(f, vec=v)
-            jt_vec = self.sigmaDeriv.T @ jt_sigma_vec
+            # Replace residual by vector if provided
+            f.survey.data['residual'][...] = self._emg3d_array
+
+            # Get gradient with `v` as residual.
+            jt_sigma_vec = emg3d.optimize.gradient(f)
+
+            jt_vec = self.sigmaDeriv.T @ jt_sigma_vec.ravel('F')
             return jt_vec
 
         else:
             # This is for forming full sensitivity matrix
-            # Currently, it is not correct. 
-            # Requires a fix in optimize.gradient 
-            # Jt is supposed to be a complex value ... 
-            Jt = np.zeros((self.model.size, self.survey.nD), order="F")
-            for i_datum in range(self.survey.nD):
-                v = np.zeros(self.survey.nD)
-                v[i_datum] = 1.
-                jt_sigma_vec = optimize.gradient(f, vec=v)
-                Jt[:, i_datum] = self.sigmaDeriv.T @ jt_sigma_vec
-            
-            return Jt
+            # Currently, it is not correct.
+            # Requires a fix in optimize.gradient
+            # Jt is supposed to be a complex value ...
+            # Jt = np.zeros((self.model.size, self.survey.nD), order="F")
+            # for i_datum in range(self.survey.nD):
+            #     vec = np.zeros(self.survey.nD)
+            #     vec[i_datum] = 1.
+            #     vec = vec.reshape(self.emg3d_survey.shape)
+            #     jt_sigma_vec = emg3d.optimize.gradient(f, vector=vec)
+            #     Jt[:, i_datum] = self.sigmaDeriv.T @ jt_sigma_vec
+            # return Jt
+
+            raise NotImplementedError
 
     def getJ(self, m, f=None):
-        """
-            Generate Full sensitivity matrix
-        """
+        """Generate full sensitivity matrix."""
+
         if self._Jmatrix is not None:
             return self._Jmatrix
+
         else:
             if self.verbose:
                 print("Calculating J and storing")
@@ -212,30 +321,61 @@ class Simulation3DEMG3D(BaseFDEMSimulation):
             if f is None:
                 f = self.fields(m)
             self._Jmatrix = (self._Jtvec(m, v=None, f=f)).T
+
         return self._Jmatrix
 
-
     def dpred(self, m=None, f=None):
+        r"""Return the predicted (modelled) data for a given model.
+
+        The fields, f, (if provided) will be used for the predicted data
+        instead of recalculating the fields (which may be expensive!).
+
+        .. math::
+
+            d_\text{pred} = P(f(m))
+
+        Where P is a projection of the fields onto the data space.
+
+        :param numpy.ndarray m: model
+        :param SimPEG.electromagnetics.frequency_domain.fields.FieldsFDEM f: fields object
+        :rtype: numpy.ndarray
+        :return: data, the data
+        """
+
         if self.verbose:
             print("Compute predicted")
-        # currently this does not change once self.fields is computed.
+
         if f is None:
             f = self.fields(m=m)
-        # this may not be robust, need to be changed... 
-        data = f.data.synthetic.values.flatten()
-        return data 
-  
+
+        # Map emg3d-data-array to SimPEG-data-vector
+        return f.data.synthetic.data[self._dmap_simpeg_emg3d]
+
     def fields(self, m=None):
+        """Return the electric fields for a given model.
+
+        :param numpy.ndarray m: model
+        :rtype: numpy.ndarray
+        :return: f, the fields
+        """
+
         if self.verbose:
             print("Compute fields")
-        self.model = m
- 
-        f = simulations.Simulation(
-            model=self.emg3d_sigma, 
-            **self.emg3d_simulation_inputs
-        )
-        std = f.survey.standard_deviation
-        # Store weights
-        f.data['weights'] = std**-2        
-        f.compute()
-        return f
+
+        if m is not None:
+            # Store model.
+            self.model = m
+
+            # Update simulation.
+            self._emg3d_simulation_update
+
+        # Compute forward model and sets initial residuals.
+        _ = self.emg3d_sim.misfit
+
+        # Store the data at each step in the survey-xarray
+        if m is not None:
+            current_data = self.emg3d_survey.data.synthetic.copy()
+            self.emg3d_survey.data[f"it_{self._it_count}"] = current_data
+            self._it_count += 1  # Update counter
+
+        return self.emg3d_sim
