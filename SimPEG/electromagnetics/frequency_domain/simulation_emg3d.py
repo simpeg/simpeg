6b60e9a54513f3ca76589d9cb796d3cb2ca41013
seogi_macbook <sgkang09@gmail.com>
2021-07-08 01:37:37 -0700

    bla..

--- SimPEG/electromagnetics/frequency_domain/simulation_emg3d.py
+++ SimPEG/electromagnetics/frequency_domain/simulation_emg3d.py
@@ -215,6 +215,7 @@ class Simulation3DEMG3D(BaseFDEMSimulation):
         # Replace residual by vector if provided
         f.survey.data['residual'][...] = self._emg3d_array
         # Get gradient with `v` as residual.
+        jt_vec = np.empty(self.model.size, dtype=float)
         jt_vec = self.sigmaDeriv.T @ emg3d.optimize.gradient(f).ravel('F')
         return jt_vec
 
