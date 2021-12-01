1ad3ab82c9003ea6495ab32cc63d796039fa6bee
Joseph Capriotti <josephrcapriotti@gmail.com>
2021-11-30 17:10:26 -0800

    exclude cross gradient and linear correspondence from regular tests

--- tests/base/test_regularization.py
+++ tests/base/test_regularization.py
@@ -28,6 +28,8 @@ IGNORE_ME = [
     "PGIwithRelationships",
     "PGIwithNonlinearRelationshipsSmallness",
     "PGIsmallness",
+    "CrossGradient",
+    "LinearCorrespondence",
 ]
 
 
@@ -284,7 +286,8 @@ class RegularizationTests(unittest.TestCase):
         )
 
         objfct = objective_function.L2ObjectiveFunction(
-            W=utils.sdiag(np.sqrt(cell_weights * mesh.cell_volumes)), mapping=wires.sigma
+            W=utils.sdiag(np.sqrt(cell_weights * mesh.cell_volumes)),
+            mapping=wires.sigma,
         )
 
         self.assertTrue(reg(m) == objfct(m))
