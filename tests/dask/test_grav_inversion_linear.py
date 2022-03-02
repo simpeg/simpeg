0a26b491c546110427fbf6160db7e74c6f142651
fourndo <fourndo@gmail.com>
2022-03-01 16:48:51 -0800

    Update test

--- tests/dask/test_grav_inversion_linear.py
+++ tests/dask/test_grav_inversion_linear.py
@@ -45,7 +45,6 @@ class GravInvLinProblemTest(unittest.TestCase):
         # Go from topo to actv cells
         topo = np.c_[utils.mkvc(xx), utils.mkvc(yy), utils.mkvc(zz)]
         actv = utils.surface2ind_topo(self.mesh, topo, "N")
-        actv = np.where(actv)[0]
 
         # Create active map to go from reduce space to full
         self.actvMap = maps.InjectActiveCells(self.mesh, actv, -100)
