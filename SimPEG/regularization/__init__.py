9c184878af2e7dd45bc7ff653979d008a77b0e30
fourndo <fourndo@gmail.com>
2022-03-21 08:19:03 -0700

    Move deprecate reg classed to init

--- SimPEG/regularization/__init__.py
+++ SimPEG/regularization/__init__.py
@@ -1,3 +1,4 @@
+from ..utils.code_utils import deprecate_class
 from .base import (
     BaseRegularization,
     LeastSquaresRegularization,
@@ -14,7 +15,33 @@ from .pgi import (
     PGIwithNonlinearRelationshipsSmallness,
     PGIwithRelationships,
 )
-from .tikhonov import Tikhonov
 from .cross_gradient import CrossGradient
 from .correspondence import LinearCorrespondence
 from .jtv import JointTotalVariation
+
+
+@deprecate_class(removal_version="0.16.0", future_warn=True)
+class SimpleSmall(Small):
+    def __init__(self, mesh=None, **kwargs):
+        if "alpha_s" not in kwargs:
+            kwargs["alpha_s"] = 1.0
+        super().__init__(mesh=mesh, **kwargs)
+
+
+@deprecate_class(removal_version="0.16.0", future_warn=True)
+class SimpleSmoothDeriv(SmoothDeriv):
+    def __init__(self, mesh=None, **kwargs):
+        super().__init__(mesh=mesh, normalized_gradients=True, **kwargs)
+
+
+@deprecate_class(removal_version="0.16.0", future_warn=True)
+class Simple(LeastSquaresRegularization):
+    def __init__(self, mesh=None, **kwargs):
+        if "alpha_s" not in kwargs:
+            kwargs["alpha_s"] = 1.0
+        super().__init__(mesh=mesh, normalized_gradients=True, **kwargs)
+
+
+@deprecate_class(removal_version="0.x.0", future_warn=True)
+class Tikhonov(LeastSquaresRegularization):
+    pass
