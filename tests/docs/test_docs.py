5e94e8d7fa7ea2a02df53db3091a05c994503b6d
dccowan <devinccowan@gmail.com>
2022-05-06 12:33:17 -0700

    update test_docs to discretize

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -7,7 +7,7 @@ import platform
 class Doc_Test(unittest.TestCase):
     @property
     def path_to_docs(self):
-        dirname, filename = os.path.split(os.path.abspath(__file__))
+        dirname, file_name = os.path.split(os.path.abspath(__file__))
         return dirname.split(os.path.sep)[:-2] + ["docs"]
 
     def test_html(self):
@@ -15,7 +15,7 @@ class Doc_Test(unittest.TestCase):
         os.chdir(os.path.sep.join(self.path_to_docs))
 
         if platform.system() != "Windows":
-            response = subprocess.run(["make", "html-noplot"])
+            response = subprocess.run(["make", "html"])
             self.assertTrue(response.returncode == 0)
         else:
             response = subprocess.call(["make", "html"], shell=True)
@@ -28,7 +28,7 @@ class Doc_Test(unittest.TestCase):
         os.chdir(os.path.sep.join(self.path_to_docs))
 
         if platform.system() != "Windows":
-            response = subprocess.run(["make", "linkcheck-noplot"])
+            response = subprocess.run(["make", "linkcheck"])
             self.assertTrue(response.returncode == 0)
         else:
             response = subprocess.call(["make", "linkcheck"], shell=True)
