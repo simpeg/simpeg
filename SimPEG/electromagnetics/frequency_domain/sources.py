37d8e021510161d3c67a3e17e2608c3040181abe
Thibaut Astic <97514898+thibaut-kobold@users.noreply.github.com>
2022-08-09 16:51:02 -0700

    Update SimPEG/electromagnetics/frequency_domain/sources.py
    Co-authored-by: Lindsey Heagy <lindseyheagy@gmail.com>

--- SimPEG/electromagnetics/frequency_domain/sources.py
+++ SimPEG/electromagnetics/frequency_domain/sources.py
@@ -540,7 +540,7 @@ class CircularLoop(MagDipole):
 
     @property
     def moment(self):
-        return np.pi * self.radius ** 2 * self.current * self.N
+        return np.pi * self.radius ** 2 * self.current * self.n_turns
 
     def _srcFct(self, obsLoc, coordinates="cartesian"):
         if getattr(self, "_loop", None) is None:
