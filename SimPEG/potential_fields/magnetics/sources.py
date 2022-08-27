502d6f00a517fc71e050d7d4f45de73e93568fa1
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-08-27 14:35:02 -0700

    mag implementation TODO: fix tmi implementation

--- SimPEG/potential_fields/magnetics/sources.py
+++ SimPEG/potential_fields/magnetics/sources.py
@@ -1,31 +1,114 @@
 from ...survey import BaseSrc
-from .analytics import IDTtoxyz
+from SimPEG.utils.mat_utils import dip_azimuth2cartesian
+from SimPEG.utils.code_utils import deprecate_class
 
 
-class SourceField(BaseSrc):
-    """Define the inducing field"""
+class UniformBackgroundField(BaseSrc):
+    """A constant uniform background magnetic field.
+
+    This inducing field has a uniform magnetic background field defined by an amplitude,
+    inclination, and declination.
+
+    Parameters
+    ----------
+    receiver_list : list of SimPEG.potential_fields.magnetics.Point
+    parameters : tuple of (amplitude, inclutation, declination), optional
+        Deprecated input for the function, provided in this position for backwards
+        compatibility
+    amplitude : float, optional
+        amplitude of the inducing backgound field, usually this is in units of nT.
+    inclination : float, optional
+        Dip angle in degrees from the horizon, positive points into the earth.
+    declination : float, optional
+        Azimuthal angle in degrees from north, positive clockwise.
+    """
 
     def __init__(
         self,
         receiver_list=None,
-        parameters=None,
         amplitude=50000,
         inclination=90,
         declination=0,
         **kwargs
     ):
-        if parameters is not None:
-            if len(parameters) != 3:
-                raise ValueError(
-                    "Inducing field 'parameters' must be a list or tuple of length 3 (amplitude, inclination, declination"
-                )
-            amplitude, inclination, declination = parameters
-
         self.amplitude = amplitude
         self.inclination = inclination
         self.declination = declination
 
-        self.b0 = IDTtoxyz(-parameters[1], parameters[2], parameters[0])
-        super(SourceField, self).__init__(
-            receiver_list=receiver_list, parameters=parameters, **kwargs
+        super().__init__(receiver_list=receiver_list, **kwargs)
+
+    @property
+    def amplitude(self):
+        """Amplitude of the inducing backgound field.
+
+        Returns
+        -------
+        float
+            Usually this is in nT. It should match the units of your magnetic data.
+        """
+        return self._amplitude
+
+    @amplitude.setter
+    def amplitude(self, value):
+        try:
+            value = float(value)
+        except Exception:
+            raise TypeError("amplitude must be a number")
+        self._amplitude = value
+
+    @property
+    def inclination(self):
+        """Dip angle in from the horizon.
+
+        Returns
+        -------
+        float
+            In degrees, positive points into the earth
+        """
+        return self._inclination
+
+    @inclination.setter
+    def inclination(self, value):
+        try:
+            value = float(value)
+        except Exception:
+            raise TypeError("inclination must be a number")
+        if value > 90 or value < -90:
+            raise ValueError("inclination should be between -90 and 90")
+        self._inclination = value
+
+    @property
+    def declination(self):
+        """Azimuthal angle from north.
+
+        Returns
+        -------
+        float
+            In degrees, positive is clockwise
+        """
+        return self._declination
+
+    @declination.setter
+    def declination(self, value):
+        try:
+            value = float(value)
+        except Exception:
+            raise TypeError("declination must be a number")
+        self._declination = value
+
+    @property
+    def b0(self):
+        return (
+            self.amplitude
+            * dip_azimuth2cartesian(self.inclination, self.declination).squeeze()
+        )
+
+
+class SourceField(UniformBackgroundField):
+    def __init__(self, receiver_list=None, parameters=[50000, 90, 0]):
+        super().__init__(
+            receiver_list=receiver_list,
+            amplitude=parameters[0],
+            inclination=parameters[1],
+            declination=parameters[2],
         )
