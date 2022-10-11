5c721f2c5e9b65f1bf3f9c4fa17adb56fcf49813
John Kuttai <john.kuttai@gmail.com>
2022-10-11 15:31:39 -0700

    Added dask fields fixed to TE mode

--- SimPEG/dask/electromagnetics/natural_source/simulation.py
+++ SimPEG/dask/electromagnetics/natural_source/simulation.py
@@ -1,4 +1,5 @@
-from ....electromagnetics.natural_source.simulation import Simulation2DMagneticField as Sim
+from ....electromagnetics.natural_source.simulation import Simulation2DMagneticField as Sim_m
+from ....electromagnetics.natural_source.simulation import Simulation2DElectricField as Sim_e
 from ....utils import Zero, mkvc
 import numpy as np
 import scipy.sparse as sp
@@ -7,7 +8,7 @@ from dask.distributed import Future
 import zarr
 from time import time
 
-def dask_boundary_fields(self, model=None):
+def dask_magnetic_boundary_fields(self, model=None):
     "Returns the 1D field objects at the boundaries"
     
     if getattr(self, "_boundary_fields", None) is None:
@@ -45,4 +46,44 @@ def dask_boundary_fields(self, model=None):
     return self._boundary_fields
 
 
-Sim.boundary_fields = dask_boundary_fields
+Sim_m.boundary_fields = dask_magnetic_boundary_fields
+
+
+def dask_electric_boundary_fields(self, model=None):
+        "Returns the 1D field objects at the boundaries"
+        if getattr(self, "_boundary_fields", None) is None:
+            if model is None:
+                model = self.model
+            sim = self._sim_left
+            if self.muiMap is None:
+                try:
+                    sim.mui = self._P_l @ self.mui
+                except Exception:
+                    sim.mui = self.mui
+            if self.sigmaMap is None:
+                try:
+                    sim.sigma = self._P_l @ self.sigma
+                except Exception:
+                    sim.sigma = self.sigma
+            f_left, Ainv = sim.fields(model, return_Ainv=True)
+            self._sim_left.Ainv = Ainv
+
+            sim = self._sim_right
+            if self.muiMap is None:
+                try:
+                    sim.mui = self._P_r @ self.mui
+                except Exception:
+                    sim.mui = self.mui
+            if self.sigmaMap is None:
+                try:
+                    sim.sigma = self._P_r @ self.sigma
+                except Exception:
+                    sim.sigma = self.sigma
+            f_right, Ainv = sim.fields(model, return_Ainv=True)
+            self._sim_right.Ainv = Ainv
+
+            self._boundary_fields = (f_left, f_right)
+        return self._boundary_fields
+
+
+Sim_e.boundary_fields = dask_electric_boundary_fields
