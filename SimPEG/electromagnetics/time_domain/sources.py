00388adc114183ec7cdeac79f12969edc019eb6a
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-10-12 14:49:35 -0700

    fix triangular waveform example

--- SimPEG/electromagnetics/time_domain/sources.py
+++ SimPEG/electromagnetics/time_domain/sources.py
@@ -569,7 +569,7 @@ class TriangularWaveform(TrapezoidWaveform):
     >>> from SimPEG.electromagnetics import time_domain as tdem
 
     >>> times = np.linspace(0, 1e-2, 1000)
-    >>> waveform = tdem.sources.TriangularWaveform(off_time=6e-3, peak_time=3e-3)
+    >>> waveform = tdem.sources.TriangularWaveform(start_time=1E-3, off_time=6e-3, peak_time=3e-3)
     >>> plt.plot(times, [waveform.eval(t) for t in times])
     >>> plt.show()
 
