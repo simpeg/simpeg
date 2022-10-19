314466e16f084f47e549c41c4187154a0faf8c14
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-10-19 08:34:20 -0700

    add period

--- SimPEG/props.py
+++ SimPEG/props.py
@@ -371,7 +371,7 @@ class HasModel(BaseSimPEG, metaclass=PhysicalPropertyMetaclass):
 
     @property
     def model(self):
-        """The inversion model
+        """The inversion model.
 
         Returns
         -------
