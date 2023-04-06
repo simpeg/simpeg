2abc35976b43774ecd1e9ebe4572c8576baa6dcb
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-04-06 12:09:32 -0500

    add more testing

--- tests/meta/test_dask_meta.py
+++ tests/meta/test_dask_meta.py
@@ -8,12 +8,14 @@ import pytest
 
 from SimPEG.meta import (
     MetaSimulation,
-    SumMetaSimulation,
-    RepeatedSimulation,
     DaskMetaSimulation,
     DaskSumMetaSimulation,
     DaskRepeatedSimulation,
 )
+
+from distributed import Client
+
+client = Client()
 
 
 def test_dask_meta_correctness():
@@ -48,7 +50,7 @@ def test_dask_meta_correctness():
         mappings.append(maps.IdentityMap())
 
     meta_sim = MetaSimulation(sims, mappings)
-    dask_sim = DaskMetaSimulation(sims, mappings)
+    dask_sim = DaskMetaSimulation(sims, mappings, client)
 
     # test fields objects
     f_meta = meta_sim.fields(m_test)
@@ -101,267 +103,284 @@ def test_dask_meta_correctness():
     np.testing.assert_allclose(diag_dask, diag_dask2)
 
 
-# def test_sum_sim_correctness():
-#     mesh = TensorMesh([16, 16, 16], origin="CCN")
-#
-#     rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
-#     rx = gravity.Point(rx_locs, components=["gz"])
-#     survey = gravity.Survey(gravity.SourceField(rx))
-#     full_sim = gravity.Simulation3DIntegral(
-#         mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
-#     )
-#
-#     mesh_bot = TensorMesh([mesh.h[0], mesh.h[1], mesh.h[2][:8]], origin=mesh.origin)
-#     mesh_top = TensorMesh(
-#         [mesh.h[0], mesh.h[1], mesh.h[2][8:]], origin=["C", "C", mesh.nodes_z[8]]
-#     )
-#
-#     mappings = [
-#         maps.Mesh2Mesh((mesh_bot, mesh)),
-#         maps.Mesh2Mesh((mesh_top, mesh)),
-#     ]
-#     sims = [
-#         gravity.Simulation3DIntegral(
-#             mesh_bot, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
-#         ),
-#         gravity.Simulation3DIntegral(
-#             mesh_top, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
-#         ),
-#     ]
-#
-#     sum_sim = SumMetaSimulation(sims, mappings)
-#
-#     m_test = np.arange(mesh.n_cells) / mesh.n_cells + 0.1
-#
-#     # test fields objects
-#     f_full = full_sim.fields(m_test)
-#     f_meta = sum_sim.fields(m_test)
-#     np.testing.assert_allclose(f_full, sum(f_meta))
-#
-#     # test data output
-#     d_full = full_sim.dpred(m_test, f=f_full)
-#     d_meta = sum_sim.dpred(m_test, f=f_meta)
-#     np.testing.assert_allclose(d_full, d_meta)
-#
-#     # test Jvec
-#     u = np.random.rand(mesh.n_cells)
-#     jvec_full = full_sim.Jvec(m_test, u, f=f_full)
-#     jvec_meta = sum_sim.Jvec(m_test, u, f=f_meta)
-#
-#     np.testing.assert_allclose(jvec_full, jvec_meta)
-#
-#     # test Jtvec
-#     v = np.random.rand(survey.nD)
-#     jtvec_full = full_sim.Jtvec(m_test, v, f=f_full)
-#     jtvec_meta = sum_sim.Jtvec(m_test, v, f=f_meta)
-#
-#     np.testing.assert_allclose(jtvec_full, jtvec_meta)
-#
-#     # test get diag
-#     diag_full = full_sim.getJtJdiag(m_test, f=f_full)
-#     diag_meta = sum_sim.getJtJdiag(m_test, f=f_meta)
-#
-#     np.testing.assert_allclose(diag_full, diag_meta)
-#
-#     # test things also works without passing optional kwargs
-#     sum_sim.model = m_test
-#     d_meta2 = sum_sim.dpred()
-#     np.testing.assert_allclose(d_meta, d_meta2)
-#
-#     jvec_meta2 = sum_sim.Jvec(m_test, u)
-#     np.testing.assert_allclose(jvec_meta, jvec_meta2)
-#
-#     jtvec_meta2 = sum_sim.Jtvec(m_test, v)
-#     np.testing.assert_allclose(jtvec_meta, jtvec_meta2)
-#
-#     sum_sim._jtjdiag = None
-#     diag_meta2 = sum_sim.getJtJdiag(m_test)
-#     np.testing.assert_allclose(diag_meta, diag_meta2)
-#
-#
-# def test_repeat_sim_correctness():
-#     # meta sim is tested for correctness
-#     # so can test the repeat against the meta sim
-#     mesh = TensorMesh([8, 8, 8], origin="CCN")
-#
-#     rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
-#     rx = gravity.Point(rx_locs, components=["gz"])
-#     survey = gravity.Survey(gravity.SourceField(rx))
-#     sim = gravity.Simulation3DIntegral(
-#         mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
-#     )
-#
-#     time_mesh = TensorMesh(
-#         [
-#             8,
-#         ],
-#         origin=[
-#             0,
-#         ],
-#     )
-#     sim_ts = np.linspace(0, 1, 6)
-#
-#     mappings = []
-#     simulations = []
-#     eye = sp.eye(mesh.n_cells, mesh.n_cells)
-#     for t in sim_ts:
-#         ave_time = time_mesh.get_interpolation_matrix(
-#             [
-#                 t,
-#             ]
-#         )
-#         ave_full = sp.kron(ave_time, eye, format="csr")
-#         mappings.append(maps.LinearMap(ave_full))
-#         simulations.append(
-#             gravity.Simulation3DIntegral(
-#                 mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
-#             )
-#         )
-#
-#     meta_sim = MetaSimulation(simulations, mappings)
-#     repeat_sim = RepeatedSimulation(sim, mappings)
-#
-#     model = np.random.rand(time_mesh.n_cells, mesh.n_cells).reshape(-1)
-#
-#     # test field things
-#     f_full = meta_sim.fields(model)
-#     f_meta = repeat_sim.fields(model)
-#     np.testing.assert_equal(np.c_[f_full], np.c_[f_meta])
-#
-#     d_full = meta_sim.dpred(model, f_full)
-#     d_repeat = repeat_sim.dpred(model, f_meta)
-#     np.testing.assert_equal(d_full, d_repeat)
-#
-#     # test Jvec
-#     u = np.random.rand(len(model))
-#     jvec_full = meta_sim.Jvec(model, u, f=f_full)
-#     jvec_meta = repeat_sim.Jvec(model, u, f=f_meta)
-#     np.testing.assert_allclose(jvec_full, jvec_meta)
-#
-#     # test Jtvec
-#     v = np.random.rand(len(sim_ts) * survey.nD)
-#     jtvec_full = meta_sim.Jtvec(model, v, f=f_full)
-#     jtvec_meta = repeat_sim.Jtvec(model, v, f=f_meta)
-#     np.testing.assert_allclose(jtvec_full, jtvec_meta)
-#
-#     # test get diag
-#     diag_full = meta_sim.getJtJdiag(model, f=f_full)
-#     diag_meta = repeat_sim.getJtJdiag(model, f=f_meta)
-#     np.testing.assert_allclose(diag_full, diag_meta)
-#
-#
-# def test_metai_errors():
-#     mesh = TensorMesh([16, 16, 16], origin="CCN")
-#
-#     rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j]
-#     rx_locs = rx_locs.reshape(3, -1).T
-#     rxs = dc.receivers.Pole(rx_locs)
-#     source_locs = np.mgrid[-0.5:0.5:10j, 0:1:1j, 0:1:1j].reshape(3, -1).T
-#     src_list = [
-#         dc.sources.Pole(
-#             [
-#                 rxs,
-#             ],
-#             location=loc,
-#         )
-#         for loc in source_locs
-#     ]
-#
-#     # split by chunks of sources
-#     chunk_size = 3
-#     sims = []
-#     mappings = []
-#     for i in range(0, len(src_list) + 1, chunk_size):
-#         end = min(i + chunk_size, len(src_list))
-#         if i == end:
-#             break
-#         survey_chunk = dc.Survey(src_list[i:end])
-#         sims.append(
-#             dc.Simulation3DNodal(
-#                 mesh, survey=survey_chunk, sigmaMap=maps.IdentityMap(mesh)
-#             )
-#         )
-#         mappings.append(maps.IdentityMap(mesh))
-#
-#     # incompatible length of mappings and simulations lists
-#     with pytest.raises(ValueError):
-#         MetaSimulation(sims[:-1], mappings)
-#
-#     # mappings have incompatible input lengths:
-#     mappings[0] = maps.Projection(mesh.n_cells + 1, np.arange(mesh.n_cells) + 1)
-#     with pytest.raises(ValueError):
-#         MetaSimulation(sims, mappings)
-#
-#     # incompatible mapping and simulation
-#     mappings[0] = maps.Projection(mesh.n_cells, [0, 1, 3, 5, 10])
-#     with pytest.raises(ValueError):
-#         MetaSimulation(sims, mappings)
-#
-#
-# def test_sum_errors():
-#     mesh = TensorMesh([16, 16, 16], origin="CCN")
-#
-#     mesh_bot = TensorMesh([mesh.h[0], mesh.h[1], mesh.h[2][:8]], origin=mesh.origin)
-#     mesh_top = TensorMesh(
-#         [mesh.h[0], mesh.h[1], mesh.h[2][8:]], origin=["C", "C", mesh.nodes_z[8]]
-#     )
-#
-#     mappings = [
-#         maps.Mesh2Mesh((mesh_bot, mesh)),
-#         maps.Mesh2Mesh((mesh_top, mesh)),
-#     ]
-#
-#     rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
-#
-#     rx1 = gravity.Point(rx_locs, components=["gz"])
-#     survey1 = gravity.Survey(gravity.SourceField(rx1))
-#     rx2 = gravity.Point(rx_locs[1:], components=["gz"])
-#     survey2 = gravity.Survey(gravity.SourceField(rx2))
-#
-#     sims = [
-#         gravity.Simulation3DIntegral(
-#             mesh_bot, survey=survey1, rhoMap=maps.IdentityMap(mesh_bot), n_processes=1
-#         ),
-#         gravity.Simulation3DIntegral(
-#             mesh_top, survey=survey2, rhoMap=maps.IdentityMap(mesh_top), n_processes=1
-#         ),
-#     ]
-#
-#     # Test simulations with different numbers of data.
-#     with pytest.raises(ValueError):
-#         SumMetaSimulation(sims, mappings)
-#
-#
-# def test_repeat_errors():
-#     mesh = TensorMesh([16, 16, 16], origin="CCN")
-#
-#     rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j]
-#     rx_locs = rx_locs.reshape(3, -1).T
-#     rxs = dc.receivers.Pole(rx_locs)
-#     source_locs = np.mgrid[-0.5:0.5:10j, 0:1:1j, 0:1:1j].reshape(3, -1).T
-#     src_list = [
-#         dc.sources.Pole(
-#             [
-#                 rxs,
-#             ],
-#             location=loc,
-#         )
-#         for loc in source_locs
-#     ]
-#     survey = dc.Survey(src_list)
-#     sim = dc.Simulation3DNodal(mesh, survey=survey, sigmaMap=maps.IdentityMap(mesh))
-#
-#     # split by chunks of sources
-#     mappings = []
-#     for _i in range(10):
-#         mappings.append(maps.IdentityMap(mesh))
-#
-#     # mappings have incompatible input lengths:
-#     mappings[0] = maps.Projection(mesh.n_cells + 1, np.arange(mesh.n_cells) + 1)
-#     with pytest.raises(ValueError):
-#         RepeatedSimulation(sim, mappings)
-#
-#     # incompatible mappings and simulations
-#     mappings[0] = maps.Projection(mesh.n_cells, [0, 1, 3, 5, 10])
-#     with pytest.raises(ValueError):
-#         RepeatedSimulation(sim, mappings)
+def test_sum_sim_correctness():
+    mesh = TensorMesh([16, 16, 16], origin="CCN")
+
+    rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
+    rx = gravity.Point(rx_locs, components=["gz"])
+    survey = gravity.Survey(gravity.SourceField(rx))
+    full_sim = gravity.Simulation3DIntegral(
+        mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
+    )
+
+    mesh_bot = TensorMesh([mesh.h[0], mesh.h[1], mesh.h[2][:8]], origin=mesh.origin)
+    mesh_top = TensorMesh(
+        [mesh.h[0], mesh.h[1], mesh.h[2][8:]], origin=["C", "C", mesh.nodes_z[8]]
+    )
+
+    mappings = [
+        maps.Mesh2Mesh((mesh_bot, mesh)),
+        maps.Mesh2Mesh((mesh_top, mesh)),
+    ]
+    sims = [
+        gravity.Simulation3DIntegral(
+            mesh_bot, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
+        ),
+        gravity.Simulation3DIntegral(
+            mesh_top, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
+        ),
+    ]
+
+    sum_sim = DaskSumMetaSimulation(sims, mappings, client)
+
+    m_test = np.arange(mesh.n_cells) / mesh.n_cells + 0.1
+
+    # test fields objects
+    f_full = full_sim.fields(m_test)
+    f_meta = sum_sim.fields(m_test)
+    # Again don't serialize and collect the fields on the main
+    # process directly.
+    # np.testing.assert_allclose(f_full, sum(f_meta))
+
+    # test data output
+    d_full = full_sim.dpred(m_test, f=f_full)
+    d_meta = sum_sim.dpred(m_test, f=f_meta)
+    np.testing.assert_allclose(d_full, d_meta)
+
+    # test Jvec
+    u = np.random.rand(mesh.n_cells)
+    jvec_full = full_sim.Jvec(m_test, u, f=f_full)
+    jvec_meta = sum_sim.Jvec(m_test, u, f=f_meta)
+
+    np.testing.assert_allclose(jvec_full, jvec_meta)
+
+    # test Jtvec
+    v = np.random.rand(survey.nD)
+    jtvec_full = full_sim.Jtvec(m_test, v, f=f_full)
+    jtvec_meta = sum_sim.Jtvec(m_test, v, f=f_meta)
+
+    np.testing.assert_allclose(jtvec_full, jtvec_meta)
+
+    # test get diag
+    diag_full = full_sim.getJtJdiag(m_test, f=f_full)
+    diag_meta = sum_sim.getJtJdiag(m_test, f=f_meta)
+
+    np.testing.assert_allclose(diag_full, diag_meta)
+
+    # test things also works without passing optional kwargs
+    sum_sim.model = m_test
+    d_meta2 = sum_sim.dpred()
+    np.testing.assert_allclose(d_meta, d_meta2)
+
+    jvec_meta2 = sum_sim.Jvec(m_test, u)
+    np.testing.assert_allclose(jvec_meta, jvec_meta2)
+
+    jtvec_meta2 = sum_sim.Jtvec(m_test, v)
+    np.testing.assert_allclose(jtvec_meta, jtvec_meta2)
+
+    sum_sim._jtjdiag = None
+    diag_meta2 = sum_sim.getJtJdiag(m_test)
+    np.testing.assert_allclose(diag_meta, diag_meta2)
+
+
+def test_repeat_sim_correctness():
+    # meta sim is tested for correctness
+    # so can test the repeat against the meta sim
+    mesh = TensorMesh([8, 8, 8], origin="CCN")
+
+    rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
+    rx = gravity.Point(rx_locs, components=["gz"])
+    survey = gravity.Survey(gravity.SourceField(rx))
+    sim = gravity.Simulation3DIntegral(
+        mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
+    )
+
+    time_mesh = TensorMesh(
+        [
+            8,
+        ],
+        origin=[
+            0,
+        ],
+    )
+    sim_ts = np.linspace(0, 1, 6)
+
+    mappings = []
+    simulations = []
+    eye = sp.eye(mesh.n_cells, mesh.n_cells)
+    for t in sim_ts:
+        ave_time = time_mesh.get_interpolation_matrix(
+            [
+                t,
+            ]
+        )
+        ave_full = sp.kron(ave_time, eye, format="csr")
+        mappings.append(maps.LinearMap(ave_full))
+        simulations.append(
+            gravity.Simulation3DIntegral(
+                mesh, survey=survey, rhoMap=maps.IdentityMap(), n_processes=1
+            )
+        )
+
+    meta_sim = MetaSimulation(simulations, mappings)
+    repeat_sim = DaskRepeatedSimulation(sim, mappings, client)
+
+    model = np.random.rand(time_mesh.n_cells, mesh.n_cells).reshape(-1)
+
+    # test field things
+    f_full = meta_sim.fields(model)
+    f_meta = repeat_sim.fields(model)
+    # np.testing.assert_equal(np.c_[f_full], np.c_[f_meta])
+
+    d_full = meta_sim.dpred(model, f_full)
+    d_repeat = repeat_sim.dpred(model, f_meta)
+    np.testing.assert_equal(d_full, d_repeat)
+
+    # test Jvec
+    u = np.random.rand(len(model))
+    jvec_full = meta_sim.Jvec(model, u, f=f_full)
+    jvec_meta = repeat_sim.Jvec(model, u, f=f_meta)
+    np.testing.assert_allclose(jvec_full, jvec_meta)
+
+    # test Jtvec
+    v = np.random.rand(len(sim_ts) * survey.nD)
+    jtvec_full = meta_sim.Jtvec(model, v, f=f_full)
+    jtvec_meta = repeat_sim.Jtvec(model, v, f=f_meta)
+    np.testing.assert_allclose(jtvec_full, jtvec_meta)
+
+    # test get diag
+    diag_full = meta_sim.getJtJdiag(model, f=f_full)
+    diag_meta = repeat_sim.getJtJdiag(model, f=f_meta)
+    np.testing.assert_allclose(diag_full, diag_meta)
+
+
+def test_dask_meta_errors():
+    mesh = TensorMesh([16, 16, 16], origin="CCN")
+
+    rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j]
+    rx_locs = rx_locs.reshape(3, -1).T
+    rxs = dc.receivers.Pole(rx_locs)
+    source_locs = np.mgrid[-0.5:0.5:10j, 0:1:1j, 0:1:1j].reshape(3, -1).T
+    src_list = [
+        dc.sources.Pole(
+            [
+                rxs,
+            ],
+            location=loc,
+        )
+        for loc in source_locs
+    ]
+
+    # split by chunks of sources
+    chunk_size = 3
+    sims = []
+    mappings = []
+    for i in range(0, len(src_list) + 1, chunk_size):
+        end = min(i + chunk_size, len(src_list))
+        if i == end:
+            break
+        survey_chunk = dc.Survey(src_list[i:end])
+        sims.append(
+            dc.Simulation3DNodal(
+                mesh, survey=survey_chunk, sigmaMap=maps.IdentityMap(mesh)
+            )
+        )
+        mappings.append(maps.IdentityMap(mesh))
+
+    # incompatible length of mappings and simulations lists
+    with pytest.raises(ValueError):
+        DaskMetaSimulation(sims[:-1], mappings, client)
+
+    # Bad Simulation type?
+    with pytest.raises(TypeError):
+        DaskRepeatedSimulation(
+            len(sims)
+            * [
+                lambda x: x * 2,
+            ],
+            mappings,
+            client,
+        )
+
+    # mappings have incompatible input lengths:
+    mappings[0] = maps.Projection(mesh.n_cells + 10, np.arange(mesh.n_cells) + 1)
+    with pytest.raises(ValueError):
+        DaskMetaSimulation(sims, mappings, client)
+
+    # incompatible mapping and simulation
+    mappings[0] = maps.Projection(mesh.n_cells, [0, 1, 3, 5, 10])
+    with pytest.raises(ValueError):
+        DaskMetaSimulation(sims, mappings, client)
+
+
+def test_sum_errors():
+    mesh = TensorMesh([16, 16, 16], origin="CCN")
+
+    mesh_bot = TensorMesh([mesh.h[0], mesh.h[1], mesh.h[2][:8]], origin=mesh.origin)
+    mesh_top = TensorMesh(
+        [mesh.h[0], mesh.h[1], mesh.h[2][8:]], origin=["C", "C", mesh.nodes_z[8]]
+    )
+
+    mappings = [
+        maps.Mesh2Mesh((mesh_bot, mesh)),
+        maps.Mesh2Mesh((mesh_top, mesh)),
+    ]
+
+    rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j].reshape(3, -1).T
+
+    rx1 = gravity.Point(rx_locs, components=["gz"])
+    survey1 = gravity.Survey(gravity.SourceField(rx1))
+    rx2 = gravity.Point(rx_locs[1:], components=["gz"])
+    survey2 = gravity.Survey(gravity.SourceField(rx2))
+
+    sims = [
+        gravity.Simulation3DIntegral(
+            mesh_bot, survey=survey1, rhoMap=maps.IdentityMap(mesh_bot), n_processes=1
+        ),
+        gravity.Simulation3DIntegral(
+            mesh_top, survey=survey2, rhoMap=maps.IdentityMap(mesh_top), n_processes=1
+        ),
+    ]
+
+    # Test simulations with different numbers of data.
+    with pytest.raises(ValueError):
+        DaskSumMetaSimulation(sims, mappings, client)
+
+
+def test_repeat_errors():
+    mesh = TensorMesh([16, 16, 16], origin="CCN")
+
+    rx_locs = np.mgrid[-0.25:0.25:5j, -0.25:0.25:5j, 0:1:1j]
+    rx_locs = rx_locs.reshape(3, -1).T
+    rxs = dc.receivers.Pole(rx_locs)
+    source_locs = np.mgrid[-0.5:0.5:10j, 0:1:1j, 0:1:1j].reshape(3, -1).T
+    src_list = [
+        dc.sources.Pole(
+            [
+                rxs,
+            ],
+            location=loc,
+        )
+        for loc in source_locs
+    ]
+    survey = dc.Survey(src_list)
+    sim = dc.Simulation3DNodal(mesh, survey=survey, sigmaMap=maps.IdentityMap(mesh))
+
+    # split by chunks of sources
+    mappings = []
+    for _i in range(10):
+        mappings.append(maps.IdentityMap(mesh))
+
+    # mappings have incompatible input lengths:
+    mappings[0] = maps.Projection(mesh.n_cells + 1, np.arange(mesh.n_cells) + 1)
+    with pytest.raises(ValueError):
+        DaskRepeatedSimulation(sim, mappings, client)
+
+    # incompatible mappings and simulations
+    mappings[0] = maps.Projection(mesh.n_cells, [0, 1, 3, 5, 10])
+    with pytest.raises(ValueError):
+        DaskRepeatedSimulation(sim, mappings, client)
+
+    # Bad Simulation type?
+    with pytest.raises(TypeError):
+        DaskRepeatedSimulation(lambda x: x * 2, mappings, client)
