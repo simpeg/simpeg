ee85b17e8aacc3c58cf9f810bf059a5994657864
fourndo <fourndo@gmail.com>
2023-05-25 11:18:16 +0200

    Add latest to dask.init

--- SimPEG/dask/__init__.py
+++ SimPEG/dask/__init__.py
@@ -1,7 +1,10 @@
 try:
     import SimPEG.dask.simulation
+    import SimPEG.dask.electromagnetics.frequency_domain.simulation
     import SimPEG.dask.electromagnetics.static.resistivity.simulation
+    import SimPEG.dask.electromagnetics.static.resistivity.simulation_2d
     import SimPEG.dask.electromagnetics.static.induced_polarization.simulation
+    import SimPEG.dask.electromagnetics.static.induced_polarization.simulation_2d
     import SimPEG.dask.electromagnetics.time_domain.simulation
     import SimPEG.dask.potential_fields.base
     import SimPEG.dask.potential_fields.gravity.simulation
