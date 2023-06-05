deff39e76c23c68636e9a77a28d54d80c377fdf2
Thibaut <thibaut.astic@koboldmetals.com>
2023-06-05 11:04:11 -0700

    call getJ

--- SimPEG/dask/electromagnetics/static/induced_polarization/simulation.py
+++ SimPEG/dask/electromagnetics/static/induced_polarization/simulation.py
@@ -1,8 +1,6 @@
-from .....electromagnetics.static.induced_polarization.simulation import (
-    BaseIPSimulation as Sim,
-)
-
 import dask.array as da
+
+from .....electromagnetics.static.induced_polarization.simulation import BaseIPSimulation as Sim
 
 
 def dask_getJtJdiag(self, m, W=None, f=None):
@@ -10,13 +8,14 @@ def dask_getJtJdiag(self, m, W=None, f=None):
     Return the diagonal of JtJ
     """
     if getattr(self, "_gtgdiag", None) is None:
+        J = self.getJ(m, f=f)
         # Need to check if multiplying weights makes sense
         if W is None:
             W = self._scale
         else:
             W = self._scale * W.diagonal()
         w = da.from_array(W)[:, None]
-        self._gtgdiag = da.sum((w * self.getJ(m, f=f)) ** 2, axis=0).compute()
+        self._gtgdiag = da.sum((w * J) ** 2, axis=0).compute()
 
     return self._gtgdiag
 
