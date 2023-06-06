92accbcf2f2d918b386cd7b30dc492b6bf90fb91
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-06-06 11:33:41 -0700

    turn deprecations into future warnings

--- SimPEG/utils/code_utils.py
+++ SimPEG/utils/code_utils.py
@@ -1232,21 +1232,31 @@ def validate_active_indices(property_name, index_arr, n_cells):
 #                      DEPRECATIONS
 ###############################################################
 memProfileWrapper = deprecate_function(
-    mem_profile_class, "memProfileWrapper", removal_version="0.18.0"
+    mem_profile_class, "memProfileWrapper", removal_version="0.18.0", future_warn=True
 )
-setKwargs = deprecate_function(set_kwargs, "setKwargs", removal_version="0.18.0")
-printTitles = deprecate_function(print_titles, "printTitles", removal_version="0.18.0")
-printLine = deprecate_function(print_line, "printLine", removal_version="0.18.0")
+setKwargs = deprecate_function(
+    set_kwargs, "setKwargs", removal_version="0.18.0", future_warn=True
+)
+printTitles = deprecate_function(
+    print_titles, "printTitles", removal_version="0.18.0", future_warn=True
+)
+printLine = deprecate_function(
+    print_line, "printLine", removal_version="0.18.0", future_warn=True
+)
 printStoppers = deprecate_function(
-    print_stoppers, "printStoppers", removal_version="0.18.0"
+    print_stoppers, "printStoppers", removal_version="0.18.0", future_warn=True
 )
 checkStoppers = deprecate_function(
-    check_stoppers, "checkStoppers", removal_version="0.18.0"
+    check_stoppers, "checkStoppers", removal_version="0.18.0", future_warn=True
 )
-printDone = deprecate_function(print_done, "printDone", removal_version="0.18.0")
-callHooks = deprecate_function(call_hooks, "callHooks", removal_version="0.18.0")
+printDone = deprecate_function(
+    print_done, "printDone", removal_version="0.18.0", future_warn=True
+)
+callHooks = deprecate_function(
+    call_hooks, "callHooks", removal_version="0.18.0", future_warn=True
+)
 dependentProperty = deprecate_function(
-    dependent_property, "dependentProperty", removal_version="0.18.0"
+    dependent_property, "dependentProperty", removal_version="0.18.0", future_warn=True
 )
 asArray_N_x_Dim = deprecate_function(
     as_array_n_by_dim, "asArray_N_x_Dim", removal_version="0.19.0", future_warn=True
