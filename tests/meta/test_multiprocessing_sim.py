8f57ad8fff7792f254bffec5c075573ffbeb13a7
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-06-08 13:51:35 -0700

    add some more debug printing

--- tests/meta/test_multiprocessing_sim.py
+++ tests/meta/test_multiprocessing_sim.py
@@ -51,34 +51,40 @@ def test_meta_correctness():
 
     serial_sim = MetaSimulation(dc_sims, dc_mappings)
     parallel_sim = MultiprocessingMetaSimulation(dc_sims, dc_mappings)
+    print("In test multi sim")
 
     try:
         # create fields objects
+        print("test A")
         f_serial = serial_sim.fields(m_test)
         f_parallel = parallel_sim.fields(m_test)
+        print("test B")
 
         # test data output
         d_full = serial_sim.dpred(m_test, f=f_serial)
         d_mult = parallel_sim.dpred(m_test, f=f_parallel)
         np.testing.assert_allclose(d_full, d_mult)
+        print("test C")
 
         # test Jvec
         u = np.random.rand(mesh.n_cells)
         jvec_full = serial_sim.Jvec(m_test, u, f=f_serial)
         jvec_mult = parallel_sim.Jvec(m_test, u, f=f_parallel)
-
+        print("test D")
         np.testing.assert_allclose(jvec_full, jvec_mult)
 
         # test Jtvec
         v = np.random.rand(serial_sim.survey.nD)
         jtvec_full = serial_sim.Jtvec(m_test, v, f=f_serial)
         jtvec_mult = parallel_sim.Jtvec(m_test, v, f=f_parallel)
+        print("test E")
 
         np.testing.assert_allclose(jtvec_full, jtvec_mult)
 
         # test get diag
         diag_full = serial_sim.getJtJdiag(m_test, f=f_serial)
         diag_mult = parallel_sim.getJtJdiag(m_test, f=f_parallel)
+        print("test F")
 
         np.testing.assert_allclose(diag_full, diag_mult)
 
@@ -86,18 +92,22 @@ def test_meta_correctness():
         parallel_sim.model = m_test
         d_mult2 = parallel_sim.dpred()
         np.testing.assert_allclose(d_mult, d_mult2)
+        print("test g")
 
         jvec_mult2 = parallel_sim.Jvec(m_test, u)
         np.testing.assert_allclose(jvec_mult, jvec_mult2)
+        print("test h")
 
         jtvec_mult2 = parallel_sim.Jtvec(m_test, v)
         np.testing.assert_allclose(jtvec_mult, jtvec_mult2)
+        print("test i")
 
         # also pass a diagonal matrix here for testing.
         parallel_sim._jtjdiag = None
         W = sp.eye(parallel_sim.survey.nD)
         diag_mult2 = parallel_sim.getJtJdiag(m_test, W=W)
         np.testing.assert_allclose(diag_mult, diag_mult2)
+        print("test j")
     except Exception as err:
         raise err
     finally:
