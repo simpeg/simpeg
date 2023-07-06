679a0411df93a491bd5f605c207dd3edde40f398
dccowan <devinccowan@gmail.com>
2023-07-06 13:59:53 -0700

    Create face and edge property mass matrices. Add tests

--- SimPEG/base/__init__.py
+++ SimPEG/base/__init__.py
@@ -3,4 +3,6 @@ from .pde_simulation import (
     BaseElectricalPDESimulation,
     BaseMagneticPDESimulation,
     with_property_mass_matrices,
+    with_surface_property_mass_matrices,
+    with_line_property_mass_matrices,
 )
