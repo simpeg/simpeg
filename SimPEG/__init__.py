b9dbc489b918ef60e9970501058007531112a113
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-08-08 15:12:38 -0600

    use setuptools_scm to track version

--- SimPEG/__init__.py
+++ SimPEG/__init__.py
@@ -163,7 +163,21 @@ from .utils.solver_utils import (
     SolverBiCG,
 )
 
-__version__ = "0.19.0"
 __author__ = "SimPEG Team"
 __license__ = "MIT"
 __copyright__ = "2013 - 2020, SimPEG Team, http://simpeg.xyz"
+
+
+# Version
+try:
+    # - Released versions just tags:       0.8.0
+    # - GitHub commits add .dev#+hash:     0.8.1.dev4+g2785721
+    # - Uncommitted changes add timestamp: 0.8.1.dev4+g2785721.d20191022
+    from SimPEG.version import version as __version__
+except ImportError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. SimPEG should be
+    # installed properly!
+    from datetime import datetime
+
+    __version__ = "unknown-" + datetime.today().strftime("%Y%m%d")
