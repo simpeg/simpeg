de69e0369b2699dc623860a22636dd82dfafbd3c
Thibaut <thibaut.astic@koboldmetals.com>
2023-09-20 17:12:50 -0700

    add tests

--- tests/pf/test_forward_Mag_Linear.py
+++ tests/pf/test_forward_Mag_Linear.py
@@ -1,10 +1,12 @@
 import unittest
+
 import discretize
-from SimPEG import utils, maps
-from SimPEG.potential_fields import magnetics as mag
+import numpy as np
 from geoana.em.static import MagneticPrism
 from scipy.constants import mu_0
-import numpy as np
+
+from SimPEG import maps, utils
+from SimPEG.potential_fields import magnetics as mag
 
 
 def test_ana_mag_forward():
@@ -47,7 +49,7 @@ def test_ana_mag_forward():
     active_cells = model != 0.0
     model_reduced = model[active_cells]
 
-    # Create reduced identity map for Linear Pproblem
+    # Create reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
 
     # Create plane of observations
@@ -59,10 +61,12 @@ def test_ana_mag_forward():
     components = ["bx", "by", "bz", "tmi"]
 
     rxLoc = mag.Point(locXyz, components=components)
-    srcField = mag.SourceField([rxLoc], parameters=H0)
+    srcField = mag.UniformBackgroundField(
+        [rxLoc], amplitude=H0[0], inclination=H0[1], declination=H0[2]
+    )
     survey = mag.Survey(srcField)
 
-    # Creat reduced identity map for Linear Pproblem
+    # Creat reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
 
     sim = mag.Simulation3DIntegral(
@@ -99,6 +103,98 @@ def test_ana_mag_forward():
     np.testing.assert_allclose(d_y, d[:, 1])
     np.testing.assert_allclose(d_z, d[:, 2])
     np.testing.assert_allclose(d_t, d @ tmi)
+
+
+def test_ana_mag_tmi_grad_forward():
+    nx = 5
+    ny = 5
+
+    H0 = (50000.0, 60.0, 250.0)
+    b0 = mag.analytics.IDTtoxyz(-H0[1], H0[2], H0[0])
+    chi1 = 0.01
+    chi2 = 0.02
+
+    # Define a mesh
+    cs = 0.2
+    hxind = [(cs, 41)]
+    hyind = [(cs, 41)]
+    hzind = [(cs, 41)]
+    mesh = discretize.TensorMesh([hxind, hyind, hzind], "CCC")
+
+    # create a model of two blocks, 1 inside the other
+    block1 = np.array([[-1.5, 1.5], [-1.5, 1.5], [-1.5, 1.5]])
+    block2 = np.array([[-0.7, 0.7], [-0.7, 0.7], [-0.7, 0.7]])
+
+    def get_block_inds(grid, block):
+        return np.where(
+            (grid[:, 0] > block[0, 0])
+            & (grid[:, 0] < block[0, 1])
+            & (grid[:, 1] > block[1, 0])
+            & (grid[:, 1] < block[1, 1])
+            & (grid[:, 2] > block[2, 0])
+            & (grid[:, 2] < block[2, 1])
+        )
+
+    block1_inds = get_block_inds(mesh.cell_centers, block1)
+    block2_inds = get_block_inds(mesh.cell_centers, block2)
+
+    model = np.zeros(mesh.n_cells)
+    model[block1_inds] = chi1
+    model[block2_inds] = chi2
+
+    active_cells = model != 0.0
+    model_reduced = model[active_cells]
+
+    # Create reduced identity map for Linear Problem
+    idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
+
+    # Create plane of observations
+    xr = np.linspace(-20, 20, nx)
+    yr = np.linspace(-20, 20, ny)
+    X, Y = np.meshgrid(xr, yr)
+    Z = np.ones_like(X) * 3.0
+    locXyz = np.c_[X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]
+    components = ["tmi_x", "tmi_y", "tmi_z"]
+
+    rxLoc = mag.Point(locXyz, components=components)
+    srcField = mag.UniformBackgroundField(
+        [rxLoc], amplitude=H0[0], inclination=H0[1], declination=H0[2]
+    )
+    survey = mag.Survey(srcField)
+
+    # Creat reduced identity map for Linear Problem
+    idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
+
+    sim = mag.Simulation3DIntegral(
+        mesh,
+        survey=survey,
+        chiMap=idenMap,
+        ind_active=active_cells,
+        store_sensitivities="forward_only",
+        n_processes=None,
+    )
+
+    data = sim.dpred(model_reduced)
+    d_x = data[0::3]
+    d_y = data[1::3]
+    d_z = data[2::3]
+
+    # Compute analytical response from magnetic prism
+    prism_1 = MagneticPrism(block1[:, 0], block1[:, 1], chi1 * b0 / mu_0)
+    prism_2 = MagneticPrism(block2[:, 0], block2[:, 1], -chi1 * b0 / mu_0)
+    prism_3 = MagneticPrism(block2[:, 0], block2[:, 1], chi2 * b0 / mu_0)
+
+    d = (
+        prism_1.magnetic_field_gradient(locXyz)
+        + prism_2.magnetic_field_gradient(locXyz)
+        + prism_3.magnetic_field_gradient(locXyz)
+    ) * mu_0
+    tmi_x = (d[:, 0, 0] * b0[0] + d[:, 0, 1] * b0[1] + d[:, 0, 2] * b0[2]) / H0[0]
+    tmi_y = (d[:, 1, 0] * b0[0] + d[:, 1, 1] * b0[1] + d[:, 1, 2] * b0[2]) / H0[0]
+    tmi_z = (d[:, 2, 0] * b0[0] + d[:, 2, 1] * b0[1] + d[:, 2, 2] * b0[2]) / H0[0]
+    np.testing.assert_allclose(d_x, tmi_x, rtol=1e-10, atol=1e-12)
+    np.testing.assert_allclose(d_y, tmi_y, rtol=1e-10, atol=1e-12)
+    np.testing.assert_allclose(d_z, tmi_z, rtol=1e-10, atol=1e-12)
 
 
 def test_ana_mag_grad_forward():
@@ -141,7 +237,7 @@ def test_ana_mag_grad_forward():
     active_cells = model != 0.0
     model_reduced = model[active_cells]
 
-    # Create reduced identity map for Linear Pproblem
+    # Create reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
 
     # Create plane of observations
@@ -153,10 +249,12 @@ def test_ana_mag_grad_forward():
     components = ["bxx", "bxy", "bxz", "byy", "byz", "bzz"]
 
     rxLoc = mag.Point(locXyz, components=components)
-    srcField = mag.SourceField([rxLoc], parameters=H0)
+    srcField = mag.UniformBackgroundField(
+        [rxLoc], amplitude=H0[0], inclination=H0[1], declination=H0[2]
+    )
     survey = mag.Survey(srcField)
 
-    # Creat reduced identity map for Linear Pproblem
+    # Creat reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)))
 
     sim = mag.Simulation3DIntegral(
@@ -245,10 +343,12 @@ def test_ana_mag_vec_forward():
     components = ["bx", "by", "bz", "tmi"]
 
     rxLoc = mag.Point(locXyz, components=components)
-    srcField = mag.SourceField([rxLoc], parameters=H0)
+    srcField = mag.UniformBackgroundField(
+        [rxLoc], amplitude=H0[0], inclination=H0[1], declination=H0[2]
+    )
     survey = mag.Survey(srcField)
 
-    # Create reduced identity map for Linear Pproblem
+    # Create reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)) * 3)
 
     sim = mag.Simulation3DIntegral(
@@ -331,10 +431,12 @@ def test_ana_mag_amp_forward():
     components = ["bx", "by", "bz"]
 
     rxLoc = mag.Point(locXyz, components=components)
-    srcField = mag.SourceField([rxLoc], parameters=H0)
+    srcField = mag.UniformBackgroundField(
+        [rxLoc], amplitude=H0[0], inclination=H0[1], declination=H0[2]
+    )
     survey = mag.Survey(srcField)
 
-    # Create reduced identity map for Linear Pproblem
+    # Create reduced identity map for Linear Problem
     idenMap = maps.IdentityMap(nP=int(sum(active_cells)) * 3)
 
     sim = mag.Simulation3DIntegral(
