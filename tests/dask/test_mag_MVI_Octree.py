ce4f5d9aed4bc065908e56524e3fade02379556b
Santiago Soler <santisoler@fastmail.com>
2024-01-03 10:23:14 -0800

    Update active_cells in test on MVI octree using dask

--- tests/dask/test_mag_MVI_Octree.py
+++ tests/dask/test_mag_MVI_Octree.py
@@ -166,18 +166,18 @@ class MVIProblemTest(unittest.TestCase):
 
         # Create a Combo Regularization
         # Regularize the amplitude of the vectors
-        reg_a = regularization.Sparse(mesh, indActive=actv, mapping=wires.amp)
+        reg_a = regularization.Sparse(mesh, active_cells=actv, mapping=wires.amp)
         reg_a.norms = [0.0, 0.0, 0.0, 0.0]  # Sparse on the model and its gradients
         reg_a.mref = np.zeros(3 * nC)
 
         # Regularize the vertical angle of the vectors
-        reg_t = regularization.Sparse(mesh, indActive=actv, mapping=wires.theta)
+        reg_t = regularization.Sparse(mesh, active_cells=actv, mapping=wires.theta)
         reg_t.alpha_s = 0.0  # No reference angle
         reg_t.space = "spherical"
         reg_t.norms = [2.0, 0.0, 0.0, 0.0]  # Only norm on gradients used
 
         # Regularize the horizontal angle of the vectors
-        reg_p = regularization.Sparse(mesh, indActive=actv, mapping=wires.phi)
+        reg_p = regularization.Sparse(mesh, active_cells=actv, mapping=wires.phi)
         reg_p.alpha_s = 0.0  # No reference angle
         reg_p.space = "spherical"
         reg_p.norms = [2.0, 0.0, 0.0, 0.0]  # Only norm on gradients used
