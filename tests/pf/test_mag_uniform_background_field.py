0462c54eb6c9638ee33825fcb093e8057ea4efcf
Santiago Soler <santisoler@fastmail.com>
2024-01-05 11:54:54 -0800

    Raise error on old parameters arg in UniformBackgroundField

---
+++ tests/pf/test_mag_uniform_background_field.py
@@ -0,0 +1,19 @@
+"""
+Test the UniformBackgroundField class
+"""
+import pytest
+from SimPEG.potential_fields.magnetics import UniformBackgroundField
+
+
+def test_invalid_parameters_argument():
+    """
+    Test if error is raised after passing 'parameters' as argument
+    """
+    parameters = (1, 35, 60)
+    msg = (
+        "'parameters' property has been deprecated."
+        "Please pass the amplitude, inclination and declination"
+        " through their own arguments."
+    )
+    with pytest.raises(TypeError, match=msg):
+        UniformBackgroundField(parameters=parameters)
