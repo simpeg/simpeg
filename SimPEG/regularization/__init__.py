76126330c3b495649b15fa7100b52c1c1dcc9cd7
Santiago Soler <santisoler@fastmail.com>
2024-01-16 14:43:06 -0800

    Restore deprecated regularizations and raise error
    Restore the removed deprecated regularization classes, but raise an
    error when they are initialized.
    
    ---
    
    Co-authored by: Joseph Capriotti <josephrcapriotti@gmail.com>

--- SimPEG/regularization/__init__.py
+++ SimPEG/regularization/__init__.py
@@ -146,6 +146,7 @@ to constrain inversions.
   BaseAmplitude
 
 """
+from ..utils.code_utils import deprecate_class
 from .base import (
     BaseRegularization,
     WeightedLeastSquares,
@@ -168,3 +169,87 @@ from .vector import (
     AmplitudeSmallness,
     AmplitudeSmoothnessFirstOrder,
 )
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class SimpleSmall(Smallness):
+    """Deprecated class, replaced by Smallness."""
+
+    pass
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class SimpleSmoothDeriv(SmoothnessFirstOrder):
+    """Deprecated class, replaced by SmoothnessFirstOrder."""
+
+    pass
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class Simple(WeightedLeastSquares):
+    """Deprecated class, replaced by WeightedLeastSquares."""
+
+    def __init__(self, mesh=None, alpha_x=1.0, alpha_y=1.0, alpha_z=1.0, **kwargs):
+        # These alphas are now refered to as length_scalse in the
+        # new WeightedLeastSquares regularization
+        super().__init__(
+            mesh=mesh,
+            length_scale_x=alpha_x,
+            length_scale_y=alpha_y,
+            length_scale_z=alpha_z,
+            **kwargs
+        )
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class Tikhonov(WeightedLeastSquares):
+    """Deprecated class, replaced by WeightedLeastSquares."""
+
+    def __init__(
+        self, mesh=None, alpha_s=1e-6, alpha_x=1.0, alpha_y=1.0, alpha_z=1.0, **kwargs
+    ):
+        super().__init__(
+            mesh=mesh,
+            alpha_s=alpha_s,
+            alpha_x=alpha_x,
+            alpha_y=alpha_y,
+            alpha_z=alpha_z,
+            **kwargs
+        )
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class Small(Smallness):
+    """Deprecated class, replaced by Smallness."""
+
+    pass
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class SmoothDeriv(SmoothnessFirstOrder):
+    """Deprecated class, replaced by SmoothnessFirstOrder."""
+
+    pass
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class SmoothDeriv2(SmoothnessSecondOrder):
+    """Deprecated class, replaced by SmoothnessSecondOrder."""
+
+    pass
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class PGIwithNonlinearRelationshipsSmallness(PGIsmallness):
+    """Deprecated class, replaced by PGIsmallness."""
+
+    def __init__(self, gmm, **kwargs):
+        super().__init__(gmm, non_linear_relationships=True, **kwargs)
+
+
+@deprecate_class(removal_version="0.19.0", future_warn=True, error=True)
+class PGIwithRelationships(PGI):
+    """Deprecated class, replaced by PGI."""
+
+    def __init__(self, mesh, gmmref, **kwargs):
+        super().__init__(mesh, gmmref, non_linear_relationships=True, **kwargs)
