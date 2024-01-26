ad97ccb2ebf3a4e518e5cf79a4fd32e362ed40cb
Thibaut <thibaut.astic@koboldmetals.com>
2024-01-25 16:25:06 -0800

    add init

--- SimPEG/regularization/__init__.py
+++ SimPEG/regularization/__init__.py
@@ -149,28 +149,28 @@ to constrain inversions.
 from ..utils.code_utils import deprecate_class
 from .base import (
     BaseRegularization,
-    WeightedLeastSquares,
     BaseSimilarityMeasure,
     Smallness,
     SmoothnessFirstOrder,
     SmoothnessSecondOrder,
+    WeightedLeastSquares,
 )
+from .correspondence import LinearCorrespondence
+from .cross_gradient import CrossGradient
+from .jtv import JointTotalVariation
+from .pgi import PGI, PGIsmallness
 from .regularization_mesh import RegularizationMesh
 from .regularization_mesh_lateral import LCRegularizationMesh
-from .sparse import BaseSparse, SparseSmallness, SparseSmoothness, Sparse
-from .pgi import PGIsmallness, PGI
-from .cross_gradient import CrossGradient
-from .correspondence import LinearCorrespondence
-from .jtv import JointTotalVariation
+from .rotated import RotatedSparse, SmoothnessFullGradient
+from .sparse import BaseSparse, Sparse, SparseSmallness, SparseSmoothness
 from .vector import (
-    BaseVectorRegularization,
-    CrossReferenceRegularization,
-    BaseAmplitude,
-    VectorAmplitude,
     AmplitudeSmallness,
     AmplitudeSmoothnessFirstOrder,
+    BaseAmplitude,
+    BaseVectorRegularization,
+    CrossReferenceRegularization,
+    VectorAmplitude,
 )
-from .rotated import SmoothnessFullGradient
 
 
 @deprecate_class(removal_version="0.19.0", future_warn=True)
