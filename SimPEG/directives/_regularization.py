9ffee4b22289ecb6b26aadea113862d34ae6dd48
domfournier <dominiquef@mirageoscience.com>
2024-01-28 09:58:27 -0800

    Refactor names and create new directive for spherical weighting

--- SimPEG/directives/_regularization.py
+++ SimPEG/directives/_regularization.py
@@ -2,9 +2,10 @@ import warnings
 
 import numpy as np
 
-from SimPEG.directives import InversionDirective, UpdatePreconditioner
+from SimPEG.directives import InversionDirective, UpdatePreconditioner, BetaSchedule
 from SimPEG.regularization import Sparse, BaseSparse, SmoothnessFirstOrder
 from SimPEG.utils import validate_integer, validate_float, validate_type
+from SimPEG.utils.code_utils import deprecate_property
 
 
 class Update_IRLS(InversionDirective):
@@ -17,10 +18,15 @@ class Update_IRLS(InversionDirective):
         max_irls_iterations=20,
         update_beta=True,
         beta_search=False,
-        coolingFactor=2.0,
-        coolingRate=1,
+        cooling_factor=2.0,
+        cooling_rate=1,
         **kwargs,
     ):
+        self._input_norms: list[float] | None = None
+        self._beta_schedule: BetaSchedule | None = None
+        self._target: float | None = None
+        self._start: float | None = None
+
         self.f_old = 0
         self.f_min_change = 1e-2
         self.beta_tol = 1e-1
@@ -44,8 +50,8 @@ class Update_IRLS(InversionDirective):
         self.max_irls_iterations = max_irls_iterations
         self.update_beta = update_beta
         self.beta_search = beta_search
-        self.coolingFactor = coolingFactor
-        self.coolingRate = coolingRate
+        self.cooling_factor = cooling_factor
+        self.cooling_rate = cooling_rate
 
     @property
     def max_irls_iterations(self):
@@ -64,34 +70,58 @@ class Update_IRLS(InversionDirective):
         )
 
     @property
-    def coolingFactor(self):
-        """Beta is divided by this value every `coolingRate` iterations.
+    def cooling_factor(self):
+        """Beta is divided by this value every `cooling_rate` iterations.
 
         Returns
         -------
         float
         """
-        return self._coolingFactor
+        return self._cooling_factor
 
-    @coolingFactor.setter
-    def coolingFactor(self, value):
-        self._coolingFactor = validate_float(
-            "coolingFactor", value, min_val=0.0, inclusive_min=False
+    @cooling_factor.setter
+    def cooling_factor(self, value):
+        self._cooling_factor = validate_float(
+            "cooling_factor", value, min_val=0.0, inclusive_min=False
         )
 
+    coolingFactor = deprecate_property(
+        cooling_factor,
+        "coolingFactor",
+        "cooling_factor",
+        future_warn=True,
+        removal_version="0.22.0",
+    )
+
     @property
-    def coolingRate(self):
+    def cooling_rate(self):
         """Cool after this number of iterations.
 
         Returns
         -------
         int
         """
-        return self._coolingRate
+        return self._cooling_rate
 
-    @coolingRate.setter
-    def coolingRate(self, value):
-        self._coolingRate = validate_integer("coolingRate", value, min_val=1)
+    @cooling_rate.setter
+    def cooling_rate(self, value):
+        self._cooling_rate = validate_integer("cooling_rate", value, min_val=1)
+
+    coolingRate = deprecate_property(
+        cooling_rate,
+        "coolingRate",
+        "cooling_rate",
+        future_warn=True,
+        removal_version="0.22.0",
+    )
+
+    minGNiter = deprecate_property(
+        cooling_rate,
+        "minGNiter",
+        "cooling_rate",
+        future_warn=True,
+        removal_version="0.22.0",
+    )
 
     @property
     def update_beta(self):
@@ -154,12 +184,12 @@ class Update_IRLS(InversionDirective):
 
     def initialize(self):
         if self.mode == 1:
-            self.norms = []
+            self._input_norms = []
             for reg in self.reg.objfcts:
                 if not isinstance(reg, Sparse):
                     continue
-                self.norms.append(reg.norms)
-                reg.norms = [2.0 for obj in reg.objfcts]
+                self._input_norms.append(reg.norms)
+                reg.norms = [2.0 for _ in reg.objfcts]
                 reg.model = self.invProb.model
 
         # Update the model used by the regularization
@@ -169,20 +199,15 @@ class Update_IRLS(InversionDirective):
 
             reg.model = self.invProb.model
 
-        if self.sphericalDomain:
-            self.angleScale()
-
     def endIter(self):
-        if self.sphericalDomain:
-            self.angleScale()
+        # After reaching target misfit with l2-norm, switch to IRLS (mode:2)
+        if self.invProb.phi_d < self.start and self.mode == 1:
+            self.start_irls()
 
         # Check if misfit is within the tolerance, otherwise scale beta
-        if np.all(
-            [
-                np.abs(1.0 - self.invProb.phi_d / self.target) > self.beta_tol,
-                self.update_beta,
-                self.mode != 1,
-            ]
+        if (
+            np.abs(1.0 - self.invProb.phi_d / self.target) > self.beta_tol
+            and self.update_beta
         ):
             ratio = self.target / self.invProb.phi_d
 
@@ -191,31 +216,25 @@ class Update_IRLS(InversionDirective):
             else:
                 ratio = np.mean([0.75, ratio])
 
-            self.invProb.beta = self.invProb.beta * ratio
+            self.beta_schedule.coolingFactor = ratio
 
-            if np.all([self.mode != 1, self.beta_search]):
+            if self.beta_search:
                 print("Beta search step")
-                # self.update_beta = False
                 # Re-use previous model and continue with new beta
                 self.invProb.model = self.reg.objfcts[0].model
                 self.opt.xc = self.reg.objfcts[0].model
                 self.opt.iter -= 1
                 return
 
-        elif np.all([self.mode == 1, self.opt.iter % self.coolingRate == 0]):
-            self.invProb.beta = self.invProb.beta / self.coolingFactor
-
-        # After reaching target misfit with l2-norm, switch to IRLS (mode:2)
-        if np.all([self.invProb.phi_d < self.start, self.mode == 1]):
-            self.start_irls()
-
         # Only update after GN iterations
-        if np.all(
-            [(self.opt.iter - self.iterStart) % self.minGNiter == 0, self.mode != 1]
-        ):
+        if (
+            (self.opt.iter - self.iterStart) % self.minGNiter == 0
+        ) and self.stopping_criteria():
             if self.stopping_criteria():
                 self.opt.stopNextIteration = True
                 return
+            else:
+                self.opt.stopNextIteration = False
 
             # Print to screen
             for reg in self.reg.objfcts:
@@ -271,7 +290,7 @@ class Update_IRLS(InversionDirective):
                 obj.irls_threshold = threshold
 
         # Re-assign the norms supplied by user l2 -> lp
-        for reg, norms in zip(self.reg.objfcts, self.norms):
+        for reg, norms in zip(self.reg.objfcts, self._input_norms):
             if not isinstance(reg, Sparse):
                 continue
             reg.norms = norms
@@ -286,27 +305,12 @@ class Update_IRLS(InversionDirective):
             if not self.silent:
                 print("irls_threshold " + str(reg.objfcts[0].irls_threshold))
 
-    def angleScale(self):
+    @property
+    def beta_schedule(self) -> BetaSchedule:
         """
-        Update the scales used by regularization for the
-        different block of models
+        The beta schedule used by the directive.
         """
-        # Currently implemented for MVI-S only
-        max_p = []
-        for reg in self.reg.objfcts[0].objfcts:
-            f_m = abs(reg.f_m(reg.model))
-            max_p += [np.max(f_m)]
-
-        max_p = np.asarray(max_p).max()
-
-        max_s = [np.pi, np.pi]
-
-        for reg, var in zip(self.reg.objfcts[1:], max_s):
-            for obj in reg.objfcts:
-                # TODO Need to make weights_shapes a public method
-                obj.set_weights(
-                    angle_scale=np.ones(obj._weights_shapes[0]) * max_p / var
-                )
+        return self._beta_schedule
 
     def validate(self, directiveList):
         dList = directiveList.dList
@@ -325,6 +329,29 @@ class Update_IRLS(InversionDirective):
                 "directives list",
                 stacklevel=2,
             )
+
+        beta_schedule = [isinstance(d, BetaSchedule) for d in dList]
+        if not any(beta_schedule):
+            warnings.warn(
+                "Handling of the beta scheduling by the `Update_IRLS` directive will be deprecated "
+                "in future releases. "
+                "A `BetaSchedule` will be added to your directives list, please consider adding it yourself.",
+                stacklevel=2,
+            )
+            self._beta_schedule = BetaSchedule(
+                coolingFactor=self.cooling_factor, coolingRate=self.cooling_rate
+            )
+            directiveList.dList.append(self._beta_schedule)
+
+        if self.sphericalDomain:
+            warnings.warn(
+                "The scaling of spherical parameters by the `Update_IRLS` directive will be deprecated "
+                "in future releases. "
+                "A `SphericalDomain` will be added to your directives list, please consider adding it yourself.",
+                stacklevel=2,
+            )
+            directiveList.dList.append(SphericalDomain())
+
         return True
 
     def stopping_criteria(self):
@@ -348,12 +375,11 @@ class Update_IRLS(InversionDirective):
 
         # Check if the function has changed enough
         f_change = np.abs(self.f_old - phim_new) / (self.f_old + 1e-12)
-        if np.all(
-            [
-                f_change < self.f_min_change,
-                self.irls_iteration > 1,
-                np.abs(1.0 - self.invProb.phi_d / self.target) < self.beta_tol,
-            ]
+
+        if (
+            f_change < self.f_min_change
+            and self.irls_iteration > 1
+            and np.abs(1.0 - self.invProb.phi_d / self.target) < self.beta_tol
         ):
             print("Minimum decrease in regularization." + "End of IRLS")
             return True
@@ -361,3 +387,46 @@ class Update_IRLS(InversionDirective):
         self.f_old = phim_new
 
         return False
+
+
+class SphericalDomain(InversionDirective):
+    """
+    Directive to update the regularization weights to account for spherical
+    parameters in radian and SI.
+
+    The scaling applied to the regularization weights is based on the ratio
+    between the maximum value of the model and the maximum value of angles (pi).
+    """
+
+    def initialize(self):
+        self.component_scaling()
+
+    def endIter(self):
+        self.component_scaling()
+
+    def component_scaling(self):
+        """
+        Add an 'angle_scale' to the list of weights on the angle regularization for the
+        different block of models to account for units of radian and SI.
+        """
+        # TODO Need to establish a clearer connection between the regularizations
+        # and the model blocks. There is an assumption here that the first
+        # regularization controls the amplitude.
+        max_p = []
+        for reg in self.reg.objfcts[0].objfcts:
+            f_m = abs(reg.f_m(reg.model))
+            max_p += [np.max(f_m)]
+
+        max_p = np.asarray(max_p).max()
+
+        max_s = [np.pi, np.pi]
+
+        for reg, var in zip(self.reg.objfcts, max_s):
+            if reg.units != "radian":
+                continue
+
+            for obj in reg.objfcts:
+                # TODO Need to make weights_shapes a public method
+                obj.set_weights(
+                    angle_scale=np.ones(obj._weights_shapes[0]) * max_p / var
+                )
