4038ec2ef0aadd59b685787934ccd8b50e44432f
Lindsey Heagy <lindseyheagy@gmail.com>
2024-02-01 16:29:26 -0800

    add face divergence to FDEM H fields object as we need it for computing charge density

--- SimPEG/electromagnetics/frequency_domain/fields.py
+++ SimPEG/electromagnetics/frequency_domain/fields.py
@@ -1379,6 +1379,7 @@ class Fields3DMagneticField(FieldsFDEM):
         self._nC = self.simulation.mesh.nC
         self._MfI = self.simulation.MfI
         self._MeI = self.simulation.MeI
+        self._faceDiv = self.simulation.mesh.face_divergence
 
     def _GLoc(self, fieldType):
         if fieldType in ["h", "hSecondary", "hPrimary", "b"]:
