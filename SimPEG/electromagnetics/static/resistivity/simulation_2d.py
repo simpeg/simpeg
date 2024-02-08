f808aebb43175dd3a37311c980aadf4a5a5dddad
dccowan <devinccowan@gmail.com>
2024-02-08 07:37:54 -0800

    revert kz back to ky

--- SimPEG/electromagnetics/static/resistivity/simulation_2d.py
+++ SimPEG/electromagnetics/static/resistivity/simulation_2d.py
@@ -33,7 +33,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
         The mesh.
     survey : None, Survey
         The DC resisitivity survey.
-    nkz : int
+    nky : int
         Number of evaluations of the 2D problem in the wave domain.
     storeJ : bool
         Whether to construct and store the sensitivity matrix.
@@ -60,7 +60,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
         self,
         mesh,
         survey=None,
-        nkz=11,
+        nky=11,
         storeJ=False,
         miniaturize=False,
         do_trap=False,
@@ -70,11 +70,11 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
     ):
         super().__init__(mesh=mesh, survey=survey, **kwargs)
         if 'nky' in kwargs:
-            nkz = kwargs.pop['nky']
+            nky = kwargs.pop['nky']
             warnings.warn(
-                'nky is going to be deprecated and replaced by nkz', DeprecationWarning
+                'nky is going to be deprecated and replaced by nky', DeprecationWarning
             )
-        self.nkz = nkz
+        self.nky = nky
         self.storeJ = storeJ
         self.fix_Jmatrix = fix_Jmatrix
         self.surface_faces = surface_faces
@@ -110,7 +110,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
             min_rinv = -np.log10(rs).max()
             max_rinv = -np.log10(rs).min()
             # a decent initial guess of the k_i's for the optimization = 1/rs
-            k_i = np.linspace(min_rinv, max_rinv, self.nkz)
+            k_i = np.linspace(min_rinv, max_rinv, self.nky)
 
             # these functions depend on r, so grab them
             func, g_func = get_phi(rs)
@@ -127,7 +127,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
             if not out["success"]:
                 warnings.warn(
                     "Falling back to trapezoidal for integration. "
-                    "You may need to change nkz.",
+                    "You may need to change nky.",
                     stacklevel=2,
                 )
                 do_trap = True
@@ -136,7 +136,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
                 print("doing trap")
             y = 0.0
 
-            points = np.logspace(-4, 1, self.nkz)
+            points = np.logspace(-4, 1, self.nky)
             dky = np.diff(points) / 2
             weights = np.r_[dky, 0] + np.r_[0, dky]
             weights *= np.cos(points * y)  # *(1.0/np.pi)
@@ -147,8 +147,8 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
         self._quad_weights = weights
         self._quad_points = points
 
-        self.Ainv = [None for i in range(self.nkz)]
-        self.nT = self.nkz - 1  # Only for using TimeFields
+        self.Ainv = [None for i in range(self.nky)]
+        self.nT = self.nky - 1  # Only for using TimeFields
 
         # Do stuff to simplify the forward and JTvec operation if number of dipole
         # sources is greater than the number of unique pole sources
@@ -175,18 +175,18 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
         self._survey = value
 
     @property
-    def nkz(self):
-        """Number of kzs to use in wavenumber space.
+    def nky(self):
+        """Number of kys to use in wavenumber space.
 
         Returns
         -------
         int
         """
-        return self._nkz
+        return self._nky
 
-    @nkz.setter
-    def nkz(self, value):
-        self._nkz = validate_integer("nkz", value, min_val=3)
+    @nky.setter
+    def nky(self, value):
+        self._nky = validate_integer("nky", value, min_val=3)
 
     @property
     def storeJ(self):
@@ -243,7 +243,7 @@ class BaseDCSimulation2D(BaseElectricalPDESimulation):
         if m is not None:
             self.model = m
         if self.Ainv[0] is not None:
-            for i in range(self.nkz):
+            for i in range(self.nky):
                 self.Ainv[i].clean()
         f = self.fieldsPair(self)
         kys = self._quad_points
@@ -770,13 +770,13 @@ class Simulation2DNodal(BaseDCSimulation2D):
         return self._MnSigma2D
     
 
-    def getA(self, kz):
+    def getA(self, ky):
         """
         Make the A matrix for the cell centered DC resistivity problem
         A = D MfRhoI G
         """
         # To handle Mixed boundary condition
-        self.setBC(kz=kz)
+        self.setBC(ky=ky)
 
         MeSigma = self.MeSigma2D
         MnSigma = self.MnSigma2D
@@ -784,11 +784,11 @@ class Simulation2DNodal(BaseDCSimulation2D):
         if self._gradT is None:
             self._gradT = Grad.T.tocsr()  # cache the .tocsr()
         GradT = self._gradT
-        A = GradT * MeSigma * Grad + kz**2 * MnSigma
+        A = GradT * MeSigma * Grad + ky**2 * MnSigma
 
         if self.bc_type != "Neumann":
             try:
-                A = A + sdiag(self._AvgBC[kz] @ self.sigma)
+                A = A + sdiag(self._AvgBC[ky] @ self.sigma)
             except ValueError as err:
                 if len(self.sigma) != len(self.mesh):
                     raise NotImplementedError(
@@ -799,51 +799,51 @@ class Simulation2DNodal(BaseDCSimulation2D):
                     raise err
         return A
 
-    def getADeriv(self, kz, u, v, adjoint=False):
+    def getADeriv(self, ky, u, v, adjoint=False):
         Grad = self.mesh.nodal_gradient
 
         if adjoint:
             out = self.MeSigmaDeriv(
                 Grad * u, Grad * v, adjoint=adjoint
-            ) + kz**2 * self.MnSigmaDeriv(u, v, adjoint=adjoint)
+            ) + ky**2 * self.MnSigmaDeriv(u, v, adjoint=adjoint)
         else:
             out = Grad.T * self.MeSigmaDeriv(
                 Grad * u, v, adjoint=adjoint
-            ) + kz**2 * self.MnSigmaDeriv(u, v, adjoint=adjoint)
+            ) + ky**2 * self.MnSigmaDeriv(u, v, adjoint=adjoint)
         if self.bc_type != "Neumann" and self.sigmaMap is not None:
             if getattr(self, "_MBC_sigma", None) is None:
                 self._MBC_sigma = {}
-            if kz not in self._MBC_sigma:
-                self._MBC_sigma[kz] = self._AvgBC[kz] @ self.sigmaDeriv
+            if ky not in self._MBC_sigma:
+                self._MBC_sigma[ky] = self._AvgBC[ky] @ self.sigmaDeriv
             if not isinstance(u, Zero):
                 u = u.flatten()
                 if v.ndim > 1:
                     u = u[:, None]
                 if not adjoint:
-                    out += u * (self._MBC_sigma[kz] @ v)
+                    out += u * (self._MBC_sigma[ky] @ v)
                 else:
-                    out += self._MBC_sigma[kz].T @ (u * v)
+                    out += self._MBC_sigma[ky].T @ (u * v)
         return out
 
-    def getRHS(self, kz):
+    def getRHS(self, ky):
         """
         RHS for the DC problem
         q
         """
 
-        RHS = self.getSourceTerm(kz)
+        RHS = self.getSourceTerm(ky)
         return RHS
 
-    def getRHSDeriv(self, kz, src, v, adjoint=False):
+    def getRHSDeriv(self, ky, src, v, adjoint=False):
         """
         Derivative of the right hand side with respect to the model
         """
         # TODO: add qDeriv for RHS depending on m
-        # qDeriv = src.evalDeriv(self, kz, adjoint=adjoint)
+        # qDeriv = src.evalDeriv(self, ky, adjoint=adjoint)
         # return qDeriv
         return Zero()
 
-    def setBC(self, kz=None):
+    def setBC(self, ky=None):
         if self.bc_type == "Dirichlet":
             # do nothing
             raise ValueError(
@@ -858,7 +858,7 @@ class Simulation2DNodal(BaseDCSimulation2D):
         else:
             if getattr(self, "_AvgBC", None) is None:
                 self._AvgBC = {}
-            if kz in self._AvgBC:
+            if ky in self._AvgBC:
                 return
             mesh = self.mesh
             # calculate alpha, beta, gamma at the boundary faces
@@ -900,9 +900,9 @@ class Simulation2DNodal(BaseDCSimulation2D):
 
             # use the exponentiall scaled modified bessel function of second kind,
             # (the division will cancel out the scaling)
-            # This is more stable for large values of kz * r
+            # This is more stable for large values of ky * r
             # actual ratio is k1/k0...
-            alpha[not_top] = (kz * k1e(kz * r) / k0e(kz * r) * r_dot_n)[not_top]
+            alpha[not_top] = (ky * k1e(ky * r) / k0e(ky * r) * r_dot_n)[not_top]
 
             P_bf = self.mesh.project_face_to_boundary_face
 
@@ -910,7 +910,7 @@ class Simulation2DNodal(BaseDCSimulation2D):
             AvgCC2Fb = P_bf @ self.mesh.average_cell_to_face
 
             AvgCC2Fb = sdiag(alpha * (P_bf @ self.mesh.face_areas)) @ AvgCC2Fb
-            self._AvgBC[kz] = AvgN2Fb.T @ AvgCC2Fb
+            self._AvgBC[ky] = AvgN2Fb.T @ AvgCC2Fb
 
 
 Simulation2DCellCentred = Simulation2DCellCentered  # UK and US
