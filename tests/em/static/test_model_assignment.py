76da647442b05fe3cd94e12a6fd8fccd7931f028
Santiago Soler <santisoler@fastmail.com>
2024-02-26 11:24:08 -0800

    Start adding tests for SIP simulations

--- tests/em/static/test_model_assignment.py
+++ tests/em/static/test_model_assignment.py
@@ -107,3 +107,58 @@ class TestDCSimulations:
         assert model_2 is simulation.model
         # Check if the two Js are different
         assert not np.allclose(j_1, j_2)
+
+
+class TestSIPSimulations:
+    @pytest.fixture
+    def mesh_3d(self):
+        """Sample mesh."""
+        cs = 25.0
+        hx = [(cs, 0, -1.3), (cs, 21), (cs, 0, 1.3)]
+        hy = [(cs, 0, -1.3), (cs, 21), (cs, 0, 1.3)]
+        hz = [(cs, 0, -1.3), (cs, 20)]
+        mesh = TensorMesh([hx, hy, hz], x0="CCN")
+        return mesh
+
+    @pytest.fixture
+    def survey_3d(self, mesh_3d):
+        """Sample survey."""
+        x = mesh_3d.cell_centers_x[
+            (mesh_3d.cell_centers_x > -155.0) & (mesh_3d.cell_centers_x < 155.0)
+        ]
+        y = mesh_3d.cell_centers_y[
+            (mesh_3d.cell_centers_y > -155.0) & (mesh_3d.cell_centers_y < 155.0)
+        ]
+        Aloc = np.r_[-200.0, 0.0, 0.0]
+        Bloc = np.r_[200.0, 0.0, 0.0]
+        M = utils.ndgrid(x - 25.0, y, np.r_[0.0])
+
+        times = np.arange(10) * 1e-3 + 1e-3
+        rx = sip.receivers.Pole(M, times)
+        src = sip.sources.Dipole([rx], Aloc, Bloc)
+        survey = sip.Survey([src])
+        return survey
+
+    def test_simulation_3d(self, mesh_3d, survey_3d):
+        """
+        Test model assignment on the ``getJ`` method of 3d simulations
+        """
+        wires = Wires(("eta", mesh_3d.nC), ("taui", mesh_3d.nC))
+        simulation = sip.Simulation3DNodal(
+            mesh_3d,
+            survey=survey_3d,
+            etaMap=wires.eta,
+            tauiMap=wires.taui,
+        )
+        sigma_1 = np.ones(mesh_3d.nC) * 1e-2
+        sigma_2 = np.ones(mesh_3d.nC) * 1e-1
+        model_1 = np.r_[sigma_1, 1.0 / sigma_1]
+        model_2 = np.r_[sigma_2, 1.0 / sigma_2]
+        # Call `getJ` passing a model and check if it was correctly assigned
+        j_1 = simulation.getJ(model_1)
+        assert model_1 is simulation.model
+        # Call `getJ` passing a different model and check if it was correctly assigned
+        j_2 = simulation.getJ(model_2)
+        assert model_2 is simulation.model
+        # Check if the two Js are different
+        assert not np.allclose(j_1, j_2)
