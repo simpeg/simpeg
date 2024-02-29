2c0f6423ee3a2767aac00f51b614216c995c8c5c
Santiago Soler <santisoler@fastmail.com>
2024-02-29 13:49:38 -0800

    Update other tests to use UniformBackgroundField

--- tests/dask/test_mag_nonLinear_Amplitude.py
+++ tests/dask/test_mag_nonLinear_Amplitude.py
@@ -22,7 +22,7 @@ import shutil
 class AmpProblemTest(unittest.TestCase):
     def setUp(self):
         # We will assume a vertical inducing field
-        H0 = (50000.0, 90.0, 0.0)
+        h0_amplitude, h0_inclination, h0_declination = (50000.0, 90.0, 0.0)
 
         # The magnetization is set along a different direction (induced + remanence)
         M = np.array([45.0, 90.0])
@@ -47,8 +47,11 @@ class AmpProblemTest(unittest.TestCase):
         # Create a MAGsurvey
         rxLoc = np.c_[mkvc(X.T), mkvc(Y.T), mkvc(Z.T)]
         receiver_list = magnetics.receivers.Point(rxLoc)
-        srcField = magnetics.sources.SourceField(
-            receiver_list=[receiver_list], parameters=H0
+        srcField = magnetics.sources.UniformBackgroundField(
+            receiver_list=[receiver_list],
+            amplitude=h0_amplitude,
+            inclination=h0_inclination,
+            declination=h0_declination,
         )
         survey = magnetics.survey.Survey(srcField)
 
@@ -186,8 +189,11 @@ class AmpProblemTest(unittest.TestCase):
         #
 
         receiver_list = magnetics.receivers.Point(rxLoc, components=["bx", "by", "bz"])
-        srcField = magnetics.sources.SourceField(
-            receiver_list=[receiver_list], parameters=H0
+        srcField = magnetics.sources.UniformBackgroundField(
+            receiver_list=[receiver_list],
+            amplitude=h0_amplitude,
+            inclination=h0_inclination,
+            declination=h0_declination,
         )
         surveyAmp = magnetics.survey.Survey(srcField)
 
