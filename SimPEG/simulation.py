ba4c9d02d5c1b42f482f38b8e8480a7bbc36222b
Santiago Soler <santisoler@fastmail.com>
2024-03-13 10:23:25 -0700

    Improve rst syntax for shape of time_steps

--- SimPEG/simulation.py
+++ SimPEG/simulation.py
@@ -664,7 +664,7 @@ class BaseTimeSimulation(BaseSimulation):
     Notes
     -----
     There are two ways in which the user can set the ``time_steps`` property
-    for the forward simulation. The most basic approach is to use a (n_steps, )
+    for the forward simulation. The most basic approach is to use a ``(n_steps, )``
     :py:class:`numpy.ndarray` that explicitly defines the step lengths in order.
     I.e.:
 
