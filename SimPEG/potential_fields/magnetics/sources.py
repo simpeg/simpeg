1a3dbb40f844fe3130c0e670e176f7d8f89983ac
Santiago Soler <santisoler@fastmail.com>
2024-04-11 09:31:35 -0700

    Simplify interface for UniformBackgroundField
    Remove the `**kwargs` from the constructor of `UniformBackgroundField`.
    Make `receiver_list`, `amplitude`, `inclination` and `declination` as
    required arguments without any default value. Include type hints in the
    signature of the constructor.

--- SimPEG/potential_fields/magnetics/sources.py
+++ SimPEG/potential_fields/magnetics/sources.py
@@ -2,6 +2,8 @@ from ...survey import BaseSrc
 from SimPEG.utils.mat_utils import dip_azimuth2cartesian
 from SimPEG.utils.code_utils import deprecate_class, validate_float
 
+from .receivers import Point
+
 
 class UniformBackgroundField(BaseSrc):
     """A constant uniform background magnetic field.
@@ -11,39 +13,27 @@ class UniformBackgroundField(BaseSrc):
 
     Parameters
     ----------
-    receiver_list : list of SimPEG.potential_fields.magnetics.Point
-    amplitude : float, optional
+    receiver_list : list of SimPEG.potential_fields.magnetics.Point or None
+        Point magnetic receivers.
+    amplitude : float
         amplitude of the inducing backgound field, usually this is in units of nT.
-    inclination : float, optional
+    inclination : float
         Dip angle in degrees from the horizon, positive points into the earth.
-    declination : float, optional
+    declination : float
         Azimuthal angle in degrees from north, positive clockwise.
     """
 
     def __init__(
         self,
-        receiver_list=None,
-        amplitude=50000.0,
-        inclination=90.0,
-        declination=0.0,
-        **kwargs,
+        receiver_list: list[Point] | None,
+        amplitude: float,
+        inclination: float,
+        declination: float,
     ):
-        # Raise errors on 'parameters' argument
-        #   The parameters argument was supported in the deprecated SourceField
-        #   class. We would like to raise an error in case the user passes it
-        #   so the class doesn't behave differently than expected.
-        if (key := "parameters") in kwargs:
-            raise TypeError(
-                f"'{key}' property has been removed."
-                "Please pass the amplitude, inclination and declination"
-                " through their own arguments."
-            )
-
         self.amplitude = amplitude
         self.inclination = inclination
         self.declination = declination
-
-        super().__init__(receiver_list=receiver_list, **kwargs)
+        super().__init__(receiver_list=receiver_list)
 
     @property
     def amplitude(self):
