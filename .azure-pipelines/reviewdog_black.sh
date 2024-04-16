7cc1df6f9594e9c6d064bedac80682738763cc73
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-04-16 13:45:43 -0600

    fix call

--- .azure-pipelines/reviewdog_black.sh
+++ .azure-pipelines/reviewdog_black.sh
@@ -12,19 +12,18 @@ black_exit_val="0"
 reviewdog_exit_val="0"
 
 echo "Checking python code"
-
 black_check_output="$(black --check . 2>&1)" ||
-    black_exit_val="$?"
+  black_exit_val="$?"
 
 echo "black output:"
 echo "${black_check_output}"
 
-echo "${black_check_output}" | /tmp/reviewdog -f="black"
+echo "${black_check_output}" | /tmp/reviewdog -f="black" \
   -name="black-format" \
   -reporter="github-pr-check" \
   -filter-mode="added" \
-  -level="error"\
-  -fail-on-error="true" || reviewdog_exit_val="$?"
+  -fail-on-error="true" \
+  -level="error" | reviewdog_exit_val="$?"
 
 # Throw error if an error occurred and fail_on_error is true.
 if [[ ("${black_exit_val}" -ne '0' || "${reviewdog_exit_val}" -eq "1") ]]; then
