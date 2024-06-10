0c583d78700041c3e56e1e282a29948233f986f9
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-06-10 13:31:48 -0600

    fix parser to use tomllib (in the default library as of 3.11

--- .ci/parse_style_requirements.py
+++ .ci/parse_style_requirements.py
@@ -1,10 +1,12 @@
-import toml
+import tomllib
 import pathlib
 
 root_dir = pathlib.Path(__file__).parent.parent.resolve()
 pyproject_file = root_dir / "pyproject.toml"
 
-pyproject = toml.load(pyproject_file)
+with open(pyproject_file, "rb") as f:
+    pyproject = tomllib.load(f)
+
 style_requirements = pyproject["project"]["optional-dependencies"]["style"]
 for req in style_requirements:
     print(req)
