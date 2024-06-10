af5fdc85c3106ee8d883c6c51292d2005737d728
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-06-10 13:32:06 -0600

    no need to use a temporary file within the shell script

--- .ci/install_style.sh
+++ .ci/install_style.sh
@@ -3,15 +3,10 @@ set -x #echo on
 
 # get directory of this script
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-echo $script_dir
-
 style_script=$script_dir/parse_style_requirements.py
-out_file=$script_dir/requirements_style.txt
 
-# generate the style_requirements file
-python $style_script > $out_file
+# parse the style requirements
+requirements=$(python $style_script)
 
-# then install them.
-pip install -r $out_file
+pip install $requirements
 
