b1d0666bea3df4f128679a31890cd942c6ff73b1
domfournier <dominiquef@mirageoscience.com>
2024-06-25 10:15:24 -0700

    Update simpeg/directives/_regularization.py
    Co-authored-by: Santiago Soler <santisoler@fastmail.com>

--- simpeg/directives/_regularization.py
+++ simpeg/directives/_regularization.py
@@ -17,11 +17,14 @@ class IRLSMetrics:
 
     Parameters
     ----------
-
-    input_norms: List of norms temporarily stored during the initialization.
-    irls_iteration_count: Number of IRLS iterations.
-    start_irls_iter: Iteration number when the IRLS process started.
-    f_old: Previous value of the regularization function.
+    input_norms : list of floats
+        List of norms temporarily stored during the initialization.
+    irls_iteration_count : int
+        Number of IRLS iterations.
+    start_irls_iter : int or None
+        Iteration number when the IRLS process started.
+    f_old : float
+        Previous value of the regularization function.
     """
 
     input_norms: list[float] = (2.0, 2.0, 2.0, 2.0)
