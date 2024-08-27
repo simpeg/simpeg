eff11b5fd757cb57585d04ff280bd080c12dd17f
Santiago Soler <santisoler@fastmail.com>
2024-08-27 13:47:09 -0700

    Write simple test for gravity eq sources

--- tests/pf/test_equivalent_sources.py
+++ tests/pf/test_equivalent_sources.py
@@ -1,30 +1,11 @@
 import pytest
 
 import numpy as np
-from discretize import TensorMesh
 from discretize.utils import mesh_builder_xyz, mkvc, refine_tree_xyz
 
-import numba
-import choclo
-
-from simpeg import (
-    data_misfit,
-    directives,
-    inverse_problem,
-    inversion,
-    maps,
-    optimization,
-    regularization,
-    utils,
-)
-from simpeg.potential_fields import gravity, magnetics
-
-
-def create_topography(x, y, amplitude=50, scale_factor=100):
-    """Create synthetic Gaussian topography from a function"""
-    return amplitude * np.exp(
-        -0.5 * ((x / scale_factor) ** 2.0 + (y / scale_factor) ** 2.0)
-    )
+import simpeg
+from simpeg.optimization import ProjectedGNCG
+from simpeg.potential_fields import gravity
 
 
 def create_grid(x_range, y_range, spacing):
@@ -38,7 +19,7 @@ def create_grid(x_range, y_range, spacing):
 
 
 @pytest.fixture
-def mesh(topography, coordinates):
+def tree_mesh(coordinates):
     """Sample 2D mesh to use with equivalent sources."""
     # Define parameters for building the mesh
     h = [5, 5]
@@ -46,7 +27,7 @@ def mesh(topography, coordinates):
     nCpad = [2, 4]
     # Build mesh
     mesh = mesh_builder_xyz(
-        topography[:, :2], h, padding_distance=padDist, mesh_type="TREE"
+        coordinates[:, :2], h, padding_distance=padDist, mesh_type="TREE"
     )
     # Refine mesh
     mesh = refine_tree_xyz(
@@ -61,35 +42,29 @@ def mesh(topography, coordinates):
 
 
 @pytest.fixture
-def topography():
-    """Synthetic topography grid."""
-    x, y = create_grid(x_range=(-200, 200), y_range=(-200, 200), spacing=50)
-    z = create_topography(x, y)
-    return np.c_[mkvc(x), mkvc(y), mkvc(z)]
+def mesh_top():
+    return -20.0
+
+
+@pytest.fixture
+def mesh_bottom():
+    return -50.0
 
 
 @pytest.fixture
 def coordinates():
-    """Synthetic observation points grid on 5m above the topography."""
+    """Synthetic observation points grid."""
     x, y = create_grid(x_range=(-100, 100), y_range=(-100, 100), spacing=50)
-    z = create_topography(x, y) + 5.0
+    z = np.full_like(x, fill_value=5.0)
     return np.c_[mkvc(x), mkvc(y), mkvc(z)]
 
 
 @pytest.fixture
-def coordinates_constant_height():
-    """Synthetic observation points grid with all points at the same height."""
-    x, y = create_grid(x_range=(-100, 100), y_range=(-100, 100), spacing=50)
-    z = np.full_like(x, 5.0)
-    return np.c_[mkvc(x), mkvc(y), mkvc(z)]
-
-
-@pytest.fixture
-def block_model(mesh):
+def block_model(tree_mesh):
     """Build a block model."""
-    model = utils.model_builder.add_block(
-        mesh.cell_centers,
-        np.zeros(mesh.n_cells),
+    model = simpeg.utils.model_builder.add_block(
+        tree_mesh.cell_centers,
+        np.zeros(tree_mesh.n_cells),
         np.r_[-20, -20],
         np.r_[20, 20],
         1.0,
@@ -99,116 +74,93 @@ def block_model(mesh):
 
 class TestGravityEquivalentSources:
 
-    # @pytest.fixture
-    # def synthetic_data(self, block, coordinates):
-    #     """Synthetic gravity data."""
-    #     data = gravity_forward(coordinates, block, 3200.0)
-    #     data += np.random.default_rng(seed=42).normal(scale=1.0, size=data.size)
-    #     return data
-
-    def test_gravity_equivalent_sources(self, coordinates, mesh, block_model):
+    @pytest.fixture
+    def survey(self, coordinates):
+        """
+        Sample survey for the gravity equivalent sources.
+        """
         receivers = gravity.Point(coordinates)
         source_field = gravity.SourceField([receivers])
         survey = gravity.Survey(source_field)
-        identity_map = maps.IdentityMap(nP=mesh.n_cells)
+        return survey
 
-        bottom, top = -5.0, 0.0
-        eq_sources_simulation = gravity.SimulationEquivalentSourceLayer(
-            mesh, top, bottom, survey=survey, rhoMap=identity_map
+    @pytest.fixture
+    def model(self, block_model):
+        """
+        Sample model the gravity equivalent sources.
+        """
+        density = 2.67  # in g/cc
+        return density * block_model
+
+    @pytest.fixture
+    def mapping(self, tree_mesh):
+        return simpeg.maps.IdentityMap(nP=tree_mesh.n_cells)
+
+    @pytest.fixture
+    def simulation(self, tree_mesh, mesh_bottom, mesh_top, survey, mapping):
+        simulation = gravity.SimulationEquivalentSourceLayer(
+            tree_mesh, mesh_top, mesh_bottom, survey=survey, rhoMap=mapping
         )
+        return simulation
 
-        # Create the gravity forward model operator
-        eq_sources_simulation = gravity.SimulationEquivalentSourceLayer(
-            mesh,
-            0.0,
-            -5.0,
-            survey=survey,
-            rhoMap=identity_map,
-            store_sensitivities="ram",
-        )
-
-        # Define model
-        model = 2.67 * block_model
-
-        synthetic_data = eq_sources_simulation.make_synthetic_data(
+    @pytest.fixture
+    def synthetic_data(self, simulation, model):
+        data = simulation.make_synthetic_data(
             model,
             relative_error=0.0,
-            noise_floor=1e-6,
+            noise_floor=1e-3,
             add_noise=True,
             random_seed=1,
         )
+        return data
 
-        # Create a regularization
-        reg = regularization.Sparse(mesh, mapping=identity_map)
-        reg.norms = [0, 0, 0]
-        reg.gradient_type = "components"
-
-        # Data misfit function
-        dmis = data_misfit.L2DataMisfit(
-            simulation=eq_sources_simulation, data=synthetic_data
+    def build_inversion(self, tree_mesh, simulation, synthetic_data):
+        """Build inversion problem."""
+        # Build data misfit and regularization terms
+        data_misfit = simpeg.data_misfit.L2DataMisfit(
+            simulation=simulation, data=synthetic_data
         )
-
-        # Add directives to the inversion
-        opt = optimization.ProjectedGNCG(
-            maxIter=40,
-            lower=-5.0,
-            upper=5.0,
+        regularization = simpeg.regularization.WeightedLeastSquares(mesh=tree_mesh)
+        # Choose optimization
+        optimization = ProjectedGNCG(
             maxIterLS=5,
             maxIterCG=20,
             tolCG=1e-4,
         )
-
-        inversion_prob = inverse_problem.BaseInvProblem(dmis, reg, opt, beta=1e3)
-
-        # Build directives
-        update_irls = directives.Update_IRLS(
-            f_min_change=1e-3,
-            max_irls_iterations=30,
-            beta_tol=1e-1,
-            beta_search=False,
+        # Build inverse problem
+        inverse_problem = simpeg.inverse_problem.BaseInvProblem(
+            data_misfit, regularization, optimization
         )
-        sensitivity_weights = directives.UpdateSensitivityWeights()
-        update_jacobi = directives.UpdatePreconditioner()
-        inv = inversion.BaseInversion(
-            inversion_prob,
-            directiveList=[update_irls, sensitivity_weights, update_jacobi],
+        # Define directives
+        starting_beta = simpeg.directives.BetaEstimate_ByEig(beta0_ratio=1e-1, seed=42)
+        beta_schedule = simpeg.directives.BetaSchedule(coolingFactor=3, coolingRate=1)
+        update_jacobi = simpeg.directives.UpdatePreconditioner()
+        target_misfit = simpeg.directives.TargetMisfit(chifact=1)
+        sensitivity_weights = simpeg.directives.UpdateSensitivityWeights(
+            every_iteration=False
         )
+        directives = [
+            sensitivity_weights,
+            starting_beta,
+            beta_schedule,
+            update_jacobi,
+            target_misfit,
+        ]
+        # Define inversion
+        inversion = simpeg.inversion.BaseInversion(inverse_problem, directives)
+        return inversion
 
-        recovered_model = inv.run(np.zeros(mesh.n_cells))
-
-        # Compute predicted data
-        prediction = eq_sources_simulation.dpred(recovered_model)
-
-        # -------------------------------
-        import matplotlib.pyplot as plt
-
-        x, y = coordinates[:, 0].ravel(), coordinates[:, 1].ravel()
-        size_per_side = int(np.sqrt(x.size))
-        shape = (size_per_side, size_per_side)
-        x, y = x.reshape(shape), y.reshape(shape)
-
-        vmin = min([prediction.min(), synthetic_data.dobs.min()])
-        vmax = max([prediction.max(), synthetic_data.dobs.max()])
-        _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
-        tmp = ax1.pcolormesh(x, y, prediction.reshape(shape), vmin=vmin, vmax=vmax)
-        ax2.pcolormesh(x, y, synthetic_data.dobs.reshape(shape), vmin=vmin, vmax=vmax)
-
-        for ax in (ax1, ax2):
-            ax.set_aspect("equal")
-        plt.colorbar(tmp, ax=(ax1, ax2))
-        plt.show()
-        # -------------------------------
-        diff = prediction - synthetic_data.dobs
-        maxabs = np.max(np.abs(diff))
-
-        tmp = plt.pcolormesh(x, y, diff.reshape(shape), vmin=-maxabs, vmax=maxabs)
-        plt.gca().set_aspect("equal")
-        plt.colorbar(tmp)
-        plt.show()
-        # -------------------------------
-
-        dmisfit = inv.invProb.dmisfit(recovered_model)
-        print(dmisfit, prediction.shape[0])
-        assert dmisfit < prediction.shape[0] * 1.15
-
-        np.testing.assert_almost_equal(prediction, synthetic_data.dobs, decimal=3)
+    def test_gravity_equivalent_sources(self, tree_mesh, simulation, synthetic_data):
+        """Test gravity equivalent sources against block model."""
+        # Build inversion
+        inversion = self.build_inversion(tree_mesh, simulation, synthetic_data)
+        # Run inversion
+        starting_model = np.zeros(tree_mesh.n_cells)
+        recovered_model = inversion.run(starting_model)
+        # Predict data
+        prediction = simulation.dpred(recovered_model)
+        # Check if prediction is close to the synthetic data
+        atol, rtol = 0.004, 1e-5
+        np.testing.assert_allclose(
+            prediction, synthetic_data.dobs, atol=atol, rtol=rtol
+        )
