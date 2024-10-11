263dfa9aa947b8e7d9a709a4191810167cacab4c
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-10-11 16:16:42 -0600

    simplify default solver setter
    Will now require a pymatsolver.solvers.Base subclass be set as the default solver.

--- simpeg/utils/solver_utils.py
+++ simpeg/utils/solver_utils.py
@@ -5,12 +5,13 @@ from pymatsolver import (
     SolverCG,
     SolverBiCG,
     Diagonal,
-    WrapDirect,
-    WrapIterative,
+    Pardiso,
+    Mumps,
+    wrap_direct,
+    wrap_iterative,
 )
+from pymatsolver.solvers import Base
 from .code_utils import deprecate_function
-import scipy.sparse as sp
-import numpy as np
 import warnings
 
 __all__ = [
@@ -19,8 +20,10 @@ __all__ = [
     "SolverCG",
     "SolverBiCG",
     "Diagonal",
-    "WrapDirect",
-    "WrapIterative",
+    "Pardiso",
+    "Mumps",
+    "wrap_direct",
+    "wrap_iterative",
     "get_default_solver",
     "set_default_solver",
     "SolverWrapD",
@@ -33,16 +36,8 @@ __all__ = [
 # Mumps (optional, but available for all systems)
 # Scipy's SuperLU (available for all scipy systems)
 if AvailableSolvers["Pardiso"]:
-    from pymatsolver import Pardiso
-
-    __all__ += ["Pardiso"]
-
     _DEFAULT_SOLVER = Pardiso
 elif AvailableSolvers["Mumps"]:
-    from pymatsolver import Mumps
-
-    __all__ += ["Mumps"]
-
     _DEFAULT_SOLVER = Mumps
 else:
     _DEFAULT_SOLVER = SolverLU
@@ -58,7 +53,8 @@ def get_default_solver():
 
     Returns
     -------
-    solver class
+    solver : type
+        The default solver class used by simpeg's simulations.
     """
     warnings.warn(
         f"Using the default solver: {_DEFAULT_SOLVER.__name__}",
@@ -73,61 +69,29 @@ def set_default_solver(solver):
 
     Parameters
     ----------
-    solver
-        a solver like class used to construct an object
+    solver : type
+        A solver class used to construct an object
         that acts os the inverse of a sparse matrix, and
-        supports the `__mul__` operation (at a miniumum).
+        supports the `__mul__` operation (at a miniumum)
+        with both single and multiple right hand sides.
     """
     global _DEFAULT_SOLVER
-    # likely need to do some simple tests to make sure that the class is a valid
-    # solver class?
-    # test that you can pass it a scipy sparse matrix
-    try:
-        A = sp.csr_matrix(
-            [
-                [
-                    0.5,
-                    0.0,
-                ],
-                [0.0, 2.0],
-            ]
+    if not issubclass(solver, Base):
+        raise TypeError(
+            "Default solver must be a subclass of pymatsolver.solvers.Base."
         )
-        Ainv = solver(A)
-    except TypeError as err:
-        raise TypeError(
-            "Invalid solver type, unable to pass a csr_matrix to the constructor."
-        ) from err
-
-    # test that you can multiply a single right hand side to the solver
-    try:
-        b = np.array([1.0, -1.0])
-        _ = Ainv * b
-    except Exception as err:
-        raise TypeError(
-            "Unable to multiply the solver instance by a single numpy vector"
-        ) from err
-
-    # test that you can multiply it by a 2D column vector.
-    try:
-        b = np.array([[1.0], [-1.0]])
-        _ = Ainv * b
-    except Exception as err:
-        raise TypeError(
-            "Unable to multiply the solver instance by multiple numpy vectors"
-        ) from err
-
     _DEFAULT_SOLVER = solver
 
 
 # should likely deprecate these classes in favor of the pymatsolver versions.
 SolverWrapD = deprecate_function(
-    WrapDirect,
+    wrap_iterative,
     old_name="SolverWrapD",
     removal_version="0.24.0",
     new_location="pymatsolver",
 )
 SolverWrapI = deprecate_function(
-    WrapIterative,
+    wrap_iterative,
     old_name="SolverWrapI",
     removal_version="0.24.0",
     new_location="pymatsolver",
