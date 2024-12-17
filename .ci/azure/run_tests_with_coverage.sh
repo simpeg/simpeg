84e145305efc0544d019d8ca33150325ee479c90
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-12-17 13:25:27 -0700

    make testing script run all lines
    And return the exit status of the last failed line.

--- .ci/azure/run_tests_with_coverage.sh
+++ .ci/azure/run_tests_with_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex #echo on and exit if any line fails
+set -x -o pipefail #echo on and return non-zero status if any line fails
 
 source activate simpeg-test
 pytest $TEST_TARGET --cov --cov-config=pyproject.toml -v -W ignore::DeprecationWarning
