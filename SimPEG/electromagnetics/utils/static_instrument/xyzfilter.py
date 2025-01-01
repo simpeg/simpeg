40350a7d193b0ee2ecf6d2607eb4c9ef72e7bb4f
Benjamin R. Bloss <bloss.benjamin@gmail.com>
2025-01-01 13:11:51 +0100

    removing a print statement

--- SimPEG/electromagnetics/utils/static_instrument/xyzfilter.py
+++ SimPEG/electromagnetics/utils/static_instrument/xyzfilter.py
@@ -31,7 +31,6 @@ class FilteredXYZ(libaarhusxyz.XYZ):
                 soundings = self.layer_data[key].iloc[soundingfilter]
             else:
                 soundings = self.layer_data[key].loc[soundingfilter]
-            print(f"key = {key}")
             self.layer_data[key] = soundings.iloc[:, self.get_layerfilter(key)].reset_index(drop=True)
             
         return self
