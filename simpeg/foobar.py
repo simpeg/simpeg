c9525daa9ee1d236f872bf93e93ec0733e12d4f4
Santiago Soler <santisoler@fastmail.com>
2025-01-30 09:30:56 -0800

    Revert "Add dumb file to test reviewdog on PRs"
    This reverts commit 7625cef1eb122532cb00c52238e19fa58d753ced.

--- simpeg/foobar.py
+++
@@ -1,9 +0,0 @@
-def f(x):
-    # black should complain about the spaces between the pow operator
-    return x ** 2
-
-
-def g(x, y):
-    # flake8 should complain about unused variable
-    z = x == y
-    return x + y
