d05ab95884181c5f732e97f6d9a3885440363fa0
Santiago Soler <santisoler@fastmail.com>
2025-08-12 16:14:06 -0700

    Use UpdateIRLS in gravity tutorial

--- tutorials/03-gravity/plot_inv_1c_gravity_anomaly_irls_compare_weighting.py
+++ tutorials/03-gravity/plot_inv_1c_gravity_anomaly_irls_compare_weighting.py
@@ -243,11 +243,9 @@ starting_beta = directives.BetaEstimate_ByEig(beta0_ratio=1e0)
 
 # Defines the directives for the IRLS regularization. This includes setting
 # the cooling schedule for the trade-off parameter.
-update_IRLS = directives.Update_IRLS(
+update_IRLS = directives.UpdateIRLS(
     f_min_change=1e-4,
     max_irls_iterations=30,
-    coolEpsFact=1.5,
-    beta_tol=1e-2,
 )
 
 # Options for outputting recovered models and predicted data for each beta.
@@ -302,11 +300,9 @@ starting_beta = directives.BetaEstimate_ByEig(beta0_ratio=1e0)
 
 # Defines the directives for the IRLS regularization. This includes setting
 # the cooling schedule for the trade-off parameter.
-update_IRLS = directives.Update_IRLS(
+update_IRLS = directives.UpdateIRLS(
     f_min_change=1e-4,
     max_irls_iterations=30,
-    coolEpsFact=1.5,
-    beta_tol=1e-2,
 )
 
 # Options for outputting recovered models and predicted data for each beta.
@@ -361,11 +357,9 @@ starting_beta = directives.BetaEstimate_ByEig(beta0_ratio=1e0)
 
 # Defines the directives for the IRLS regularization. This includes setting
 # the cooling schedule for the trade-off parameter.
-update_IRLS = directives.Update_IRLS(
+update_IRLS = directives.UpdateIRLS(
     f_min_change=1e-4,
     max_irls_iterations=30,
-    coolEpsFact=1.5,
-    beta_tol=1e-2,
 )
 
 # Options for outputting recovered models and predicted data for each beta.
