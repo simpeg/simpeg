d8decfcc9db0c095c0e779a87f5845db3a01560a
Santiago Soler <santisoler@fastmail.com>
2025-10-14 12:07:50 -0700

    Update usage of get_indices_block in tests and examples

--- tests/pf/test_mag_MVI_Octree.py
+++ tests/pf/test_mag_MVI_Octree.py
@@ -74,14 +74,14 @@ class MVIProblemTest(unittest.TestCase):
         M_xyz = utils.mat_utils.dip_azimuth2cartesian(M[0], M[1])
 
         # Get the indicies of the magnetized block
-        ind = utils.model_builder.get_indices_block(
+        indices = utils.model_builder.get_indices_block(
             np.r_[-20, -20, -10],
             np.r_[20, 20, 25],
             mesh.gridCC,
-        )[0]
+        )
 
         # Assign magnetization values
-        model[ind, :] = np.kron(np.ones((ind.shape[0], 1)), M_xyz * 0.05)
+        model[indices, :] = np.kron(np.ones((indices.size, 1)), M_xyz * 0.05)
 
         # Remove air cells
         self.model = model[actv, :]
