74186b97aa6f4096a6fc38ade76593b6b4b4e6f5
dccowan <devinccowan@gmail.com>
2025-10-29 16:16:52 -0700

    Test for deprecated approach for generating source terms.

--- simpeg/electromagnetics/natural_source/utils/test_utils.py
+++ simpeg/electromagnetics/natural_source/utils/test_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import discretize
 from simpeg import maps, mkvc, utils
-from ....utils import unpack_widths
+from ....utils import unpack_widths, deprecate_class
 from ..receivers import (
     Impedance,
     Tipper,
@@ -11,6 +11,7 @@ from ..survey import Survey
 from ..sources import PlanewaveXYPrimary, Planewave
 from ..simulation import Simulation3DPrimarySecondary
 from .data_utils import appResPhs
+from .source_utils import homo1DModelSource
 
 # Define the tolerances
 TOLr = 5e-2
@@ -571,3 +572,28 @@ def twoLayer(conds):
     sigBG[groundInd] = conds[1]
 
     return (M, freqs, sig, sigBG, rx_loc)
+
+
+@deprecate_class(removal_version=0.27)
+class PlanewaveXYPrimaryDeprecated(PlanewaveXYPrimary):
+    """Deprecated planewave source for 3D simulations."""
+
+    def ePrimary(self, simulation):
+        """Primary electric field source.
+
+        Parameters
+        ----------
+        simulation : simpeg.electromagnetics.natural_source_simulation.BaseNSEMSimulation
+            A NSEM simulation.
+
+        Returns
+        -------
+        numpy.ndarray
+            Primary electric field.
+        """
+        if self._ePrimary is None:
+            sigma_1d, _ = self._get_sigmas(simulation)
+            self._ePrimary = homo1DModelSource(
+                simulation.mesh, self.frequency, sigma_1d
+            )
+        return self._ePrimary
