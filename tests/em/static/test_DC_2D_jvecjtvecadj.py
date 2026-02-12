6cc4cdc08e738183532c7f8fff299655cc88e5ea
TomSHudsonFST <thomas.hudson@fleet.space>
2026-02-12 18:08:08 +0000

    Fix for issue #1712 (#1738)
    #### Summary
    Fix for an issue where a DC resistivity inversion prediction has wrong
    output size because a return statement returns a vector product rather
    than a scalar product. Resolves issue #1712 .
    
    Error: When running a 3D DC resistivity inversion, if one specifies a
    Pole-Dipole configuration then the receiver evaluation method fails
    because it tries to populate a slice of dpred (dpred[src_rx_slice]) with
    a flattened matix: mkvc(rx.eval(src, self.mesh, f))) that has an
    inconsistent shape.
    
    Source of error: The source of the error stems from the fact that
    currently, rx.eval() returns a (nrx,nrx) matrix, where nrx is the number
    of receivers. The output should instead be of shape (nrx,1). The reason
    rx.eval() returns a (nrx,nrx) matrix rather than a a (nrx,1) matrix is
    because it returns "v / self.geometric_factor[src]", where both v and
    self.geometric_factor[src] are vectors of length nrx, but it does a
    matrix division rather than a scalar devision.
    
    Solution: Various solutions to change the math output to scalar
    division. My working suggestion is:
    Replace: "return v / self.geometric_factor[src]" on L188 of
    simpeg/electromagnetics/static/resistivity/receivers.py
    with: "return v / self.geometric_factor[src].reshape(-1, 1)" where the
    reshape forces the operation to be scalar rather than vector, causing
    the returned value to be of shape (nrx,1) rather than (nrx,nrx).
    
    #### PR Checklist
    * [X] If this is a work in progress PR, set as a Draft PR
    * [X] Linted my code according to the [style
    guides](https://docs.simpeg.xyz/latest/content/getting_started/contributing/code-style.html).
    * [X] Added
    [tests](https://docs.simpeg.xyz/latest/content/getting_started/contributing/testing.html)
    to verify changes to the code.
    * [X] Added necessary documentation to any new functions/classes
    following the
    expect
    [style](https://docs.simpeg.xyz/latest/content/getting_started/contributing/documentation.html).
    * [X] Marked as ready for review (if this is was a draft PR), and
    converted
          to a Pull Request
    * [X] Tagged ``@simpeg/simpeg-developers`` when ready for review.
    
    #### Reference issue
    <!--Example: write "Closes #1712" to automatically close that issue on
    merge.-->
    
    #### What does this implement/fix?
    Replaced: "return v / self.geometric_factor[src]" on L188 of
    simpeg/electromagnetics/static/resistivity/receivers.py
    with: "return v / self.geometric_factor[src].reshape(-1, 1)" where the
    reshape forces the operation to be scalar rather than vector, causing
    the returned value to be of shape (nrx,1) rather than (nrx,nrx).
    
    ---------
    
    Co-authored-by: Santiago Soler <santisoler@fastmail.com>
    Co-authored-by: Joseph Capriotti <josephrcapriotti@gmail.com>

--- tests/em/static/test_DC_2D_jvecjtvecadj.py
+++ tests/em/static/test_DC_2D_jvecjtvecadj.py
@@ -32,10 +32,12 @@ class DCProblem_2DTests(unittest.TestCase):
         A0loc = np.r_[-150, 0.0]
         A1loc = np.r_[-130, 0.0]
         # rxloc = [np.c_[M, np.zeros(20)], np.c_[N, np.zeros(20)]]
-        rx1 = dc.receivers.Dipole(M, N)
-        rx2 = dc.receivers.Dipole(M, N, data_type="apparent_resistivity")
-        src0 = dc.sources.Pole([rx1, rx2], A0loc)
-        src1 = dc.sources.Pole([rx1, rx2], A1loc)
+        rx1 = dc.receivers.Pole(M)
+        rx2 = dc.receivers.Pole(M, data_type="apparent_resistivity")
+        rx3 = dc.receivers.Dipole(M, N)
+        rx4 = dc.receivers.Dipole(M, N, data_type="apparent_resistivity")
+        src0 = dc.sources.Pole([rx1, rx2, rx3, rx4], A0loc)
+        src1 = dc.sources.Dipole([rx1, rx2, rx3, rx4], A0loc, A1loc)
         survey = dc.survey.Survey([src0, src1])
         survey.set_geometric_factor()
         simulation = getattr(dc, self.formulation)(
