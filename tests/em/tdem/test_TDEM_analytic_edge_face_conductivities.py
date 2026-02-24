9722dd7f72123baef0f9d024f791f67fdd626d58
dccowan <devinccowan@gmail.com>
2026-02-24 11:27:47 -0800

    Clean up analytic FDEM tests for face conductances Add analytic TDEM tests for face conductances

--- tests/em/tdem/test_TDEM_analytic_edge_face_conductivities.py
+++ tests/em/tdem/test_TDEM_analytic_edge_face_conductivities.py
@@ -3,26 +3,29 @@ import numpy as np
 import pytest
 from scipy.constants import mu_0
 from simpeg import maps
-from simpeg.electromagnetics import frequency_domain as fdem
+from simpeg.electromagnetics import time_domain as tdem
 
-ABS_TOL = 1e-13
-REL_TOL = 0.01
+REL_TOL = 0.07
 
-@pytest.mark.parametrize("orientation", ["x", "z"])
-@pytest.mark.parametrize("rx_type", ["MagneticFluxDensity"])
-@pytest.mark.parametrize("formulation", ["MagneticFluxDensity", "ElectricField"])
-@pytest.mark.parametrize("mesh_type", ["CYL", "TREE"])
-def test_layer_conductance_to_analytic(mesh_type, formulation, rx_type, orientation):
+CASES_LIST = [
+    ("ElectricField", "CYL"),
+    ("ElectricField", "TREE"),
+    ("MagneticFluxDensity", "TREE")
+]
+
+@pytest.mark.parametrize("formulation, mesh_type", CASES_LIST)
+def test_layer_conductance_to_analytic(formulation, mesh_type):
     # Some static parameters
-    loop_radius = np.pi**-0.5
-    receiver_location = np.c_[12.0, 0.0, 1.0]
-    source_location = np.r_[0.0, 0.0, 1.0]
-    frequencies = np.logspace(2, 3, 2)
+    times = np.logspace(-3, -2, 6)
 
-    layer_depth = 40.0
-    layer_thickness = 0.1
+    loop_radius = np.pi**-0.5
+    receiver_location = np.c_[16.0, 0.0, 1.0]
+    source_location = np.r_[0.0, 0.0, 1.0]
+
+    layer_depth = 100.0
+    layer_thickness = 1.
     layer_conductivity = 100
-    background_conductivity = 2.5e-3
+    background_conductivity = 1e-2
 
     tau = layer_thickness * layer_conductivity
 
@@ -37,18 +40,18 @@ def test_layer_conductance_to_analytic(mesh_type, formulation, rx_type, orientat
 
     # 3D LAYER MODEL
     if mesh_type == "CYL":
-        hr = [(2.0, 120), (2.0, 25, 1.3)]
-        hz = [(2.0, 25, -1.3), (2.0, 200), (2.0, 25, 1.3)]
+        hr = [(2.5, 120), (2.5, 25, 1.3)]
+        hz = [(2.5, 25, -1.3), (2.5, 200), (2.5, 25, 1.3)]
 
         mesh = discretize.CylindricalMesh([hr, 1, hz], x0="00C")
 
         ind = np.where(mesh.h[2] == np.min(mesh.h[2]))[0]
         ind = ind[int(len(ind) / 2)]
 
-        mesh.origin = mesh.origin - np.r_[0.0, 0.0, mesh.nodes_z[ind] - 24]
+        mesh.origin = mesh.origin - np.r_[0.0, 0.0, mesh.nodes_z[ind] - 50]
 
     elif mesh_type == "TREE":
-        dh = 2.5  # base cell width
+        dh = 5.0  # base cell width
         dom_width = 8000.0  # domain width
         nbc = 2 ** int(
             np.round(np.log(dom_width / dh) / np.log(2.0))
@@ -62,8 +65,8 @@ def test_layer_conductance_to_analytic(mesh_type, formulation, rx_type, orientat
             padding_cells_by_level=[8, 4, 4, 4],
             finalize=False,
         )
-        x0s = np.vstack([ii * np.c_[-60, -60, -60] for ii in range(1, 5)])
-        x1s = np.vstack([ii * np.c_[60, 60, 10] for ii in range(1, 5)])
+        x0s = np.vstack([ii * np.c_[-120, -120, -120] for ii in range(1, 5)])
+        x1s = np.vstack([ii * np.c_[120, 120, 20] for ii in range(1, 5)])
 
         mesh.refine_box(x0s, x1s, levels=[-2, -3, -4, -5], finalize=False)
         mesh.finalize()
@@ -76,70 +79,76 @@ def test_layer_conductance_to_analytic(mesh_type, formulation, rx_type, orientat
     tau_map = maps.IdentityMap(nP=mesh.n_faces)
 
     # DEFINE SURVEY
-    rx_list = [
-        getattr(fdem.receivers, "Point{}Secondary".format(rx_type))(
-            receiver_location, component="real", orientation=orientation
-        ),
-        getattr(fdem.receivers, "Point{}Secondary".format(rx_type))(
-            receiver_location, component="imag", orientation=orientation
-        ),
-    ]
+    waveform = tdem.sources.StepOffWaveform()
+
+    if formulation == 'ElectricField':
+        rx_list = [
+            tdem.receivers.PointMagneticFluxTimeDerivative(
+                receiver_location, orientation='z', times=times
+            )
+        ]
+    else:
+        rx_list = [
+            tdem.receivers.PointMagneticFluxDensity(
+                receiver_location, orientation='z', times=times
+            )
+        ]
 
     # 1D SURVEY AND SIMULATION
     src_1d = [
-        fdem.sources.MagDipole(
-            rx_list, f, location=np.r_[0.0, 0.0, 1.0], orientation=orientation
+        tdem.sources.MagDipole(
+            rx_list, location=np.r_[0.0, 0.0, 1.0], orientation='z', waveform=waveform
         )
-        for f in frequencies
     ]
-    survey_1d = fdem.Survey(src_1d)
+    survey_1d = tdem.Survey(src_1d)
 
-    sim_1d = fdem.Simulation1DLayered(
+    sim_1d = tdem.Simulation1DLayered(
         survey=survey_1d,
         thicknesses=thicknesses,
         sigmaMap=sigma_map_1d,
     )
 
     # 3D SURVEY AND SIMULATION
+    time_steps = [(5e-5, 20), (2.5e-4, 40)]
+
     if mesh_type == "CYL":
         src_3d = [
-            fdem.sources.CircularLoop(
+            tdem.sources.CircularLoop(
                 rx_list,
-                f,
                 radius=loop_radius,
                 location=source_location,
+                waveform=waveform,
             )
-            for f in frequencies
         ]
     else:
         src_3d = [
-            fdem.sources.MagDipole(
+            tdem.sources.MagDipole(
                 rx_list,
-                f,
                 location=source_location,
-                orientation=orientation,
+                orientation='z',
+                waveform=waveform,
             )
-            for f in frequencies
         ]
 
-    survey_3d = fdem.Survey(src_3d)
+    survey_3d = tdem.Survey(src_3d)
 
     # DEFINE THE SIMULATIONS
     if formulation == "MagneticFluxDensity":
-        sim_3d = fdem.Simulation3DHierarchicalMagneticFluxDensity(
+        sim_3d = tdem.Simulation3DHierarchicalMagneticFluxDensity(
             mesh=mesh, survey=survey_3d, sigma=sigma_3d, tauMap=tau_map
         )
     else:
-        sim_3d = fdem.Simulation3DHierarchicalElectricField(
+        sim_3d = tdem.Simulation3DHierarchicalElectricField(
             mesh=mesh, survey=survey_3d, sigma=sigma_3d, tauMap=tau_map
         )
+    sim_3d.time_steps = time_steps
 
     # COMPUTE SOLUTIONS
-    analytic_solution = mu_0 * sim_1d.dpred(sigma_1d)  # ALWAYS RETURNS H-FIELD
+    analytic_solution = sim_1d.dpred(sigma_1d)  # ALWAYS RETURNS H-FIELD
     numeric_solution = sim_3d.dpred(tau_3d)
 
     np.testing.assert_allclose(
-        numeric_solution, analytic_solution, atol=ABS_TOL, rtol=REL_TOL
+        numeric_solution, analytic_solution, rtol=REL_TOL
     )
 
 
