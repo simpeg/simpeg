7464ec353615c2946c946c6adce938a449d1960b
dccowan <devinccowan@gmail.com>
2026-05-01 15:16:11 -0700

    Remove the addition of vector potential source for simplex meshes.

--- simpeg/electromagnetics/time_domain/sources.py
+++ simpeg/electromagnetics/time_domain/sources.py
@@ -1347,40 +1347,6 @@ class MagDipole(BaseTDEMSrc):
     def _aSrc(self, simulation):
         coordinates = "cartesian"
 
-        # # For adding Simplex meshes
-        # if isinstance(simulation.mesh, SimplexMesh):
-        #     if simulation._formulation == "EB":
-        #         edges = simulation.mesh.edges
-        #         edge_tangents = simulation.mesh.edge_tangents
-        #         axyz = self._srcFct(edges, coordinates)
-        #         a = np.sum(axyz * edge_tangents, axis=1)
-        #     else:
-        #         faces = simulation.mesh.faces
-        #         face_normals = simulation.mesh.face_normals
-        #         axyz = self._srcFct(faces, coordinates)
-        #         a = np.sum(axyz * face_normals, axis=1)
-
-        # else:
-        #     if simulation._formulation == "EB":
-        #         gridX = simulation.mesh.gridEx
-        #         gridY = simulation.mesh.gridEy
-        #         gridZ = simulation.mesh.gridEz
-
-        #     elif simulation._formulation == "HJ":
-        #         gridX = simulation.mesh.gridFx
-        #         gridY = simulation.mesh.gridFy
-        #         gridZ = simulation.mesh.gridFz
-
-        #     if simulation.mesh._meshType == "CYL":
-        #         coordinates = "cylindrical"
-        #         if simulation.mesh.is_symmetric:
-        #             return self._srcFct(gridY)[:, 1]
-
-        #     ax = self._srcFct(gridX, coordinates)[:, 0]
-        #     ay = self._srcFct(gridY, coordinates)[:, 1]
-        #     az = self._srcFct(gridZ, coordinates)[:, 2]
-        #     a = np.concatenate((ax, ay, az))
-
         if simulation.mesh._meshType == "CYL":
             coordinates = "cylindrical"
             if simulation.mesh.is_symmetric:
