134eeaa5d4343a969ce08fa50b61a9f8e8056cca
dccowan <devinccowan@gmail.com>
2026-06-12 09:25:40 -0700

    change n_skin_depth to skin_depth_factor

--- simpeg/electromagnetics/natural_source/utils/source_utils.py
+++ simpeg/electromagnetics/natural_source/utils/source_utils.py
@@ -19,7 +19,7 @@ def primary_e_1d_solution(
     freq,
     top_bc="dirichlet",
     bot_bc="robin",
-    n_skin_depths=3,
+    skin_depth_factor=3,
     n_pad=None,
 ):
     r"""Compute 1D electric field solution on nodes.
@@ -42,7 +42,7 @@ def primary_e_1d_solution(
         Assumes only a downgoing wave at the bottom boundary. Use "dirichlet"
         to set the value directly from the semi-analytic propagator matrix
         solution. Use "robin" to set the boundary condition discretely.
-    n_skin_depths : float
+    skin_depth_factor : float
         Number of additional skin depths added to the bottom of the 1D mesh
         where the discrete solution is solved.
     n_pad : int
@@ -122,11 +122,11 @@ def primary_e_1d_solution(
     if n_pad is not None:
         warn(
             "Number of padding cells no longer set directly with 'n_pad'. "
-            "Use 'n_skin_depths' to set extent of padding for 1D mesh."
+            "Use 'skin_depth_factor' to set extent of padding for 1D mesh."
         )
     else:
         skin_depth = np.sqrt(2 / (omega(freq) * mu_0 * sigma_1d[0]))
-        n_pad = int(np.ceil(n_skin_depths * skin_depth / hz[0]))
+        n_pad = int(np.ceil(skin_depth_factor * skin_depth / hz[0]))
 
     hz_ext = np.pad(hz, (n_pad, 0), mode="edge")
     mesh_ext = TensorMesh([hz_ext], origin=[mesh.origin[-1] - hz[0] * n_pad])
@@ -193,7 +193,7 @@ def primary_h_1d_solution(
     freq,
     top_bc="dirichlet",
     bot_bc="robin",
-    n_skin_depths=3,
+    skin_depth_factor=3,
     n_pad=None,
 ):
     r"""Compute 1D magnetic field solution on nodes.
@@ -216,7 +216,7 @@ def primary_h_1d_solution(
         Assumes only a downgoing wave at the bottom boundary. Use "dirichlet"
         to set the value directly from the semi-analytic propagator matrix
         solution. Use "robin" to set the boundary condition discretely.
-    n_skin_depths : float
+    skin_depth_factor : float
         Number of additional skin depths added to the bottom of the 1D mesh
         where the discrete solution is solved.
     n_pad : int
@@ -295,11 +295,11 @@ def primary_h_1d_solution(
     if n_pad is not None:
         warn(
             "Number of padding cells no longer set directly with 'n_pad'. "
-            "Use 'n_skin_depths' to set extent of padding for 1D mesh."
+            "Use 'skin_depth_factor' to set extent of padding for 1D mesh."
         )
     else:
         skin_depth = np.sqrt(2 / (omega(freq) * mu_0 * sigma_1d[0]))
-        n_pad = int(np.ceil(n_skin_depths * skin_depth / hz[0]))
+        n_pad = int(np.ceil(skin_depth_factor * skin_depth / hz[0]))
 
     hz_ext = np.pad(hz, (n_pad, 0), mode="edge")
     mesh_ext = TensorMesh([hz_ext], origin=[mesh.origin[-1] - hz[0] * n_pad])
