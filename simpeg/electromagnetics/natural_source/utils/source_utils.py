501fd697e0e6ea9ad055851045c0b39263a6c772
Devin C. Cowan <devinccowan@gmail.com>
2026-08-19 11:07:51 -0700

    Update simpeg/electromagnetics/natural_source/utils/source_utils.py
    Co-authored-by: Santiago Soler <santisoler@fastmail.com>

--- simpeg/electromagnetics/natural_source/utils/source_utils.py
+++ simpeg/electromagnetics/natural_source/utils/source_utils.py
@@ -18,7 +18,7 @@ def primary_e_1d_solution(
     freq,
     top_bc="dirichlet",
     bot_bc="robin",
-    skin_depth_factor=3,
+    skin_depth_factor=3.0,
 ):
     r"""Compute 1D electric field solution on nodes.
 
